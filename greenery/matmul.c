b68cc4c337e7883c25285a51c90369ccf2e87bdf
skahan <skahan@n01.sampa>
2011-01-04 14:16:02 -0800

    matmul works, but slow

--- greenery/matmul.c
+++ greenery/matmul.c
@@ -63,19 +63,21 @@ void matmul_green(thread *me, void *arg) {
   double *x = la->x;
   CSR A = la->A;
 
+  // fprintf(stderr, "m %d y %p x %p A.rs %p A.col %p A.val %p\n",
+  //	  m, y, x, A.rowstart, A.col, A.val);
   // Basic SpMV implementation, 
   // y <- y + A*x, where A is in CSR. 
   for ( i = 0; i < m; ++i) { 
     int k;
     double y0 = y[i];
+    
+    prefetch_and_switch(me, &(x[A.col[A.rowstart[i]]]), 0);
     for ( k = A.rowstart[i]; k < A.rowstart[i+1]; ++k) {
-      prefetch_and_switch(me, &(x[A.col[k]]), 0);
       y0 += A.val[k] * x[A.col[k]];
     }
     y[i] = y0;
   }
 }
-
 
 int main(int argc, char ** argv) {
   int i;
@@ -102,9 +104,6 @@ int main(int argc, char ** argv) {
   for ( i = 0; i < N; i++) x[i] = 1.0;
   memset(y, 0, sizeof(double)*N);
 
-#pragma omp parallel
-  if (omp_get_num_threads() < num_cores) fprintf(stderr, "num threads is really %d\n", omp_get_num_threads());
-
   struct timeval start,finish;
   gettimeofday(&start,NULL);
   for (i = 0; i < iters; i++) matmul(y, A, N, x);
@@ -122,24 +121,25 @@ int main(int argc, char ** argv) {
   /* same computation with green threads */
   if (!threads_per_core) exit(0);
   memset(y, 0, sizeof(double)*N);
-
-  gettimeofday(&start,NULL);
-  for (i = 0; i < iters; i++) {
+    gettimeofday(&start,NULL);
+    /*    for (i = 0; i < iters; i++) { */
     int c;
-#pragma omp parallel for
+ #pragma omp parallel for
     for ( c = 0; c < num_cores; c++) { 
       loop_arg * la = (loop_arg *) malloc(sizeof(loop_arg)*threads_per_core);
 
-      thread *master = thread_init();
-      scheduler * sched = create_scheduler(master);
-      thread *thread;
+      thread * master;
+      scheduler * sched;
+
+      master = thread_init();
+      sched = create_scheduler(master);
 
       /* M in this scope is # rows for which this core is responsible; M0 is index of the first of these rows */
 #define ROWS_IN_BLOCK(num_rows, block_index, num_blocks) ((num_rows/num_blocks)+(block_index < num_rows%num_blocks?1:0))
 #define FIRST_ROW_IN_BLOCK(num_rows, block_index, num_blocks) ((num_rows/num_blocks)*block_index + (block_index < num_rows%num_blocks?block_index:num_rows%num_blocks))
       int M = ROWS_IN_BLOCK(N,c,num_cores);
       int M0 = FIRST_ROW_IN_BLOCK(N,c,num_cores);
-      fprintf(stderr, "gthreads: Core %d responsible for rows %d to %d\n", c, M0, M0+M-1);
+      //fprintf(stderr, "gthreads: Core %d responsible for rows %d to %d\n", c, M0, M0+M-1);
 
       int t;
       for (t = 0; t < threads_per_core; ++t) {
@@ -148,30 +148,30 @@ int main(int argc, char ** argv) {
 	int m0 = FIRST_ROW_IN_BLOCK(M,t,threads_per_core);
 	la[t].id = t;
         la[t].x = x;
-	la[t].y = &y[m0];
+	la[t].y = &y[M0+m0];
 	la[t].A.rowstart = &A.rowstart[M0+m0];
-	la[t].A.col = &A.col[(M0+m0)*NZ];
-	la[t].A.val = &A.val[(M0+m0)*NZ];
+	la[t].A.col = A.col;
+	la[t].A.val = A.val;
 	la[t].m = m;
-	thread = thread_spawn(master, sched, matmul_green, (void*) &la[t]);
       }
-      for (t = 0; t < threads_per_core; ++t) {
-	fprintf(stderr, "t %d: id %ld, x %p, y %p, rs %ld, col %ld, val %ld, m %ld\n", t, la[t].id, (void*)la[t].x, (void*)la[t].y,
-		la[t].A.rowstart - A.rowstart, la[t].A.col - A.col,
-		la[t].A.val -A.val,
-		la[t].m);
+
+      int i;
+      for (i = 0; i < iters; i++)  {
+        thread *threads[threads_per_core];
+        for (t = 0; t < threads_per_core; ++t) threads[t] = thread_spawn(master, sched, matmul_green, (void*) &la[t]);
+	run_all(sched);
+        for (t = 0; t < threads_per_core; ++t) destroy_thread(threads[t]);
       }
-      fprintf(stderr, "running sched core %d\n", c);
-      run_all(sched);
-      fprintf(stderr, "done running sched core %d\n", c);
+
+
       destroy_scheduler(sched);
       destroy_thread(master);
       free(la);
     }
-  }
-  gettimeofday(&finish,NULL);
 
-  elapsed = ((finish.tv_sec - start.tv_sec)*1000000.0 + finish.tv_usec - start.tv_usec)/1000000.0;
+    gettimeofday(&finish,NULL);
+    elapsed = ((finish.tv_sec - start.tv_sec)*1000000.0 + finish.tv_usec - start.tv_usec)/1000000.0;
+
   fprintf(stderr, "gthreads: %g flops:  %d iters each in %g secs\n", iters*NZ*2*N/elapsed,iters, elapsed/iters);
 
   /* check sum */
