945c15b03a0493c916877b1737da62676e5045af
Brandon Myers <bdmyers@cs.washington.edu>
2011-02-18 01:24:30 -0800

    ready for experiements on magny cours machine

--- bench/gups/gups.c
+++ bench/gups/gups.c
@@ -1,27 +1,41 @@
+#include <features.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 #include <getopt.h>
+#include <omp.h>
 
-uint32_t* getIndices(int fieldsize, int num, int isRandom);
+
+#define MILLION 1000000
+#define BILLION 1000000000
+
+uint64_t* getIndices(uint64_t fieldsize, uint64_t num, int isRandom, int num_threads);
 uint64_t get_timediff_ns(struct timespec* start, struct timespec* end);
-void processArgs(int argc, char** argv, int* fieldsize, int* num_ups, int* num_threads, int* isRandom, int* isAtomic);
+void processArgs(int argc, char** argv, uint64_t* fieldsize, uint64_t* num_ups, int* num_threads, int* isRandom, int* isAtomic);
 
 
 
 int main(int argc, char** argv) {
-    int fieldsize = 1<<30;
-    int num_ups = 1<<24;
+    uint64_t fieldsize = 1<<30;
+    uint64_t num_ups = 1<<24;
     int isRandom = 1;
     int num_threads = 1;
     int isAtomic = 0;
 
     processArgs(argc, argv, &fieldsize, &num_ups, &num_threads, &isRandom, &isAtomic);
-
+   //printf("%lu fieldsize\n",fieldsize*sizeof(uint64_t)); 
     uint64_t* field = (uint64_t*)malloc(fieldsize*sizeof(uint64_t));
-    uint32_t* indices = getIndices(fieldsize, num_ups,isRandom);
+    if (field==0) {
+        printf("malloc failed\n");
+        exit(1);
+    }
+    printf("allocated %lu GB\n",fieldsize*sizeof(uint64_t)/BILLION);
+    //printf("field ptr=%lx\n",field);
+    printf("done field\n");
+    uint64_t* indices = getIndices(fieldsize, num_ups,isRandom,num_threads);
+    printf("done ind\n");
 
 
     struct timespec start;
@@ -33,7 +47,7 @@ int main(int argc, char** argv) {
         #pragma omp parallel num_threads(num_threads)
         {
          #pragma omp for
-         for (int i=0; i<num_ups; i++) {
+         for (uint64_t i=0; i<num_ups; i++) {
              __sync_fetch_and_add(&field[indices[i]], 1);
          }
         }
@@ -41,7 +55,7 @@ int main(int argc, char** argv) {
          #pragma omp parallel num_threads(num_threads)
         {
          #pragma omp for
-         for (int i=0; i<num_ups; i++) {
+         for (uint64_t i=0; i<num_ups; i++) {
              field[indices[i]]++;
          }
         }
@@ -53,28 +67,40 @@ int main(int argc, char** argv) {
 
     uint64_t runtime_ns = get_timediff_ns(&start, &end);
 
-    int MILLION=1000000;
-    int BILLION=1000000000;
     float gups = ((float)num_ups)/(runtime_ns);
     float mbperitem = sizeof(uint64_t)/((float)MILLION);
-printf("%d fieldsize, %f Gupdates, %ld ns, %f ns/up, %f gups, %f MB/s\n", fieldsize, num_ups/((float)BILLION), runtime_ns, ((float)runtime_ns)/num_ups, gups, gups*BILLION*mbperitem);
+    float gupdates = num_ups/((float)BILLION);
+    float ns_per_up = ((float)runtime_ns)/num_ups;
+    float mb_rate = gups*BILLION*mbperitem;
+printf("%ld fieldsize, %f Gupdates, %ld ns, %f ns/up, %f gups, %f MB/s\n", fieldsize, gupdates, runtime_ns, ns_per_up, gups, mb_rate);
 
+    printf("{'fieldsize':%ld, 'updates':%ld, 'gups':%f, 'runtime_ns':%ld, 'atomic':%d, 'random':%d, 'num_threads':%d, 'f_ele_size':%lu}\n", fieldsize, num_ups, gups, runtime_ns, isAtomic, isRandom, num_threads, sizeof(uint64_t));
 
     return 0;
 }
 
-uint32_t* getIndices(int fieldsize, int num, int isRandom) {
-    srand ((unsigned int) time (NULL));
+uint64_t* getIndices(uint64_t fieldsize, uint64_t num, int isRandom, int num_threads) {
 
-    uint32_t* indices = (uint32_t*)malloc(num*sizeof(uint32_t));
+    uint64_t* indices = (uint64_t*)malloc(num*sizeof(uint64_t));
 
-    if (isRandom) {
-        for (int i=0; i<num; i++) {
-            indices[i] = rand()%fieldsize;
+    if (isRandom) { 
+        unsigned int seedp;
+        srand((unsigned int)time(NULL));
+        #pragma omp parallel num_threads(num_threads) private(seedp)
+        {
+            seedp = omp_get_thread_num();
+            #pragma omp for
+            for (uint64_t i=0; i<num; i++) {
+                indices[i] = rand_r(&seedp) / (RAND_MAX/fieldsize + 1);
+            }
         }
     } else {
-        for (int i=0; i<num; i++) {
-            indices[i] = i;
+        #pragma omp parallel num_threads(num_threads)
+        {
+        #pragma omp for
+        for (uint64_t i=0; i<num; i++) {
+            indices[i] = i%fieldsize;
+        }
         }
     }
 
@@ -82,13 +108,12 @@ uint32_t* getIndices(int fieldsize, int num, int isRandom) {
 }
 
 uint64_t get_timediff_ns(struct timespec* start, struct timespec* end) {
-    int BILLION = 1000000000;
     return ((uint64_t) end->tv_sec * BILLION + end->tv_nsec) - ((uint64_t) start->tv_sec * BILLION + start->tv_nsec);
 }
 
 
 
-void processArgs(int argc, char** argv, int* fieldsize, int* num_ups, int* num_threads, int* isRandom, int* isAtomic) {
+void processArgs(int argc, char** argv, uint64_t* fieldsize, uint64_t* num_ups, int* num_threads, int* isRandom, int* isAtomic) {
 
 
   static struct option long_options[] = {
@@ -107,10 +132,10 @@ void processArgs(int argc, char** argv, int* fieldsize, int* num_ups, int* num_t
     case 0:   // flag set
       break;
     case 'f':
-      *fieldsize = (1<<(atoi(optarg)));
+      *fieldsize = (1L<<(atoi(optarg)));
       break;
     case 'u':
-      *num_ups = (1<<(atoi(optarg)));
+      *num_ups = (1L<<(atoi(optarg)));
       break;
     case 'c':
       *num_threads = atoi(optarg);
