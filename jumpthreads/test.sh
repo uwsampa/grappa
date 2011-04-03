6457a8a04eb5e33d86e4231d9e1db73a7c380cde
Andrew Hunter <ahh@cs.washington.edu>
2011-04-02 22:39:05 -0700

    add nice to testing script

--- jumpthreads/test.sh
+++ jumpthreads/test.sh
@@ -2,15 +2,28 @@
 
 CHASES=$1
 OUTPUT=$2
+HYPER=$3
+if [ $HYPER = nohyper ]
+then
+CORES="-6 -5 -4 -3 -2 -1 1 2 3 4 5 6"
+PIN="0-11:2"
+echo $CORES
+elif [ $HYPER = hyper ]
+then
+CORES="-12 -10 -8 -6 -4 -2 2 4 6 8 10 12"
+PIN="0,12,2,14,4,16,6,18,8,20,10,22"
+else
+ echo "bad value for HYPER: " $HYPER
+ exit 1
+fi
 
 echo > $OUTPUT
-for i in $(seq 1 1)
+for i in $(seq 1 32)
 do
-  make clean && make NTHR=$i
-#  for c in -6 -5 -4 -3 -2 -1 1 2 3 4 5 6
-  for c in -12 -10 -8 -6 -4 -2 2 4 6 8 10 12
+  make clean && make NTHR=$i pchase
+  for c in $CORES
   do
-#  GOMP_CPU_AFFINITY=0-11:2 hugectl --heap -- numactl --membind=0 -- ./pchase $c $CHASES $i >> $OUTPUT
-  GOMP_CPU_AFFINITY=0,12,2,14,4,16,6,18,8,20,10,22 hugectl --heap -- numactl --membind=0 -- ./pchase $c $CHASES $i >> $OUTPUT
+  echo $i $c
+  GOMP_CPU_AFFINITY=$PIN hugectl --heap -- numactl --membind=0 -- ./pchase $c $CHASES $i >> $OUTPUT
   done
 done
