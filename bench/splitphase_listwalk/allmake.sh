5ac1b3b6a19ca03abe471d7dd62bdfaa079b7e30
Brandon <bdmyers@ubuntu.(none)>
2011-06-22 11:19:08 -0700

    allmake convenience utility

---
+++ bench/splitphase_listwalk/allmake.sh
@@ -0,0 +1,14 @@
+THIS_DIR=`pwd`
+BASE_DIR=$THIS_DIR/../..
+Q_DIR=$BASE_DIR/queues
+G_DIR=$BASE_DIR/global_memory
+
+cd $Q_DIR
+make clean libcorequeue.a
+
+cd $G_DIR
+make clean libsplitphase.a
+
+cd $THIS_DIR
+make clean
+make
