2e5b416841f10b2bb83f5c11f9c99de2a2918f7c
Brandon <bdmyers@ubuntu.(none)>
2011-06-23 10:56:14 -0700

    have delegate term after all clients QUIT

--- global_memory/Delegate.hpp
+++ global_memory/Delegate.hpp
@@ -11,14 +11,14 @@ class Delegate {
         CoreQueue<uint64_t>** outQs;
         uint32_t numLocal;
         GlobalMemory* global_mem;
-        bool running;
+        int activeCount;
 
     public:
         Delegate(CoreQueue<uint64_t>* qs_out[], CoreQueue<uint64_t>* qs_in[], uint32_t numLocal, GlobalMemory* gm);
         void run();
         
         // should only be called by same thread that is doing run()
-        void terminate();
+        void decrementActive();
         bool doContinue();
 
 };
