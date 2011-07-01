d540e5983d33b0d95ff478fde40446e3f65aead1
Brandon Myers <bdmyers@cs.washington.edu>
2011-07-01 11:26:18 -0700

    O3 compilation; cache align MemoryDecriptor

--- global_memory/MemoryDescriptor.hpp
+++ global_memory/MemoryDescriptor.hpp
@@ -4,20 +4,32 @@
 #include "GmTypes.hpp"
 #include "thread.h"
 
+#define MD_USE_CACHE_ALIGN 0
+#define MD_CACHE_LINE_SIZE_BYTES 64
+
+
 class MemoryDescriptor {
     private:
         uint64_t* address;
         uint64_t _data;
         oper_enum operation;
-
         threadid_t thread_id;
         coreid_t core_id;
+        #if MD_USE_CACHE_ALIGN
+char pad1[MD_CACHE_LINE_SIZE_BYTES];
+       // char pad1[MD_CACHE_LINE_SIZE_BYTES-(sizeof(uint64_t*)+sizeof(uint64_t)+sizeof(oper_enum)+sizeof(threadid_t)+sizeof(coreid_t))];
+        #endif
 
+         volatile bool full;
+        #if MD_USE_CACHE_ALIGN
+       char pad2[MD_CACHE_LINE_SIZE_BYTES];
+       // char pad2[MD_CACHE_LINE_SIZE_BYTES-sizeof(bool)];
+        #endif
+    
     public:
-        MemoryDescriptor();
+       MemoryDescriptor();
         ~MemoryDescriptor();
 
-volatile        bool full;
         void fillData( uint64_t data );
         bool checkData( uint64_t* data);
         void setEmpty();
