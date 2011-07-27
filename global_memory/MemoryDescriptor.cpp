32cb0610a8a1fc0904f58e60dbad8bef09f44f8b
Brandon Myers <bdmyers@cs.washington.edu>
2011-07-26 17:06:19 -0700

    fix comment-out in MemoryDescriptor.cpp

--- global_memory/MemoryDescriptor.cpp
+++ global_memory/MemoryDescriptor.cpp
@@ -14,7 +14,8 @@ char* sprintit(int x) {
 }*/
 
 MemoryDescriptor::MemoryDescriptor()
-    : latency_timer(sprintit(omp_get_thread_num()), 2, 0, false) {
+   // : latency_timer(sprintit(omp_get_thread_num()), 2, 0, false) {
+    : latency_timer("thr_latency", 2, 0, false) {
     address = 0x0;
     _data = 0;
     operation = READ;
