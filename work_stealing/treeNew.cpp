d11e607ea355f5d8f72bc13f414ffac1cafbd9aa
Brandon Myers <bdmyers@cs.washington.edu>
2011-09-21 14:18:14 -0700

    treeNew wip

--- work_stealing/treeNew.cpp
+++ work_stealing/treeNew.cpp
@@ -6,6 +6,7 @@ void swap(int* arr, int i, int j) {
 }
 
 #define LOCAL_STEAL_RETRIES 1
+#define MAX_STEALS_IN_FLIGHT 1
 
 void run(int core_index, int num_threads, global_array* ibt, int myNode, int num_cores_per_node, int num_nodes) {
     
@@ -27,18 +28,29 @@ void run(int core_index, int num_threads, global_array* ibt, int myNode, int num
             swap(perm, core_index, 0);
         
             // try to steal locally in the random order
+            // TODO: should a remote steal be started first if non in progress?
             for (int i=1; i<num_cores_per_node; i++) {
                 swap(perm, i, random()%(num_cores_per_node-i)+i);
 
-                int victim_node = perm[i];
+                int victim_queue = perm[i];
 
-                if (CWSD_OK == local_wsqs[victim_node].steal(&work)) {
+                if (CWSD_OK == local_wsqs[victim_queue].steal(&work)) {
                     gotWork = true;
                     break;
                 }
             }
 
             if (!gotWork) {
+
+                // steal remote
+                if (num_steals_in_flight < MAX_STEALS_IN_FLIGHT) {
+                    int victim_node = random()%num_nodes;
+                    
+                    //printf("p%d: sending steal request to p%d queue%d\n", myNode, victim_node, i % num_cores_per_node);
+                    gasnet_AMRequestShort3 (victim_node, WORKSTEAL_REQUEST_HANDLER, i % num_cores_per_node, core_index, current_data->index);
+                    
+                }
+
 
 
 
