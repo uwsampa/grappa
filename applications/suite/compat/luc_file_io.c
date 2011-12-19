8ed38aa2bdf46b3956d2c9ba89d230ae1ba015a9
Brandon Holt <holt.bg@gmail.com>
2011-12-19 17:19:01 -0600

    Added compatability module (and cleaned up a bit)

---
+++ applications/suite/compat/luc_file_io.c
@@ -0,0 +1,27 @@
+#define _XOPEN_SOURCE 600
+#include <stddef.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "luc_file_io.h"
+
+int64_t luc_fread(void * ptr, size_t size, size_t nmemb, size_t offset, const char * filename) {
+	int fd = -1;
+	ssize_t sz;
+	if (!filename) return -1;
+	if ((fd = open(filename, O_RDONLY)) < 1) return -1;
+	sz = pread(fd, ptr, size * nmemb, offset);
+	close(fd);
+	return (int64_t) sz;
+}
+
+int64_t luc_fwrite(void * ptr, size_t size, size_t nmemb, size_t offset, const char * filename) {
+	int fd = -1;
+	ssize_t sz;
+	if (!filename) return -1;
+	if ((fd = open(filename, O_WRONLY|O_CREAT)) < 1) return -1;
+	sz = pwrite(fd, ptr, size * nmemb, offset);
+	close(fd);
+	return (int64_t) sz;
+}
