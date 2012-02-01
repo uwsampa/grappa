a598f20298ee59c9acf641cb44fe897608c09626
Jacob Nelson <nelson@cs.washington.edu>
2012-01-31 16:09:21 -0800

    Context switching: floating point support, lightweight compiler-managed saves/restores, and memory fences

--- greenery/stack.h
+++ greenery/stack.h
@@ -10,6 +10,27 @@ typedef void (*coro_func)(struct coro *, void *arg);
 // move stacks from <old> to <new> and return <ret> (to new guy)
 void* swapstacks(void **olds, void **news, void *ret);
 
+// for now, put this here
+// define this only when we're using the minimal save context switcher
+// if you switch this off, you must update the save/restore settings in stack.S
+#define SOFTXMT_SAVE_REGISTERS_LITE
+
+// depend on compiler to save and restore callee-saved registers
+inline void* swapstacks_inline(void **olds, void **news, void *ret) {
+  asm volatile ( ""
+                 : "+a" (ret), "+D" (olds), "+S" (news) // add some dependences for ordering
+                 : "D" (olds), "S" (news), "d" (ret)    // add some dependences for ordering
+                 : "cc", "flags", 
+#ifdef SOFTXMT_SAVE_REGISTERS_LITE
+                   "rbp", // doesn't work if we need a frame pointer
+                   "rbx", "r12", "r13", "r14", "r15", "fpcr",
+#endif
+                   "memory"
+                 );
+  // warning: watch out for register restoration before the call
+  return swapstacks( olds, news, ret );
+}
+
 // Given memory going DOWN FROM <stack>, create a basic stack we can swap to
 // (using swapstack) that will call <f>. (using <it> as its <me>).
 // <me> is a location we can store the current stack.
