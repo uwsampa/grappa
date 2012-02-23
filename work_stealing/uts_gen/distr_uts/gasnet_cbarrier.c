03101dfdb8771f39a9537fe13973ff4a0e51099e
Brandon Myers <bdmyers@cs.washington.edu>
2012-02-23 14:14:07 -0800

    make the cancellable barrier comm safe by adding a yield into the gasnet_blockuntil

--- work_stealing/uts_gen/distr_uts/gasnet_cbarrier.c
+++ work_stealing/uts_gen/distr_uts/gasnet_cbarrier.c
@@ -52,13 +52,32 @@ void cbarrier_cancel() {
     gasnet_AMRequestShort0(HOME_NODE, CANCEL_CBARRIER_REQUEST_HANDLER);
 }
 
+#define STATEMENT_WRAP(stmt, id, value) \
+        int _wrapped_(id)_ () { \
+            (stmt); \
+            return (value); \
+        } 
+
+#define STATEMENT(id) \
+        _wrapped_(id)_ ()
+
+#define STATEMENT_WRAP(SET_LOCK(&cb_lock), 0, 1);
+#define set_my_lock = STATEMENT(0)
+
+#define STATEMENT_WRAP(UNSET_LOCK(&cb_lock), 1, 0);
+#define unset_my_lock = STATEMENT(1)
+
+#define STATEMENT_WRAP(SoftXMT_yield(), 2, 0);
+#define yield_stmt = STATEMENT(3)
+
 int cbarrier_wait() {
     gasnet_AMRequestShort0(HOME_NODE, ENTER_CBARRIER_REQUEST_HANDLER);
 
     int isDone;
-    GASNET_BLOCKUNTIL((SET_COND(&cb_lock) &&
+    GASNET_BLOCKUNTIL((set_my_lock &&
                         cb_reply > 0)
-                        || UNSET_COND(&cb_lock));
+                        || unset_my_lock
+                        || yield_stmt);
     isDone = cb_done;
     cb_done = 0;
     cb_reply = 0;
