76173263483e25d5b2c39441663760c5660e2600
Brandon Myers <bdmyers@cs.washington.edu>
2012-02-24 11:27:29 -0800

    cleanup collective file

--- system/Collective.hpp
+++ system/Collective.hpp
@@ -15,7 +15,7 @@ int64_t collective_mult(int64_t a, int64_t b);
 #define COLL_MULT &collective_mult
 
 
-int64_t SoftXMT_collective_reduce(int64_t (*commutative_func)(int64_t, int64_t), Node home_node, int64_t myValue, int64_t initial );
+int64_t SoftXMT_collective_reduce( int64_t (*commutative_func)(int64_t, int64_t), Node home_node, int64_t myValue, int64_t initialValue );
 
 
 #endif // _COLLECTIVE_HPP
