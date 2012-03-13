2ae30989537b339d2d4ce130aee0df8cced194d6
Brandon Holt <holt.bg@gmail.com>
2012-03-13 14:54:35 -0700

    Generalized to non-multiples of 2.

--- system/ForkJoin_tests.cpp
+++ system/ForkJoin_tests.cpp
@@ -23,19 +23,36 @@ static void user_main(thread * me, void * args) {
   
   LOG(INFO) << "beginning user main... (" << SoftXMT_mynode() << ")";
   
-  size_t N = 128;
-  GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
-  
-  func_initialize a; a.base_addr = data; a.value = 0;
-  fork_join(me, &a, 0, N);
-  
-  for (size_t i=0; i<N; i++) {
-    Incoherent<int64_t>::RO c(data+i, 1);
-    VLOG(2) << i << " == " << *c;
-    BOOST_CHECK_EQUAL(i, *c);
+  {
+    size_t N = 128;
+    GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
+    
+    func_initialize a; a.base_addr = data; a.value = 0;
+    fork_join(me, &a, 0, N);
+    
+    for (size_t i=0; i<N; i++) {
+      Incoherent<int64_t>::RO c(data+i, 1);
+      VLOG(2) << i << " == " << *c;
+      BOOST_CHECK_EQUAL(i, *c);
+    }
+    SoftXMT_free(data);
+  }
+  {
+    size_t N = 101;
+    GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
+    
+    func_initialize a; a.base_addr = data; a.value = 0;
+    fork_join(me, &a, 0, N);
+    
+    for (size_t i=0; i<N; i++) {
+      Incoherent<int64_t>::RO c(data+i, 1);
+      VLOG(2) << i << " == " << *c;
+      BOOST_CHECK_EQUAL(i, *c);
+    }
+    SoftXMT_free(data);
   }
   
-  SoftXMT_free(data);
+  
   SoftXMT_signal_done();
 }
 
