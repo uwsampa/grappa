9541bf759f858c5986e6b1c44708dcab6c42b9bd
Brandon Myers <bdmyers@cs.washington.edu>
2012-03-16 20:12:02 -0700

    code complete for templated StealQueue Optimization for <T = Task>

---
+++ work_stealing/uts_gen/distr_uts/StealQueue.cpp
@@ -0,0 +1,178 @@
+#include "StealQueue.hpp"
+#include <boost/type_traits/is_same.hpp>
+       
+
+/// global task queue 
+#define MAXSTACKDEPTH 500000 
+#include "Task.hpp"
+StealQueue<Task> myStealStack(MAXSTACKDEPTH);
+
+
+        
+template <class T>
+void StealQueue<T>::registerAddress( StealQueue<T> * addr ) {
+    CHECK( !boost::is_same<T, Task>::value ) << "[T = Task] instantiation need not call this (uses global &myStealStack)";
+    staticQueueAddress = addr;
+}
+
+
+template <class T>
+void StealQueue<T>::mkEmpty( ) {
+    SET_LOCK(stackLock);
+    sharedStart = 0;
+    local  = 0;
+    top    = 0;
+    workAvail = 0;
+    UNSET_LOCK(stackLock);
+}
+
+/// local top position:  stack index of top element
+template <class T>
+uint64_t StealQueue<T>::topPosn()
+{
+  if (top <= local)
+    ss_error("ss_topPosn: empty local stack");
+  return top - 1;
+}
+
+
+/// release k values from bottom of local stack
+template <class T>
+void StealQueue<T>::release( int k ) {
+  SET_LOCK(stackLock);
+  if (top - local >= k) {
+    local += k;
+    workAvail += k;
+    nRelease++;
+  }
+  else
+    ss_error("ss_release:  do not have k vals to release");
+  UNSET_LOCK(stackLock);
+}
+
+
+/// move k values from top of shared stack into local stack
+/// return false if k vals are not avail on shared stack
+template <class T>
+int StealQueue<T>::acquire( int k ) {
+  int avail;
+  SET_LOCK(stackLock);
+  avail = local - sharedStart;
+  if (avail >= k) {
+    local -= k;
+    workAvail -= k;
+    nAcquire++;
+  }
+  UNSET_LOCK(stackLock);
+  return (avail >= k);
+}
+
+//////////////////////////////////////////////////
+// Work stealing
+/////////////////////////////////////////////////
+
+/* TODO remove the locks */
+
+int local_steal_amount;
+LOCK_T lsa_lock = LOCK_INITIALIZER;
+
+template <class T>
+struct workStealRequest_args {
+    int k;
+    Node from;
+    StealQueue<T>* victim_local;
+};
+
+struct workStealReply_args {
+    int k;
+};
+
+template <class T>
+void workStealReply_am( workStealReply_args * args,  size_t size, void * payload, size_t payload_size ) {
+    T* stolen_work = static_cast<T>( payload );
+    int k = args->k;
+
+    if (k > 0) {
+        SET_LOCK(&lsa_lock);
+        memcpy(&myStealStack.stack[myStealStack.top], stolen_work, payload_size);
+        local_steal_amount = k;
+        UNSET_LOCK(&lsa_lock);
+    } else {
+        myStealStack.nFail++;
+        SET_LOCK(&lsa_lock);
+        local_steal_amount = 0;
+        UNSET_LOCK(&lsa_lock);
+    }
+    
+}
+
+template <class T>
+void workStealRequest_am(workStealRequest_args<T> * args, size_t size, void * payload, size_t payload_size) {
+    int k = args->k;
+
+    // if using StealQueue<Task> use the global task steal stack address
+    StealQueue<T>* victimStack = (boost::is_same<T, Task>::value) ? &myStealStack : args->victimLocal;
+   
+    SET_LOCK(victimStack->stackLock);
+    
+    int victimLocal = victimStack->local;
+    int victimShared = victimStack->sharedStart;
+    int victimWorkAvail = victimStack->workAvail;
+    
+    if (victimLocal - victimShared != victimWorkAvail)
+        ss_error("handle steal request: stealStack invariant violated");
+    
+    int ok = victimWorkAvail >= k;
+    if (ok) {
+        /* reserve a chunk */
+        victimStack->sharedStart =  victimShared + k;
+        victimStack->workAvail = victimWorkAvail - k;
+    }
+    UNSET_LOCK(victimStack->stackLock);
+    
+    /* if k elts reserved, move them to local portion of our stack */
+    if (ok) {
+        Node_ptr* victimStackBase = victimStack->stack;
+        Node_ptr* victimSharedStart = victimStackBase + victimShared;
+   
+        workStealReply_args reply_args = { k };
+        SoftXMT_call_on( args->from, &workStealReply_am<T>, 
+                         &reply_args, sizeof(workStealReply_args), 
+                         victimSharedStart, k*sizeof( T ));
+    } else {
+        workStealReply_args reply_args = { 0 };
+        SoftXMT_call_on( args->from, &workStealReply_am<T>, &reply_args );
+    }
+
+}
+
+template <class T>
+int StealQueue<T>::steal_locally( Node victim, int k ) {
+
+    SET_LOCK(&lsa_lock);
+    local_steal_amount = -1;
+    UNSET_LOCK(&lsa_lock);
+
+    workStealRequest_args req_args = { k, SoftXMT_mynode(), staticQueueAddress };
+    SoftXMT_call_on( victim, &workStealRequest_am<T>, &req_args );
+
+    // steal is blocking
+    // TODO: use suspend-wake mechanism
+    while (true) {
+        SET_LOCK(&lsa_lock);
+        if (local_steal_amount != -1) {
+            break;
+        }
+        UNSET_LOCK(&lsa_lock);
+        SoftXMT_yield();
+    }
+    
+    if (local_steal_amount > 0) {
+        nSteal++;
+        top += local_steal_amount;
+    }
+    UNSET_LOCK(&lsa_lock);
+
+    return local_steal_amount;
+}
+/////////////////////////////////////////////////////////
---
+++ work_stealing/uts_gen/distr_uts/StealQueue.hpp
@@ -0,0 +1,158 @@
+
+#ifndef STEAL_QUEUE_HPP
+#define STEAL_QUEUE_HPP
+
+#include "uts.h"
+#include "SoftXMT.hpp"
+
+template <class T>
+class StealQueue {
+    private:
+        uint64_t stackSize;     /* total space avail (in number of elements) */
+        uint64_t workAvail;     /* elements available for stealing */
+        uint64_t sharedStart;   /* index of start of shared portion of stack */
+        uint64_t local;         /* index of start of local portion */
+        uint64_t top;           /* index of stack top */
+        uint64_t maxStackDepth;                      /* stack stats */ 
+        uint64_t nNodes, maxTreeDepth, nVisited, nLeaves;        /* tree stats: (num pushed, max depth, num popped, leaves)  */
+        uint64_t nAcquire, nRelease, nSteal, nFail;  /* steal stats */
+        uint64_t wakeups, falseWakeups, nNodes_last;
+        double time[SS_NSTATES], timeLast;
+        /* perf measurements */ 
+        int entries[SS_NSTATES], curState; 
+        LOCK_T* stackLock; 
+        LOCK_T* stackLock_g;
+        /* lock for manipulation of shared portion */ 
+        T* stack;       /* addr of actual
+                                  stack of nodes
+                                  in local addr
+                                  space */
+        T* stack_g; /* addr of same
+                              stack in global
+                              addr space */
+
+        StealQueue<T>* staticQueueAddress;        
+    
+    public:
+        StealQueue( uint64_t numEle ) 
+            : stackSize( numEle )
+            , maxStackDepth( 0 )
+            , nNodes( 0 ), maxTreedepth( 0 ), nVisited( 0 ), nLeaves( 0 )
+            , nAcquire( 0 ), nRelease( 0 ), nSteal( 0 ), nFail( 0 )
+            , wakeups( 0 ), falseWakeups( 0 ), nNodes_last( 0 ),
+            , staticQueueAddress( NULL ) {
+
+                uint64_t nbytes = nelts * sizeof(T);
+
+                // allocate stack in shared addr space with affinity to calling thread
+                // and record local addr for efficient access in sequel
+                stack_g = static_cast<T*>( malloc( nbytes ) );
+                stack = stack_g;
+
+                if (stack == NULL) {
+                    LOG(FATAL) << "Request for " << nbytes <<< " bytes for stealStack on thread " << SoftXMT_mynode() << " failed";
+                    ss_error("ss_init: unable to allocate space for stealstack");
+                }
+
+                stackLock = (LOCK_T*)malloc(sizeof(LOCK_T));
+                INIT_LOCK(stackLock);
+
+                mkEmpty();
+            }
+        
+        void mkEmpty(); 
+        void push( T c); 
+        T top( ); 
+        void pop( ); 
+        uint64_t topPosn( );
+        uint64_t localDepth( ); 
+        void release( int k ); 
+        int acquire( int k ); 
+        int steal_locally( Node victim, int chunkSize ); 
+        void setState( int state );
+
+        
+        /// register local address of remote steal queues
+        void registerAddress( StealQueue<T> * addr ) {
+        
+};
+
+        
+void ss_error( char *str ) {
+    CHECK(false) << str;
+}
+
+
+
+
+/// Push onto top of local stack
+template <class T>
+inline void StealQueue<T>::push( T c ) {
+  if (top >= stackSize) {
+      char buf[2048];
+      sprintf(buf, "ss_push: overflow (top:%d stackSize:%d)\n", s->top, s->stackSize);
+      ss_error(buf);
+  }
+  stack[top] = c; 
+  top++;
+  nNodes++;
+  maxStackDepth = maxint(top, maxStackDepth);
+  //s->maxTreeDepth = maxint(s->maxTreeDepth, c->height); //XXX dont want to deref c here (expensive for just a bookkeeping operation
+}
+
+
+/// local pop
+template <class T>
+inline void StealQueue<T>::pop( ) {
+  T* r;
+  if (top <= local)
+    ss_error("ss_pop: empty local stack");
+  top--;
+  nVisited++;
+  r = &(stack[top]);
+}
+
+
+/// local depth
+template <class T>
+inline int StealQueue<T>::localDepth() {
+  return (top - local);
+}
+
+
+template <class T>
+void StealQueue<T>::ss_setState( int state ) { return; }
+
+
+///////////////////////////////////////////////////
+// Pushing work
+//////////////////////////////////////////////////
+//
+// 
+/*
+struct pushWorkRequest_args {
+    int amount_pushed; 
+};
+
+void pushWorkRequest_am( pushWorkRequest_args * args, size_t size, void * payload, size_t payload_size ) {
+    Node_ptr* received_work = (Node_ptr*) payload;
+    int amount_pushed = args->amount_pushed;
+    
+    SET_LOCK(&lsa_lock);
+    memcpy(&myStealStack.stack[myStealStack.top], received_work, payload_size);
+    myStealStack.top += amount_pushed;
+    UNSET_LOCK(&lsa_lock);
+}
+
+/// Push work offline to a remote node. 
+/// Not concurrency-safe because pushes to local portion of queue,
+/// which is not lock protected for pops.
+void StealQueue::pushRemote(Node destnode, Node_ptr* work, int k) {
+    pushWorkRequest_args pargs = { k };
+    SoftXMT_call_on( destnode, &pushWorkRequest_am, &pargs, sizeof(pushWorkRequest_args), work, sizeof(Node_ptr)*k);
+}
+*/
+////////////////////////////////////////////////////
+
+
+#endif
