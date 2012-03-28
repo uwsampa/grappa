865e79ba6159cea0cf9f718ab30001cfa3c37101
Jacob Nelson <nelson@cs.washington.edu>
2012-03-28 02:52:35 -0700

    Initial checkin for multi-block global cache accesses

--- system/Cache_tests.cpp
+++ system/Cache_tests.cpp
@@ -1,7 +1,8 @@
 
 #include "Cache.hpp"
 
-
+#include "GlobalAllocator.hpp"
+#include "Delegate.hpp"
 
 #include <boost/test/unit_test.hpp>
 
@@ -65,13 +66,101 @@ void user_main( thread * me, void * args )
     buf4.block_until_released( );
   }
 
+
+  {
+    const size_t array_size = 128;
+    LOG(INFO) << "Mallocing " << array_size << " words";
+    GlobalAddress< int64_t > array = SoftXMT_typed_malloc< int64_t >( array_size );
+    
+    // write indices properly
+    LOG(INFO) << "writing indices";
+    for( int i = 0; i < array_size; ++i ) {
+      SoftXMT_delegate_write_word( array + i, i );
+    }
+
+    // check that we can read a word at a time
+    LOG(INFO) << "verifying word reads";
+    for( int i = 0; i < array_size; ++i ) {
+      LOG(INFO) << "checking element " << i;
+      Incoherent<int64_t>::RO d( array + i, 1 );
+      BOOST_CHECK_EQUAL( i, *d );
+      int64_t dd = SoftXMT_delegate_read_word( array + i );
+      BOOST_CHECK_EQUAL( i, dd );
+      BOOST_CHECK_EQUAL( dd, *d );
+    }
+
+    // check that we can read two words at a time
+    // LOG(INFO) << "verifying block reads";
+    // for( int i = 0; i < array_size; ++i ) {
+    //   Incoherent<int64_t>::RO d( array + i, 2 );
+    //   BOOST_CHECK_EQUAL( i, d[0] );
+    //   if( i+1 < array_size ) BOOST_CHECK_EQUAL( i+1, d[1] );
+    // }
+
+
+    LOG(INFO) << "verifying block reads";
+    for( int size = 1; size < array_size; ++size ) {
+      LOG(INFO) << "checking blocks of size " << size; 
+      for( int start = 0; start < array_size; ++start ) {
+        //LOG(INFO) << "checking from start " << start;
+        int actual_size = ( start + size > array_size ? array_size : start + size ) - start;
+        DVLOG(5) << "starting iteration with start:" << start 
+                 << " size:" << size 
+                 << " actual_size:" << actual_size;
+
+        DVLOG(5) << "*****************************************************************************";
+        //start:2 size:4 actual_size:4 i:2
+        if( start == 2 && size == 4 ) {
+          BOOST_CHECK_EQUAL( 2, SoftXMT_delegate_read_word( array + start + 0 ) );
+          BOOST_CHECK_EQUAL( 3, SoftXMT_delegate_read_word( array + start + 1 ) );
+          BOOST_CHECK_EQUAL( 4, SoftXMT_delegate_read_word( array + start + 2 ) );
+          BOOST_CHECK_EQUAL( 5, SoftXMT_delegate_read_word( array + start + 3 ) );
+        }
+        DVLOG(5) << "*****************************************************************************";
+
+        {
+          Incoherent<int64_t>::RW d( array + start, actual_size );
+          d.block_until_acquired();
+          DVLOG(5) << "*****************************************************************************";
+          for( int i = start; i < actual_size; ++i ) {
+            DVLOG(5) << "checking word with start:" << start 
+                     << " size:" << size 
+                     << " actual_size:" << actual_size 
+                     << " i:" << i;
+            BOOST_CHECK_EQUAL( i, d[i - start] );
+            CHECK_EQ( i, d[i - start] ) << "start:" << start 
+                                        << " size:" << size 
+                                        << " actual_size:" << actual_size 
+                                        << " i:" << i;
+	    d[i - start] = i;
+          }
+        }
+      }
+    }
+
+    // check that we can read the words that were written
+    LOG(INFO) << "verifying word writes";
+    for( int i = 0; i < array_size; ++i ) {
+      LOG(INFO) << "checking element " << i;
+      //Incoherent<int64_t>::RO d( array + i, 1 );
+      //BOOST_CHECK_EQUAL( i, *d );
+      int64_t dd = SoftXMT_delegate_read_word( array + i );
+      BOOST_CHECK_EQUAL( i, dd );
+      //BOOST_CHECK_EQUAL( dd, *d );
+    }
+
+
+    SoftXMT_free( array );
+  }
+
   SoftXMT_signal_done();
 }
 
 BOOST_AUTO_TEST_CASE( test1 ) {
 
   SoftXMT_init( &(boost::unit_test::framework::master_test_suite().argc),
-                &(boost::unit_test::framework::master_test_suite().argv) );
+                &(boost::unit_test::framework::master_test_suite().argv),
+                1 << 23);
 
   SoftXMT_activate();
 
