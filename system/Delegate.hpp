c63fa53f0dd090a22d514ea914ca7157248cc60d
Brandon Holt <bholt@cs.washington.edu>
2012-03-30 12:17:55 -0700

    First pass at delegate compare and swap.

--- system/Delegate.hpp
+++ system/Delegate.hpp
@@ -10,4 +10,6 @@ int64_t SoftXMT_delegate_read_word( GlobalAddress<int64_t> address );
 
 int64_t SoftXMT_delegate_fetch_and_add_word( GlobalAddress<int64_t> address, int64_t data );
 
+bool SoftXMT_delegate_compare_and_swap_word(GlobalAddress<int64_t> address, int64_t cmpval, int64_t newval);
+
 #endif
