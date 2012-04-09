550280bede046d683784fa0eb85ba913e57a9be1
Brandon Holt <bholt@cs.washington.edu>
2012-04-08 18:30:45 -0700

    Re-wrote forkjoin to not pass around current thread

--- system/ForkJoin_tests.cpp
+++ system/ForkJoin_tests.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_SUITE( ForkJoin_tests );
 struct func_initialize : public ForkJoinIteration {
   GlobalAddress<int64_t> base_addr;
   int64_t value;
-  void operator()(Thread * me, int64_t index) {
+  void operator()(int64_t index) {
     VLOG(2) << "called func_initialize with index = " << index;
     Incoherent<int64_t>::RW c(base_addr+index, 1);
     c[0] = value+index;
@@ -20,7 +20,7 @@ struct func_initialize : public ForkJoinIteration {
 };
 
 struct func_hello : public ForkJoinIteration {
-  void operator()(Thread * me, int64_t index) {
+  void operator()(int64_t index) {
     LOG(INFO) << "Hello from " << index << "!";
   }
 };
@@ -34,7 +34,7 @@ static void user_main(Thread * me, void * args) {
     GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
     
     func_initialize a; a.base_addr = data; a.value = 0;
-    fork_join(me, &a, 0, N);
+    fork_join(&a, 0, N);
     
     for (size_t i=0; i<N; i++) {
       Incoherent<int64_t>::RO c(data+i, 1);
@@ -48,7 +48,7 @@ static void user_main(Thread * me, void * args) {
     GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
     
     func_initialize a; a.base_addr = data; a.value = 0;
-    fork_join(me, &a, 0, N);
+    fork_join(&a, 0, N);
     
     for (size_t i=0; i<N; i++) {
       Incoherent<int64_t>::RO c(data+i, 1);
@@ -59,14 +59,14 @@ static void user_main(Thread * me, void * args) {
   }
   {
     func_hello f;
-    fork_join_custom(me, &f);
+    fork_join_custom(&f);
   }
   {
     size_t N = fLI64::FLAGS_max_forkjoin_threads_per_node * SoftXMT_nodes() * 3 + 13;
     GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
     
     func_initialize a; a.base_addr = data; a.value = 0;
-    fork_join(me, &a, 0, N);
+    fork_join(&a, 0, N);
     
     for (size_t i=0; i<N; i++) {
       Incoherent<int64_t>::RO c(data+i, 1);
