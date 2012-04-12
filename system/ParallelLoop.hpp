fe7c98828e9d0736d39784af6ee62b6812ad502d
Brandon Myers <bdmyers@cs.washington.edu>
2012-04-12 13:51:11 -0700

    fixing

--- system/ParallelLoop.hpp
+++ system/ParallelLoop.hpp
@@ -6,12 +6,14 @@
 
 static int64_t threshold = 1;
 
+/// private args will be copied to every Future
+
 template < typename ArgsStruct >
 struct parloop_args {
     int64_t start_index;
     int64_t iterations;
-    void (*loop_body)(int64_t i, ArgsStruct * sargs);
-    ArgsStruct * shared_args;
+    void (*loop_body)(int64_t, ArgsStruct *);
+    ArgsStruct private_args;
 };
 
 template < typename ArgsStruct >
@@ -19,7 +21,7 @@ static void parallel_loop_task_wrap( parloop_args< ArgsStruct > * args ) {
     parallel_loop( args->start_index,
                    args->iterations,
                    args->loop_body,
-                   args->shared_args );
+                   args->private_args ); // copy the loop body args
 }
 
 /// Parallel loop with static bounds.
@@ -27,19 +29,19 @@ static void parallel_loop_task_wrap( parloop_args< ArgsStruct > * args ) {
 /// in that writes to it may or may not be reflected in
 /// the original struct pointed to.
 template < typename ArgsStruct >
-void parallel_loop(int64_t start_index, int64_t iterations, void (*loop_body)(int64_t i, ArgsStruct * sargs), ArgsStruct * shared_args) {
+void parallel_loop(int64_t start_index, int64_t iterations, void (*loop_body)(int64_t, ArgsStruct *), ArgsStruct private_args) {
        if ( iterations == 0 ) {
            return;
        } else if ( iterations <= threshold ) {
            for ( int64_t ind=0; ind<iterations; ind++ ) {
-               loop_body( start_index+ind, shared_args );
+               loop_body( start_index+ind, &private_args );
            }
        } else {
            // spawn right half
            parloop_args< ArgsStruct > right_args = { start_index + (iterations+1)/2,
                             iterations/2,
                             loop_body,
-                            shared_args };
+                            private_args }; // copy the loop body args
 
            Future< parloop_args<ArgsStruct> >  d ( &parallel_loop_task_wrap, &right_args );
            d.asPublicTask( );
@@ -47,7 +49,7 @@ void parallel_loop(int64_t start_index, int64_t iterations, void (*loop_body)(in
            // do left half
            parallel_loop( start_index, (iterations+1)/2,
                           loop_body,
-                          shared_args);
+                          private_args); // copy the loop body args
 
            // touch
            d.touch( );
