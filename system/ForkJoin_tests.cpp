94d9c37b988ab36adb9c015088c4889eedb3139b
Brandon Holt <bholt@cs.washington.edu>
2012-04-14 00:50:34 -0700

    Fixed ForkJoin_test for user_main as task

--- system/ForkJoin_tests.cpp
+++ system/ForkJoin_tests.cpp
@@ -25,9 +25,9 @@ struct func_hello : public ForkJoinIteration {
   }
 };
 
-static void user_main(Thread * me, void * args) {
+static void user_main(int * args) {
   
-  LOG(INFO) << "beginning user main... (" << SoftXMT_mynode() << ")";
+  LOG(INFO) << "beginning user main.... (" << SoftXMT_mynode() << ")";
   
   {
     size_t N = 128;
@@ -62,12 +62,14 @@ static void user_main(Thread * me, void * args) {
     fork_join_custom(&f);
   }
   {
-    size_t N = fLI64::FLAGS_max_forkjoin_threads_per_node * SoftXMT_nodes() * 3 + 13;
+    size_t N = 128 + 13;
     GlobalAddress<int64_t> data = SoftXMT_typed_malloc<int64_t>(N);
     
     func_initialize a; a.base_addr = data; a.value = 0;
     fork_join(&a, 0, N);
-    
+   
+	VLOG(2) << "done with init";
+
     for (size_t i=0; i<N; i++) {
       Incoherent<int64_t>::RO c(data+i, 1);
       VLOG(2) << i << " == " << *c;
@@ -76,15 +78,14 @@ static void user_main(Thread * me, void * args) {
     SoftXMT_free(data);
   }
   
-  SoftXMT_signal_done();
 }
 
 BOOST_AUTO_TEST_CASE( test1 ) {
   SoftXMT_init( &(boost::unit_test::framework::master_test_suite().argc),
-               &(boost::unit_test::framework::master_test_suite().argv), 1<<14);
+               &(boost::unit_test::framework::master_test_suite().argv), 1<<20);
   SoftXMT_activate();
   
-  SoftXMT_run_user_main(&user_main, NULL);
+  SoftXMT_run_user_main(&user_main, (int*)NULL);
   
   LOG(INFO) << "finishing...";
 	SoftXMT_finish( 0 );
