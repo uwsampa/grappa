ed2c3f711e3cd9eb774b756dbcd63a7ecd4d88d5
Brandon Holt <bholt@cs.washington.edu>
2012-04-14 00:46:43 -0700

    added flags for makefile run

--- include.mk
+++ include.mk
@@ -158,10 +158,13 @@ SRUN_EXPORT_ENV_VARIABLES=--task-prolog=$(SRUN_ENVVAR_TEMP)
 # delete when done
 .INTERMEDIATE: $(SRUN_ENVVAR_TEMP)
 
+NNODE?=$(NPROC)
+PPN?=1
+
 SRUN_HOST=--partition softxmt
 SRUN_NPROC=--nodes=$(NNODE) --ntasks-per-node=$(PPN)
 
-SRUN_MPIRUN=srun --resv-ports --cpu_bind=verbose,rank --exclusive --label --kill-on-bad-exit
+SRUN_MPIRUN=srun --resv-ports --cpu_bind=verbose,rank --exclusive --label --kill-on-bad-exit $(SRUN_FLAGS)
 
 SRUN_CLEAN_FILES= .srunrc.* 
 
