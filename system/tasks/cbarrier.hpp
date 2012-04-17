95a4bd422291620e99518ffaf6517e0daf109998
Brandon Myers <bdmyers@cs.washington.edu>
2012-04-17 03:52:16 -0700

    -fixed bug: cbarrier_local_cancel causes waiting Node to wake an re-enter without decrementing num_waiting_clients -the fix just adds a bitmap so that duplicate enters from a single node are ignored until the barrier is canceled. This allows us to keep the cancel_local optimization of not notifying the home_node

--- system/tasks/cbarrier.hpp
+++ system/tasks/cbarrier.hpp
@@ -1,9 +1,12 @@
 #ifndef __CBARRIER_HPP__
 #define __CBARRIER_HPP__
 
+/// Type for Node ID. 
+#include <boost/cstdint.hpp>
+typedef int16_t Node;
 
 void cbarrier_cancel( );
-int cbarrier_wait( );
+bool cbarrier_wait( );
 void cbarrier_init( Node num_nodes );
 void cbarrier_cancel_local( );
 
