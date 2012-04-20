9ae604569caf61cb6343ae82b846a5547aa5ac73
Brandon Holt <bholt@cs.washington.edu>
2012-04-20 12:01:05 -0700

    Added stuff for wrapping bare-bones tasks with caching

--- system/Tasking.hpp
+++ system/Tasking.hpp
@@ -4,9 +4,13 @@
 
 //#include "SoftXMT.hpp"
 #include "tasks/Task.hpp"
+#include "Cache.hpp"
 
+#include <boost/type_traits/remove_pointer.hpp>
+#include <boost/typeof/typeof.hpp>
 #include <boost/static_assert.hpp>
-#define STATIC_ASSERT_SIZE_8( type ) BOOST_STATIC_ASSERT( sizeof(type) )
+
+#define STATIC_ASSERT_SIZE_8( type ) BOOST_STATIC_ASSERT( sizeof(type) == 8 )
 
 extern TaskManager * my_task_manager;
 
@@ -84,6 +88,54 @@ void SoftXMT_remote_privateTask( void (*fn_p)(T*), T args, Node target) {
   SoftXMT_call_on( target, SoftXMT_magic_identity_function(&remote_task_spawn_am<T>), &spawn_args );
   DVLOG(5) << "Sent AM to spawn private task on Node " << target;
 }
+
+///
+/// Wrapper functions for making it simpler to use the bare-bones tasking
+///
+
+/// Cache GlobalAddress argument and pass pointer to the cached copy to the wrapped function
+template< typename T, void (*F)(T*) >
+void call_with_caching(GlobalAddress<T> ptr) {
+  VLOG(5) << "caching args";
+  T args;
+  typename Incoherent<T>::RW cache(ptr, 1, &args);
+  cache.block_until_acquired();
+  F(&args);
+  cache.block_until_released();
+}
+
+/// Cache GlobalAddress argument and pass cached copy as a ref to the wrapped function
+template< typename T, void (*F)(T&) >
+void call_with_caching(GlobalAddress<T> ptr) {
+  VLOG(5) << "caching args";
+  T args;
+  typename Incoherent<T>::RW cache(ptr, 1, &args);
+  cache.block_until_acquired();
+  F(args);
+  cache.block_until_released();
+}
+
+/// Cache GlobalAddress argument and pass by value to wrapped function
+/// Also, doesn't hold onto ownership of cached value.
+/// (not sure how useful this is, but figured we'd round out the set)
+template< typename T, void (*F)(T) >
+void call_with_caching(GlobalAddress<T> ptr) {
+  VLOG(5) << "caching args";
+  T args;
+  typename Incoherent<T>::RW cache(ptr, 1, &args);
+  cache.block_until_acquired();
+  cache.block_until_released();
+  F(args);
+}
+
+/// Wrap arguments to spawn task (at call site) for tasks that used to assume caching
+#define CACHE_WRAP(fn, arg) \
+&call_with_caching<boost::remove_pointer<BOOST_TYPEOF(arg)>::type,fn>, make_global(arg)
+
+/// Declare a version of a task function that accepts a GlobalAddress to the arguments
+/// and does the caching for you.
+#define DECLARE_CACHE_WRAPPED(name, orig, T) \
+inline void name(GlobalAddress<T> a) { return call_with_caching<T,orig>(a); }
 
 #endif
 
