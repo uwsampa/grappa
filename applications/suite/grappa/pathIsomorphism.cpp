7f35bf0631da7fbeec5a5511530d2038da3823f8
Brandon Holt <bholt@cs.washington.edu>
2012-05-19 00:11:01 -0700

    Initial Grappa implementation of Suite.
    Includes reading/writing graph from checkpoint, does not include
    generating graph in Grappa yet.

---
+++ applications/suite/grappa/pathIsomorphism.cpp
@@ -0,0 +1,159 @@
+// Inspired by Ullman (1976) Subgraph-isomorphism algorithm
+// Brandon Holt
+#include "defs.hpp"
+#include <SoftXMT.hpp>
+#include <ForkJoin.hpp>
+#include <Collective.hpp>
+
+//static GlobalAddress<graphint> eV;
+//static GlobalAddress<graphint> edges;
+//static GlobalAddress<graphint> weights;
+//static GlobalAddress<color_t> marks;
+static color_t minc, maxc;
+
+static graph g;
+
+static const color_t * pattern;
+static size_t npattern;
+
+static graphint nmatches;
+
+static LocalPhaser phaser;
+
+static void checkEdgesRecursiveTask(uint64_t packed) {
+  graphint v = packed & 0xFFFFFFFF;
+  size_t poff = packed >> 32; // current pattern offset
+
+  graphint edgeRange_buf_[2];
+
+  Incoherent<graphint>::RO edgeRange(g.edgeStart+v, 2, edgeRange_buf_);
+  graphint nedges = edgeRange[1] - edgeRange[0];
+  graphint eVbuf_[nedges];
+  Incoherent<graphint>::RO ceV(g.endVertex+edgeRange[0], nedges, eVbuf_);
+  
+  for (graphint j = 0; j < nedges; j++) {
+    if (SoftXMT_delegate_read_word(g.marks+ceV[j]) == pattern[poff]) {
+      VLOG(5) << v << " -> " << ceV[j] << " " << poff << "(" << pattern[poff] << ")";
+
+      if (poff == npattern-1) {
+        nmatches++;
+      } else {
+        CHECK(ceV[j] < (1L<<32)) << "v = " << ceV[j];
+        packed = (((uint64_t)poff+1) << 32) | ceV[j];
+        phaser++;
+        SoftXMT_privateTask(&checkEdgesRecursiveTask, packed);
+      }
+    }
+  }
+  phaser.signal();
+}
+
+static void pathIsoTask(graphint v) {
+  color_t cmark_buf_;
+  
+  Incoherent<color_t>::RO cmark(g.marks+v, 1, &cmark_buf_);
+  
+  if (*cmark == *pattern) {
+    CHECK(v < (1L<<32)) << "v = " << v;
+    uint64_t packed = (((uint64_t)1) << 32) | v;
+    checkEdgesRecursiveTask(packed);
+  } else {
+    phaser.signal();
+  }
+}
+
+LOOP_FUNCTOR( pathIsomorphismFunc, nid, ((graph,g_)) ((GlobalAddress<color_t>,pattern_)) ((size_t,npattern_)) ) {
+  g = g_;
+  npattern = npattern_;
+  color_t buf_[npattern_];
+  Incoherent<color_t>::RO cpattern(pattern_, npattern_, buf_);
+  cpattern.start_acquire();
+  pattern = &(*cpattern);
+  
+  phaser.reset();
+  range_t vr = blockDist(0, g.numVertices, SoftXMT_mynode(), SoftXMT_nodes());
+  for (graphint i=vr.start; i<vr.end; i++) {
+    phaser++;
+    SoftXMT_privateTask(&pathIsoTask, i);
+  }
+  phaser.wait();
+  
+  VLOG(5) << "nmatches = " << nmatches;
+  
+  nmatches = SoftXMT_collective_reduce(&collective_add, 0, nmatches, 0);
+}
+
+/* @param g: (directed) graph to search in
+ @param vertices: pattern of vertex colors to search for (in order)
+ @return matches: list of vertices that mark the beginning of the pattern */
+graphint pathIsomorphism(graph* g, color_t* pattern, size_t npattern) {
+  pathIsomorphismFunc f(*g, make_global(pattern), npattern);
+  fork_join_custom(&f);
+  return nmatches;
+}
+
+static void markColorsTask(graphint v) {
+  graphint buf_[2];
+  Incoherent<graphint>::RO edgeRange(g.edgeStart+v, 2, buf_);
+  size_t nedges = edgeRange[1] - edgeRange[0];
+  graphint wbuf_[nedges];
+  Incoherent<graphint>::RO cw(g.intWeight+edgeRange[0], nedges, wbuf_);
+  color_t mbuf_;
+  {
+    Incoherent<color_t>::WO cmark(g.marks+v, 1, &mbuf_);
+    
+    int total = 0;
+    for (graphint j = 0; j < nedges; j++) {
+      total += cw[j];
+    }
+    *cmark = (total % (maxc-minc)) + minc;
+  }
+  phaser.signal();
+}
+
+LOOP_FUNCTOR( markColorsFunc, nid, ((graph,g_)) ((color_t,minc_)) ((color_t,maxc_)) ) {
+  g = g_;
+  minc = minc_;
+  maxc = maxc_;
+  
+  range_t vr = blockDist(0, g.numVertices, SoftXMT_mynode(), SoftXMT_nodes());
+  
+  phaser.reset();
+  
+  for (graphint i=vr.start; i<vr.end; i++) {
+    phaser++;
+    SoftXMT_privateTask(&markColorsTask, i);
+  }
+  phaser.wait();
+}
+
+/// In order to get reproducible results for pathIsomorphism, mark for 
+/// each vertex should be based on something that will be consistent 
+/// with the structure of the graph rather than the ordering or implicit 
+/// numbering of vertices.
+/// Arbitrarily choosing: sum(outgoing weights) % (maxc-minc) + minc 
+void markColors(graph * g, color_t minc, color_t maxc) {
+  markColorsFunc f(*g, minc, maxc);
+  fork_join_custom(&f);
+}
+
+//void print_match(graph *dirg, color_t *pattern, graphint startVertex) {
+//  graphint v = startVertex;
+//  color_t *c = pattern;
+//  printf("match %d: %"DFMT"(%"DFMT")", 0, v, *c);
+//  c++;
+//  while (*c != END) {
+//    graphint nextV = -1;
+//    for (graphint i=dirg->edgeStart[v]; i<dirg->edgeStart[v+1]; i++) {
+//      if (dirg->marks[dirg->endVertex[i]] == *c) {
+//        nextV = dirg->endVertex[i];
+//        break;
+//      }
+//    }
+//    assert(nextV != -1);
+//    printf(" -> %"DFMT"(%"DFMT")", nextV, *c);
+//    v = nextV;
+//    c++;
+//  }
+//  printf("\n");
+//}
---
+++ applications/suite/grappa/triangles.cpp
@@ -0,0 +1,227 @@
+// Modified from original version by Preston Briggs
+// License: (created for Cray?)
+
+#include "defs.hpp"
+#include <SoftXMT.hpp>
+#include <ForkJoin.hpp>
+#include <Collective.hpp>
+#include <iomanip>
+
+typedef GlobalAddress<graphint> Addr;
+#define read SoftXMT_delegate_read_word
+#define write SoftXMT_delegate_write_word
+
+static Addr edge;
+static Addr eV;
+static graphint ntriangles;
+static LocalPhaser phaser;
+
+#define BUFSIZE (1L<<12)
+
+// debug only
+//static graphint NV;
+//static graphint NE;
+
+static void trianglesTask(graphint v) {
+  graphint bufA_[2];
+  graphint bufB_[2];
+  
+  graphint A = v;
+  graphint B;
+  graphint _Astart;
+  graphint _Aedge;
+  graphint _Bedge;
+  graphint temp;
+  
+  Incoherent<graphint>::RO cedgeA(edge+A, 2, bufA_);
+  
+  Addr Astart = eV + cedgeA[0];
+  Addr Alimit = eV + cedgeA[1];
+  
+  while ((_Aedge = read(Astart)) <= A) ++Astart;
+  
+  while (Astart < Alimit) {
+    Addr Aedge = Astart;
+    B = _Aedge; //read(Aedge);
+    
+    Incoherent<graphint>::RO cedgeB(edge+B, 2, bufB_);
+    Addr Bedge = eV + cedgeB[0];
+    Addr Blimit = eV + cedgeB[1];
+    
+    while ((read(Bedge) <= B || read(Bedge) < A) && Bedge < Blimit) { ++Bedge; }
+    _Bedge = read(Bedge);
+    
+    while (Aedge < Alimit && Bedge < Blimit) {
+      if (_Aedge < _Bedge) {
+        _Aedge = read(++Aedge);
+      } else if (_Aedge > _Bedge) {
+        _Bedge = read(++Bedge);
+      } else {
+        VLOG(3) << "tri  " << A << " " << B << " " << _Aedge << " ";
+        ntriangles++;
+        // advance to the end of any duplicate edges
+        while (Aedge < Alimit && _Aedge == (temp = read(Aedge+1))) ++Aedge;
+        _Aedge = temp;
+        ++Aedge;
+        while (Bedge < Blimit && _Bedge == (temp = read(Bedge+1))) ++Bedge;
+        _Bedge = temp;
+        ++Bedge;
+      }
+    }
+    
+    _Astart = read(Astart);
+    while (Astart < Alimit && _Astart == (temp = read(Astart+1))) ++Astart;
+    _Aedge = temp;
+    ++Astart;
+//    while (Astart < Alimit && read(Astart+1) == read(Astart)) ++Astart;
+//    Astart++;
+  }
+  
+  phaser.signal();
+}
+
+inline void checkAndMoveCache(graphint& index, graphint& currStart, graphint& currLimit, const graphint& globalLimit, Incoherent<graphint>::RO& cache) {
+  index++;
+  if (index >= currLimit) {
+    GlobalAddress<graphint> newAddr = cache.address() + currLimit;
+    currStart += currLimit;
+    currLimit = min(BUFSIZE, globalLimit-currStart);
+    if (currLimit > 0) {
+      index = 0;
+      cache.reset(newAddr, currLimit);
+      cache.block_until_acquired();
+    }
+  }
+}
+
+static void search_children(uint64_t packed) {
+  graphint A = packed & 0xFFFFFFFF;
+  graphint Astart = packed >> 32;
+  graphint temp;
+  graphint bufRangeA[2];
+  graphint bufRangeB[2];
+  graphint bufA[BUFSIZE];
+  graphint bufB[BUFSIZE];
+  
+//  CHECK(A < NV) << A << " < " << NV;
+  
+  Incoherent<graphint>::RO rangeA(edge+A, 2, bufRangeA);
+  
+  graphint Alimit = rangeA[1] - Astart;
+  graphint astart = 0;
+  graphint alim = min(BUFSIZE, Alimit); // A current limit
+  
+//  CHECK(Astart + alim-1 < NE) << Astart << " + " << alim << " < " << NE;
+  
+  Incoherent<graphint>::RO edgesA(eV+Astart, alim, bufA);
+  
+  graphint B = edgesA[0]; // read(Astart)
+//  CHECK(B < NV) << B << " < " << NV;
+  
+  Incoherent<graphint>::RO rangeB(edge+B, 2, bufRangeB);
+  graphint Blimit = rangeB[1] - rangeB[0];
+  graphint bstart = 0;
+  graphint blim = min(BUFSIZE, Blimit); // B current limit
+  
+//  CHECK(rangeB[0] + blim-1 < NE) << rangeB[0] << " + " << blim << " < " << NE;
+  
+  Incoherent<graphint>::RO edgesB(eV+rangeB[0], blim, bufB);
+  
+  graphint ai = 0;
+  graphint bi = 0;
+#define incrA() checkAndMoveCache(ai, astart, alim, Alimit, edgesA)
+#define incrB() checkAndMoveCache(bi, bstart, blim, Blimit, edgesB)
+
+  while (bstart+bi < Blimit && edgesB[bi] <= B) incrB();
+    
+  while (astart+ai < Alimit && bstart+bi < Blimit) {
+    if (edgesA[ai] < edgesB[bi]) {
+      incrA();
+    } else if (edgesA[ai] > edgesB[bi]) {
+      incrB();
+    } else {
+//      char tmsg[64];
+//      sprintf(tmsg, "tri  %04llu %04llu %04llu \n", A, B, edgesA[ai]);
+//      VLOG(4) << tmsg;
+      ntriangles++;
+      // advance past any duplicates
+      temp = edgesA[ai];
+//      incrA();
+      while (astart+ai < Alimit && temp == edgesA[ai]) incrA();
+      temp = edgesB[bi];
+//      incrB();
+      while (bstart+bi < Blimit && temp == edgesB[bi]) incrB();
+    }
+  }
+  phaser.signal();
+}
+#undef incrA
+#undef incrB
+
+static void gen_tasks(graphint A) {
+  graphint bufA_[2];  
+  graphint temp;
+  
+  Incoherent<graphint>::RO cedgeA(edge+A, 2, bufA_);
+  
+  size_t Astart = cedgeA[0];
+  size_t Alimit = cedgeA[1];
+  
+  while ((temp = read(eV+Astart)) <= A) ++Astart;
+  
+  while (Astart < Alimit) {
+    uint64_t packed = (Astart << 32) + A;
+    phaser++;
+    SoftXMT_privateTask(&search_children, packed);
+    
+    temp = read(eV+Astart);
+    Astart++;
+    while (Astart < Alimit && temp == read(eV+Astart)) Astart++;
+  }
+  
+  phaser.signal();
+}
+
+LOOP_FUNCTOR( trianglesFunc, nid, ((graphint,NV)) ((Addr,edge_)) ((Addr,eV_)) ) {
+  edge = edge_;
+  eV = eV_;
+  ntriangles = 0;
+  
+//  NV = NV_;
+//  NE = NE_;
+  
+  range_t r = blockDist(0, NV, SoftXMT_mynode(), SoftXMT_nodes());
+  phaser.reset();
+  
+  for (graphint i=r.start; i<r.end; i++) {
+    phaser++;
+    SoftXMT_privateTask(&gen_tasks, i);
+//    SoftXMT_privateTask(&trianglesTask, i);
+  }
+  phaser.wait();
+  VLOG(3) << "ntriangles (local) = " << ntriangles;
+  
+//  ntriangles = SoftXMT_collective_reduce(&collective_add, 0, ntriangles, 0);
+  ntriangles = SoftXMT_allreduce<graphint,coll_add<graphint>,0>(ntriangles);
+}
+
+/*	Finds the number of _unique_ triangles in an undirected graph.
+ Assumes that each vertex's outgoing edge list is sorted by endVertex.
+ 
+ A triangle is 3 vertices that are connected to each other without going 
+ through any other vertices. That is:
+ 
+   A              A
+  / \   not...   / \
+ B---C          B-D-C
+ 
+ It is worth noting that this count excludes duplicate edges and self-edges 
+ that are actually present in the graph.
+ */
+graphint triangles(graph * g) {
+  
+  trianglesFunc f(g->numVertices, g->edgeStart, g->endVertex);
+  fork_join_custom(&f);
+  
+  return ntriangles;
+}
