4e7c5e3519832358fa54adb28d922500ac4d0812
Brandon Myers <bdmyers@cs.washington.edu>
2012-06-18 18:35:35 -0700

    Add env variable for run time: TAU_METRICS=LINUX_TIMERS

--- system/cluster_tau.sh
+++ system/cluster_tau.sh
@@ -4,4 +4,4 @@ export TAU_SYNCHRONIZE_CLOCKS=1
 export PROFILEDIR=`pwd`/profiles
 export TRACEDIR=`pwd`/profiles
 export TAU_VERBOSE=0
-export PATH="$PATH:/sampa/share/tau-perf/tau/x86_64/bin"
+export PATH="/sampa/share/tau-perf/tau/x86_64/bin:$PATH"
