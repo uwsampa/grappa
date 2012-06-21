b84c12ab756978b6edbc187b8ca31314095897ee
Jacob Nelson <nelson@cs.washington.edu>
2012-06-21 02:34:47 -0700

    Google sampling profiler support for multi-process jobs. Add instrumentation and make with GOOGLE_PROFILER=1 to use. See https://sites.google.com/a/cs.washington.edu/grappa/tools/profilers for more info

--- include.mk
+++ include.mk
@@ -108,7 +108,7 @@ CFLAGS+= -I$(BOOST)/include
 LDFLAGS+= -L$(BOOST)/lib64
 LD_LIBRARY_PATH:=$(LD_LIBRARY_PATH):$(BOOST)/lib
 
-GPERFTOOLS=/sampa/share/gperftools-2.0
+GPERFTOOLS=/sampa/share/gperftools-2.0-nolibunwind
 CFLAGS+= -I$(GPERFTOOLS)/include
 LDFLAGS+= -L$(GPERFTOOLS)/lib
 LD_LIBRARY_PATH:=$(LD_LIBRARY_PATH):$(GPERFTOOLS)/lib
