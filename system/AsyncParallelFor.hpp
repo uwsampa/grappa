1f6005f15c7048025955761591ddb24648e8e332
Brandon Myers <bdmyers@cs.washington.edu>
2012-06-28 00:39:51 -0700

    add a shared arg to AsynParFor

--- system/AsyncParallelFor.hpp
+++ system/AsyncParallelFor.hpp
@@ -25,4 +25,22 @@ void async_parallel_for( int64_t start, int64_t iterations ) {
   }
 }
 
+template < typename Arg,
+           void (*LoopBody)(int64_t,int64_t,GlobalAddress<Arg>),
+           void (*Spawn)(int64_t,int64_t,GlobalAddress<Arg>) >
+void async_parallel_for( int64_t start, int64_t iterations, GlobalAddress<Arg> shared_arg ) {
+  VLOG(5) << "[" << start << "," << start+iterations << ")";
+  if ( iterations == 0 ) {
+    return;
+  } else if ( iterations <= FLAGS_async_par_for_threshold ) {
+    LoopBody ( start, iterations, shared_arg );
+  } else {
+    // asyncronous spawn right half
+    Spawn ( start + (iterations+1)/2, iterations/2, shared_arg );
+   
+    // go down left half
+    async_parallel_for<Arg,LoopBody,Spawn>( start, (iterations+1)/2, shared_arg );
+  }
+}
+
 #endif // __ASYNC_PARALLEL_FOR__
