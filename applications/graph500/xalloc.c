00ff9cd63b5b4b38e0f9dbdda619f54eabbf2d1a
Brandon Holt <bholt@cs.washington.edu>
2012-07-12 16:00:21 -0700

    Don't know why PATH_MAX just started complaining, but fixed.

--- applications/graph500/xalloc.c
+++ applications/graph500/xalloc.c
@@ -136,7 +136,7 @@ void *
 xmalloc_large_ext (size_t sz)
 {
 #if !defined(__MTA__)&&defined(USE_MMAP_LARGE_EXT)
-	char extname[PATH_MAX+1];
+	char extname[4096+1];
 	char *tmppath;
 	void *out;
 	int fd, which;
