e4724652625e72e5e03403946108c6101d96a5fa
Brandon Holt <bholt@cs.washington.edu>
2012-09-28 16:19:05 -0700

    A bunch of comments (AsyncParallelFor*, sort, suite)

--- system/AsyncParallelFor.cpp
+++ system/AsyncParallelFor.cpp
@@ -1,3 +1,6 @@
 #include "AsyncParallelFor.hpp"
 
+/// When recursively breaking up for loop iterations, stop breaking it up and creating 
+/// new tasks when this threshold is reached.
 DEFINE_int64( async_par_for_threshold, 1, "Serialization amount for asynchronous parallel loops" );
+
--- system/AsyncParallelFor.hpp
+++ system/AsyncParallelFor.hpp
@@ -9,6 +9,8 @@
 #define ASYNC_PAR_FOR_DEFAULT -1
 DECLARE_int64( async_par_for_threshold );
 
+/// Version of async_parallel_for that doesn't take a shared argument. @see async_parallel_for()
+/// TODO: just call the async_parallel_for below rather than duplicating code
 template < void (*LoopBody)(int64_t,int64_t),
            void (*Spawn)(int64_t,int64_t),
            int64_t Threshold >
@@ -37,6 +39,7 @@ void async_parallel_for( int64_t start, int64_t iterations ) {
 
 #include "Addressing.hpp"
 
+/// Parallel recursive loop decomposition similar to the Cilk_for approach to loops.
 template < typename Arg,
            void (*LoopBody)(int64_t,int64_t,Arg),
            void (*Spawn)(int64_t,int64_t,Arg),
