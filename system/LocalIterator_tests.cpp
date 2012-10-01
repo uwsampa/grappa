a95e05469eeb89bd1de24ffdc0b00caaf02ee72b
Jacob Nelson <nelson@cs.washington.edu>
2012-10-01 01:29:58 -0700

    Add some more copyright headers for release

--- system/LocalIterator_tests.cpp
+++ system/LocalIterator_tests.cpp
@@ -1,3 +1,10 @@
+
+// Copyright 2010-2012 University of Washington. All Rights Reserved.
+// LICENSE_PLACEHOLDER
+// This software was created with Government support under DE
+// AC05-76RL01830 awarded by the United States Department of
+// Energy. The Government has certain rights in the software.
+
 #include <boost/test/unit_test.hpp>
 
 #include <SoftXMT.hpp>
