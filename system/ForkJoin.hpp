b9e0b8bd2a2779e35dd2b1de6c8d6b2e9fa80ca8
Jacob Nelson <nelson@cs.washington.edu>
2012-10-01 13:59:20 -0700

    header for one last file

--- system/ForkJoin.hpp
+++ system/ForkJoin.hpp
@@ -1,3 +1,10 @@
+
+// Copyright 2010-2012 University of Washington. All Rights Reserved.
+// LICENSE_PLACEHOLDER
+// This software was created with Government support under DE
+// AC05-76RL01830 awarded by the United States Department of
+// Energy. The Government has certain rights in the software.
+
 #ifndef __FORK_JOIN_HPP__
 #define __FORK_JOIN_HPP__
 
