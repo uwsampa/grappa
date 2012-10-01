6499e2a7abf089b6433cda14988a7de6ab54667a
Jacob Nelson <nelson@cs.washington.edu>
2012-10-01 14:31:22 -0700

    a few last headers

--- tools/build_deps.sh
+++ tools/build_deps.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Copyright 2010-2012 University of Washington. All Rights Reserved.
+# LICENSE_PLACEHOLDER
+# This software was created with Government support under DE
+# AC05-76RL01830 awarded by the United States Department of
+# Energy. The Government has certain rights in the software.
+
+# Grappa contains 3rd party code. Those 3rd party codes are made
+# available with original copyright notices and license terms included.
+
 cd GASNet-1.18.2
 #make distclean
 ./configure --prefix=`pwd`/../built_deps --enable-segment-everything CC='gcc -g '
