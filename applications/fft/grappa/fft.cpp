1528116afecf6c3114a2e3c966e013b68fbbe616
Brandon Holt <bholt@cs.washington.edu>
2012-11-13 19:45:08 -0800

    More work on FFT, need forall_local to support extra argument.

--- applications/fft/grappa/fft.cpp
+++ applications/fft/grappa/fft.cpp
@@ -12,14 +12,25 @@ using std::complex;
 
 typedef GlobalAddress< complex<double> > complex_addr;
 
+static int64_t N;
+static int64_t log2N;
+
 // single iteration, for single element
 void fft_elt_iter(int64_t i, complex<double> * vbase, const int64_t& m) {
   const complex<double> v = vbase[i];
   
+
 }
 
-void fft(complex_addr vec, int64_t nelem) {
-  
-  forall_local<complex<double>,split_vector>(vec, nelem);
+LOOP_FUNCTOR( setup_globals, ((int64_t,_log2N)) ) {
+  log2N = _log2N;
+  N = 1L<<log2N;
+}
 
+void fft(complex_addr vec, int64_t log2N) {
+  { setup_globals f(log2N); fork_join_custom(&f); }
+
+  for (int m=0; m<log2N; m++) {
+    forall_local<complex<double>,fft_elt_iter>(vec, N, m);
+  }
 }
