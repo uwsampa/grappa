e8672a9e63a7f49e2ff20ee1d37391cfe842187a
Brandon Holt <bholt@cs.washington.edu>
2012-11-13 19:19:08 -0800

    Started implementing fft as forall_local (intermediate)

--- applications/fft/grappa/fft.cpp
+++ applications/fft/grappa/fft.cpp
@@ -8,6 +8,18 @@
 #include <complex>
 using std::complex;
 
-void fft(GlobalAddress< complex<double> > v, int64_t nelem) {
+#include <ForkJoin.hpp>
+
+typedef GlobalAddress< complex<double> > complex_addr;
+
+// single iteration, for single element
+void fft_elt_iter(int64_t i, complex<double> * vbase, const int64_t& m) {
+  const complex<double> v = vbase[i];
   
+}
+
+void fft(complex_addr vec, int64_t nelem) {
+  
+  forall_local<complex<double>,split_vector>(vec, nelem);
+
 }
