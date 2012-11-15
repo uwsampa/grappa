0ae2f352b0ca0bace9c0ac7d841425c35cd36bef
Brandon Myers <bdmyers@cs.washington.edu>
2012-11-15 12:58:39 -0800

    fix bug in run script with nnodes, change to ppn/nnode

--- applications/pagerank/runexps.rb
+++ applications/pagerank/runexps.rb
@@ -6,7 +6,18 @@ db = "spmv.db"
 table = :spmv
 
 
-cmd = "make mpi_run TARGET=sparsematrix_graph.exe PPN=%{procs_per_node} NPROC=%{num_places} GARGS='--num_starting_workers=%{num_threads} --steal=1 --chunk_size=%{chunk_size} --aggregator_autoflush_ticks=%{autoflush_ticks} --periodic_poll_ticks=%{poll_period} --nnz_factor=%{nnz_factor} --logN=%{logN}'"
+cmd = "make mpi_run TARGET=spmv_mult.exe \
+                    PPN=%{ppn} \
+                    NNODE=%{nnode} \
+            GARGS='--num_starting_workers=%{num_threads} \
+                   --steal=1 \
+                   --chunk_size=%{chunk_size} \
+                   --aggregator_autoflush_ticks=%{autoflush_ticks} \
+                   --async_par_for_threshold=%{async_par_for_threshold} \
+                   --periodic_poll_ticks=%{poll_period} \
+                   --nnz_factor=%{nnz_factor} \
+                   --logN=%{logN} \
+                   --row_distribute=%{row_distribute}'"
 
 
 params = {
@@ -15,12 +26,14 @@ params = {
     trial: [1,2,3],
     poll_period: [20000],
     autoflush_ticks: [2000000 ],
-    procs_per_node: [4,5],#[12,11,10,9,8,7,6,5,4,3,2,1],#[4, 3, 5, 6],# 7, 8],
-    num_places: [12], #[12],# [8,4],#,12],#,12,10],
-    num_procs: expr('num_places * procs_per_node'),
-    nnz_factor: [6,10,16],
-    logN: [6,16,17,18],
-    doSteal: [1]
+    ppn: [4],#[12,11,10,9,8,7,6,5,4,3,2,1],#[4, 3, 5, 6],# 7, 8],
+    nnode: [12], #[12],# [8,4],#,12],#,12,10],
+    nproc: expr('nnode * ppn'),
+    nnz_factor: [4],#[16,4,10,20,6,100],
+    logN: [21],#[6,16,17,18,19,20,21,22,23,24,25],
+    doSteal: [1],
+    async_par_for_threshold: [2],
+    row_distribute: ['true']
 }
 
 parser = lambda{ |cmdout|
