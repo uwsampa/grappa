4efe56ef3524755ff3122fd698131be7678d4c33
Jacob Nelson <nelson@cs.washington.edu>
2012-12-04 11:37:57 -0800

    add initial RDMA aggregator test

---
+++ system/RDMAAggregator_tests.cpp
@@ -0,0 +1,37 @@
+
+// Copyright 2010-2012 University of Washington. All Rights Reserved.
+// LICENSE_PLACEHOLDER
+// This software was created with Government support under DE
+// AC05-76RL01830 awarded by the United States Department of
+// Energy. The Government has certain rights in the software.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Grappa.hpp"
+#include "RDMAAggregator.hpp"
+
+BOOST_AUTO_TEST_SUITE( RDMAAggregator_tests );
+
+
+void user_main( void * args ) 
+{
+  auto m = Grappa::scopedMessage( 0, []{ std::cout << "Test message" << std::endl; } );
+  global_rdma_aggregator.enqueue( &m );
+  global_rdma_aggregator.flush( 0 );
+}
+
+BOOST_AUTO_TEST_CASE( test1 ) {
+
+  Grappa_init( &(boost::unit_test::framework::master_test_suite().argc),
+	       &(boost::unit_test::framework::master_test_suite().argv),
+	       (1L << 20) );
+
+  Grappa_activate();
+
+  Grappa_run_user_main( &user_main, (void*)NULL );
+
+  Grappa_finish( 0 );
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
