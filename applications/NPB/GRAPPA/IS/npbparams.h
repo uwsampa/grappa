db006a88fa4fe2356a9b75d5139a9e4cd5a72536
Brandon Holt <bholt@cs.washington.edu>
2013-01-15 23:38:57 -0800

    Start with copy of MPI version.

---
+++ applications/NPB/GRAPPA/IS/npbparams.h
@@ -0,0 +1,15 @@
+#define CLASS 'A'
+#define NUM_PROCS 32
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "11 Sep 2012"
+#define NPBVERSION "3.3"
+#define MPICC "cc"
+#define CFLAGS "-O3 -g"
+#define CLINK "$(MPICC)"
+#define CLINKFLAGS "-O3"
+#define CMPI_LIB "-L/usr/lib64/openmpi/lib -lmpi"
+#define CMPI_INC "-I/usr/include/openmpi-x86_64"
