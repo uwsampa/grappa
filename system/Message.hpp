051ee643385c60151dd9d7e41a224676d6fb685f
Jacob Nelson <nelson@cs.washington.edu>
2013-01-16 15:28:23 -0800

    Improvements to scoped lambda-based messages. Currently they don't actually send anything. Future commits will include support for legacy aggregator and new aggregator.

--- system/Message.hpp
+++ system/Message.hpp
@@ -10,61 +10,77 @@
 
 #include "MessageBase.hpp"
 
+#include <glog/logging.h>
+
 namespace Grappa {
 
   /// @addtogroup Communication
   /// @{
 
-  /// A standard message. Storage is internal. Destrutor blocks until message is sent.
-  /// Best used through @ref scopedMessage function.
+  /// A standard message. Storage is internal. Destructor blocks until message is sent.
+  /// Best used through @ref message function.
   template< typename T >
   class Message : public Grappa::impl::MessageBase {
   private:
-    T payload_;                  ///< Storage for message payload.
+    T storage_;                  ///< Storage for message contents.
     
   public:
-    Message();                   ///< Not allowed.
-    Message( const Message& m ); ///< Not allowed.
-
-
     /// Construct a message.
-    /// @param dest ID of destination core.
-    /// @param t Payload of message to send.
-    inline Message( Core dest, T t )
-      : MessageBase( dest )
-      , payload_( t )
+    inline Message( )
+      : MessageBase()
+      , storage_()
     { }
     
-    // Message( const Message<T>& m t )
-    //   : next_( NULL )
-    //   , payload_( t )
-    //   , is_enqueued_( false )
-    //   , is_sent_( false )
-    // { }
+    /// Construct a message.
+    /// @param dest ID of destination core.
+    /// @param t Contents of message to send.
+    inline Message( Core dest, T t )
+      : MessageBase( dest )
+      , storage_( t )
+    { }
 
-    // Message()
-    // 	: payload_( NULL )
-    // 	, next_( NULL )
-    // 	, is_enqueued_( false )
-    // 	, is_sent_( false )
-    // { }
+    /// Default copy constructor is fine.
+    Message( const Message& m ) = delete; ///< Not allowed.
+    Message operator=( const Message& m ) = delete;         ///< Not allowed.
 
-    // operator T*() {
-    // 	return &payload_;
-    // }
+    ///
+    /// for Messages with modifiable contents. Don't use with lambdas.
+    ///
+
+    /// Access message contents.
+    inline T& operator*() {
+      return storage_;
+    }
     
-    /// Deserialize and call one of these messages from a buffer.
-    /// @param t address of message functor/payload in buffer
-    /// @return address of byte following message functor/payload in buffer
+    /// Access message contents.
+    inline T* operator->() {
+      return &storage_;
+    }
+    
+    /// How much storage do we need to send this message?
+    const size_t size( ) const {
+      return sizeof( &deserialize_and_call ) + sizeof( T );
+    }
+
+
+    ///
+    /// These are used by the aggregator to send and receive messages.
+    ///
+
+    /// Deserialize and call one of these messages from a buffer. This
+    /// is run on the remote machine.
+    ///
+    /// @param t address of message functor/contents in buffer
+    /// @return address of byte following message functor/contents in buffer
     static char * deserialize_and_call( char * t ) {
-      // DVLOG(5) << "Deserializing message";
+      DVLOG(5) << "Deserializing message";
       T * obj = reinterpret_cast< T * >( t );
       (*obj)();
       return t + sizeof( T );
     }
 
     /// Copy this message into a buffer.
-    char * serialize_to( char * p, size_t max_size ) {
+    virtual char * serialize_to( char * p, size_t max_size ) {
       // copy deserialization function pointer
       auto fp = &deserialize_and_call;
       if( sizeof( fp ) + sizeof( T ) > max_size ) {
@@ -78,8 +94,8 @@ namespace Grappa {
 	*(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( fp );
 	p += sizeof( fp );
 	
-	// copy payload
-	std::memcpy( p, &payload_, sizeof(payload_) );
+	// copy contents
+	std::memcpy( p, &storage_, sizeof(storage_) );
 	
 	//DVLOG(5) << "serialized message of size " << sizeof(fp) + sizeof(T);
 	
@@ -88,33 +104,485 @@ namespace Grappa {
       }
     }
 
-    size_t size( ) {
-      return sizeof( &deserialize_and_call ) + sizeof( T );
+  };
+
+
+  /// A message with dynamic payload. Storage for message contents is
+  /// internal, but payload is stored externally. Destructor blocks
+  /// until message is sent. Best used through @ref message function.
+  template< typename T >
+  class PayloadMessage : public Grappa::impl::MessageBase {
+  private:
+    T storage_;                  ///< Storage for message contents.
+    
+    void * payload_;
+    size_t payload_size_;
+
+  public:
+    /// Construct a message.
+    inline PayloadMessage( )
+      : MessageBase()
+      , storage_()
+      , payload_( nullptr )
+      , payload_size_( 0 )
+    { }
+    
+    /// Construct a message.
+    /// @param dest ID of destination core.
+    /// @param t Contents of message to send.
+    inline PayloadMessage( Core dest, T t, void * payload, size_t payload_size )
+      : MessageBase( dest )
+      , storage_( t )
+      , payload_( payload )
+      , payload_size_( payload_size )
+    { }
+
+    // /// Construct a message and enqueue to be sent immediately.
+    // /// @param dest ID of destination core.
+    // /// @param t Contents of message to send.
+    // inline PayloadMessage Send( Core dest, T t )
+    //   : MessageBase( dest )
+    //   , storage_( t )
+    // { 
+    //   LOG(FAIL) << "Probably doesn't work properly yet.";
+    //   send();
+    // }
+
+    /// Default copy constructor is fine.
+    //PayloadMessage( const PayloadMessage& m ) = delete; ///< Not allowed.
+    PayloadMessage operator=( const PayloadMessage& m ) = delete;         ///< Not allowed.
+
+    ///
+    /// for Messages with modifiable contents. Don't use with lambdas.
+    ///
+
+    /// Access message contents.
+    inline T& operator*() {
+      return storage_;
+    }
+    
+    /// Access message contents.
+    inline T* operator->() {
+      return &storage_;
+    }
+
+
+
+    /// How much storage do we need to send this message?
+    const size_t size( ) const {
+      return sizeof( &deserialize_and_call ) + sizeof( T ) + sizeof( int16_t ) + payload_size_;
+    }
+
+
+    ///
+    /// These are used by the aggregator to send and receive messages.
+    ///
+
+    /// Deserialize and call one of these messages from a buffer. This
+    /// is run on the remote machine.
+    ///
+    /// @param t address of message functor/contents in buffer
+    /// @return address of byte following message functor/contents in buffer
+    static char * deserialize_and_call( char * t ) {
+      // DVLOG(5) << "Deserializing message";
+      T * obj = reinterpret_cast< T * >( t );
+      t += sizeof( T );
+
+      int16_t payload_size = *(reinterpret_cast< int16_t* >(t));
+      t += sizeof( int16_t );
+
+      (*obj)( t, payload_size );
+
+      return t + payload_size;
+    }
+
+    /// Copy this message into a buffer.
+    virtual char * serialize_to( char * p, size_t max_size ) {
+      // copy deserialization function pointer
+      auto fp = &deserialize_and_call;
+      if( sizeof( fp ) + sizeof( T ) > max_size ) {
+	std::cout << "break" << std::endl;
+	return p;
+      } else {
+	// // turn into 2D pointer
+	// auto gfp = make_global( fp, destination_ );
+	// // write to buffer
+	// *(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( gfp );
+	*(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( fp );
+	p += sizeof( fp );
+	
+	// copy contents
+	std::memcpy( p, &storage_, sizeof(storage_) );
+	p += sizeof( storage_ );
+
+	*(reinterpret_cast< int16_t* >(p)) = static_cast< int16_t >( payload_size_ );
+	p += sizeof( int16_t );
+
+	std::memcpy( p, payload_, payload_size_);
+
+	// return pointer following message
+	return p + payload_size_;
+      }
     }
 
   };
+
+  /// A message with the contents stored outside the object. Destructor blocks until message is sent.
+  /// Best used through @ref message function.
+  template< typename T >
+  class ExternalMessage : public Grappa::impl::MessageBase {
+  private:
+    T * pointer_;                ///< Pointer to message contents.
+
+  public:
+    /// Construct a message.
+    inline ExternalMessage( )
+      : MessageBase()
+      , pointer_( nullptr )
+    { }
+    
+    /// Construct a message.
+    /// @param dest ID of destination core.
+    /// @param t Pointer to contents of message to send.
+    inline ExternalMessage( Core dest, T * t )
+      : MessageBase( dest )
+      , pointer_( t )
+    { }
+
+    // /// Construct a message and enqueue to be sent immediately.
+    // /// @param dest ID of destination core.
+    // /// @param t Contents of message to send.
+    // inline ExternalMessage Send( Core dest, T * t )
+    //   : MessageBase( dest )
+    //   , pointer_( t )
+    // { 
+    //   LOG(FAIL) << "Probably doesn't work properly yet.";
+    //   send();
+    // }
+
+    /// Default copy constructor is fine.
+    //ExternalMessage( const ExternalMessage& m ) = delete; ///< Not allowed.
+    ExternalMessage operator=( const ExternalMessage& m ) = delete;         ///< Not allowed.
+
+    ///
+    /// for Messages with modifiable contents. Don't use with lambdas.
+    ///
+
+    /// Access message contents.
+    inline T&  operator*() {
+      return *pointer_;
+    }
+    
+    /// Access message contents.
+    inline T* operator->() {
+      return pointer_;
+    }
+
+
+
+    /// How much storage do we need to send this message?
+    const size_t size( ) const {
+      return sizeof( &deserialize_and_call ) + sizeof( T );
+    }
+
+
+    ///
+    /// These are used by the aggregator to send and receive messages.
+    ///
+
+    /// Deserialize and call one of these messages from a buffer. This
+    /// is run on the remote machine.
+    ///
+    /// @param t address of message functor/contents in buffer
+    /// @return address of byte following message functor/contents in buffer
+    static char * deserialize_and_call( char * t ) {
+      // DVLOG(5) << "Deserializing message";
+      T * obj = reinterpret_cast< T * >( t );
+      (*obj)();
+      return t + sizeof( T );
+    }
+
+    /// Copy this message into a buffer.
+    virtual char * serialize_to( char * p, size_t max_size ) {
+      // copy deserialization function pointer
+      auto fp = &deserialize_and_call;
+      if( sizeof( fp ) + sizeof( T ) > max_size ) {
+	std::cout << "break" << std::endl;
+	return p;
+      } else {
+	// // turn into 2D pointer
+	// auto gfp = make_global( fp, destination_ );
+	// // write to buffer
+	// *(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( gfp );
+	*(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( fp );
+	p += sizeof( fp );
+	
+	// copy contents
+	std::memcpy( p, pointer_, sizeof(T) );
+	
+	//DVLOG(5) << "serialized message of size " << sizeof(fp) + sizeof(T);
+	
+	// return pointer following message
+	return p + sizeof( T );
+      }
+    }
+
+  };
+
+
+  /// A message with dynamic payload. Storage for message contents and
+  /// payload is external. Destructor blocks until message is
+  /// sent. Best used through @ref message function.
+  template< typename T >
+  class ExternalPayloadMessage : public Grappa::impl::MessageBase {
+  private:
+    T * pointer_;                  ///< Pointer to message contents.
+    
+    void * payload_;
+    size_t payload_size_;
+
+  public:
+    /// Construct a message.
+    inline ExternalPayloadMessage( )
+      : MessageBase()
+      , pointer_( nullptr )
+      , payload_( nullptr )
+      , payload_size_( 0 )
+    { }
+    
+    /// Construct a message.
+    /// @param dest ID of destination core.
+    /// @param t Contents of message to send.
+    inline ExternalPayloadMessage( Core dest, T * t, void * payload, size_t payload_size )
+      : MessageBase( dest )
+      , pointer_( t )
+      , payload_( payload )
+      , payload_size_( payload_size )
+    { }
+
+    // /// Construct a message and enqueue to be sent immediately.
+    // /// @param dest ID of destination core.
+    // /// @param t Contents of message to send.
+    // inline PayloadMessage Send( Core dest, T t )
+    //   : MessageBase( dest )
+    //   , storage_( t )
+    // { 
+    //   LOG(FAIL) << "Probably doesn't work properly yet.";
+    //   send();
+    // }
+
+    /// Default copy constructor is fine.
+    //ExternalPayloadMessage( const ExternalPayloadMessage& m ) = delete; ///< Not allowed.
+    ExternalPayloadMessage operator=( const ExternalPayloadMessage& m ) = delete;         ///< Not allowed.
+
+    ///
+    /// for Messages with modifiable contents. Don't use with lambdas.
+    ///
+
+    /// Access message contents.
+    inline T& operator*() {
+      return *pointer_;
+    }
+    
+    /// Access message contents.
+    inline T* operator->() {
+      return pointer_;
+    }
+
+
+
+    /// How much storage do we need to send this message?
+    virtual const size_t size( ) const {
+      return sizeof( &deserialize_and_call ) + sizeof( T ) + sizeof( int16_t ) + payload_size_;
+    }
+
+
+
+    ///
+    /// These are used by the aggregator to send and receive messages.
+    ///
+
+    /// Deserialize and call one of these messages from a buffer. This
+    /// is run on the remote machine.
+    ///
+    /// @param t address of message functor/contents in buffer
+    /// @return address of byte following message functor/contents in buffer
+    static char * deserialize_and_call( char * t ) {
+      T * obj = reinterpret_cast< T * >( t );
+      t += sizeof( T );
+
+      int16_t payload_size = *(reinterpret_cast< int16_t* >(t));
+      t += sizeof( int16_t );
+
+      (*obj)( t, payload_size );
+
+      return t + payload_size;
+    }
+
+    /// Copy this message into a buffer.
+    virtual char * serialize_to( char * p, size_t max_size ) {
+      // copy deserialization function pointer
+      auto fp = &deserialize_and_call;
+      if( sizeof( fp ) + sizeof( T ) > max_size ) {
+	std::cout << "break" << std::endl;
+	return p;
+      } else {
+	// // turn into 2D pointer
+	// auto gfp = make_global( fp, destination_ );
+	// // write to buffer
+	// *(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( gfp );
+	*(reinterpret_cast< intptr_t* >(p)) = reinterpret_cast< intptr_t >( fp );
+	p += sizeof( fp );
+	
+	// copy contents
+	std::memcpy( p, pointer_, sizeof(T) );
+	p += sizeof(T);
+
+	*(reinterpret_cast< int16_t* >(p)) = static_cast< int16_t >( payload_size_ );
+	p += sizeof( int16_t );
+
+	std::memcpy( p, payload_, payload_size_);
+
+	// return pointer following message
+	return p + payload_size_;
+      }
+    }
+
+  };
+
+
+
+
+
 
   /// Construct a message allocated on the stack. Can be used with
   /// lambdas, functors, or function pointers.
   /// Example:
   /// @code 
   ///   // using a lambda
-  ///   auto m1 = scopedMessage( destination, [] { blah(); } );
+  ///   auto m1 = Grappa::message( destination, [] { blah(); } );
   ///
   ///   // using a functor
   ///   class Functor { ... };
-  ///   auto m2 = scopedMessage( destination, Functor( args ) );
+  ///   auto m2 = Grappa::message( destination, Functor( args ) );
   ///
   ///   // using a function pointer (via std::bind functor)
   ///   void foo( int bar, double baz ) { blah(); }
-  ///   auto m3 = scopedMessage( destination, std::bind( &foo, 1, 2.3 ) );
+  ///   auto m3 = Grappa::message( destination, std::bind( &foo, 1, 2.3 ) );
   /// @endcode
   /// @param dest Core to which message will be delivered
   /// @param t Functor or function pointer that will be called on destination core
   /// @return Message object
   template< typename T >
-  inline Message<T> scopedMessage( Core dest, T t ) {
+  inline Message<T> message( Core dest, T t ) {
     return Message<T>( dest, t );
+  }
+
+  /// Message with payload
+  template< typename T >
+  inline PayloadMessage<T> message( Core dest, T t, void * payload, size_t payload_size ) {
+    return PayloadMessage<T>( dest, t, payload, payload_size );
+  }
+
+  /// Message with contents stored outside object
+  template< typename T >
+  inline ExternalMessage<T> message( Core dest, T * t ) {
+    return ExternalMessage<T>( dest, t );
+  }
+
+  /// Message with contents stored outside object as well as payload
+  template< typename T >
+  inline ExternalPayloadMessage<T> message( Core dest, T * t, void * payload, size_t payload_size ) {
+    return ExternalPayloadMessage<T>( dest, t, payload, payload_size );
+  }
+
+
+
+
+
+  template< typename T >
+  class SendMessage : public Message<T> {
+  public:
+    /// Construct a message and enqueue to be sent immediately.
+    /// @param dest ID of destination core.
+    /// @param t Contents of message to send.
+    inline SendMessage( Core dest, T t )
+      : Message<T>( dest, t )
+    { 
+      Message<T>::send();
+    }
+  };
+
+  template< typename T >
+  class SendPayloadMessage : public PayloadMessage<T> {
+  public:
+    /// Construct a message and enqueue to be sent immediately.
+    /// @param dest ID of destination core.
+    /// @param t Contents of message to send.
+    /// @param payload Pointer to payload
+    /// @param payload_size Size of payload
+    inline SendPayloadMessage( Core dest, T t, void * payload, size_t payload_size )
+      : PayloadMessage<T>( dest, t, payload, payload_size )
+    { 
+      PayloadMessage<T>::send();
+    }
+  };
+
+  template< typename T >
+  class SendExternalMessage : public ExternalMessage<T> {
+  public:
+    /// Construct a message and enqueue to be sent immediately.
+    /// @param dest ID of destination core.
+    /// @param t Pointer to contents of message to send.
+    inline SendExternalMessage( Core dest, T * t )
+      : ExternalMessage<T>( dest, t )
+    { 
+      ExternalMessage<T>::send();
+    }
+  };
+
+  template< typename T >
+  class SendExternalPayloadMessage : public ExternalPayloadMessage<T> {
+  public:
+    /// Construct a message and enqueue to be sent immediately.
+    /// @param dest ID of destination core.
+    /// @param t Pointer to contents of message to send.
+    /// @param payload Pointer to payload
+    /// @param payload_size Size of payload
+    inline SendExternalPayloadMessage( Core dest, T * t, void * payload, size_t payload_size )
+      : ExternalPayloadMessage<T>( dest, t, payload, payload_size )
+    { 
+      ExternalPayloadMessage<T>::send();
+    }
+  };
+
+
+  /// Same as message, but immediately enqueued to be sent.
+  template< typename T >
+  inline Message<T> send_message( Core dest, T t ) {
+    LOG(ERROR) << "Probably doesn't work properly yet.";
+    return SendMessage<T>( dest, t );
+  }
+
+  /// Message with payload, immediately enqueued to be sent.
+  template< typename T >
+  inline PayloadMessage<T> send_message( Core dest, T t, void * payload, size_t payload_size ) {
+    LOG(ERROR) << "Probably doesn't work properly yet.";
+    return SendPayloadMessage<T>( dest, t );
+  }
+
+  /// Message with contents stored outside object, immediately enqueued to be sent.
+  template< typename T >
+  inline ExternalMessage<T> send_message( Core dest, T * t ) {
+    LOG(ERROR) << "Probably doesn't work properly yet.";
+    return SendExternalMessage<T>( dest, t );
+  }
+
+  /// Message with contents stored outside object as well as payload, immediately enqueued to be sent.
+  template< typename T >
+  inline ExternalPayloadMessage<T> send_message( Core dest, T * t, void * payload, size_t payload_size ) {
+    LOG(ERROR) << "Probably doesn't work properly yet.";
+    return SendExternalPayloadMessage<T>( dest, t, payload, payload_size );
   }
 
   /// @}
--- system/MessageBase.hpp
+++ system/MessageBase.hpp
@@ -23,31 +23,34 @@ namespace Grappa {
     /// This is a virtual class.
     class MessageBase {
     private:
-      MessageBase * next_; ///< what's the next message in the list of messages to be sent? 
-      bool is_enqueued_;   ///< Have we been added to the send queue?
-      bool is_sent_;       ///< Is our payload no longer needed?
-      Core destination_;   ///< What core is this message aimed at?
+      MessageBase * next_;     ///< what's the next message in the list of messages to be sent? 
+      MessageBase * prefetch_; ///< what's the next message to prefetch?
 
-      //      ConditionVariable cv_; ///< condition variable for sleep/wake
+      Core destination_;       ///< What core is this message aimed at?
+
+      bool is_enqueued_;       ///< Have we been added to the send queue?
+      bool is_sent_;           ///< Is our payload no longer needed?
+      ConditionVariable cv_;   ///< condition variable for sleep/wake
       
       friend class RDMAAggregator;
 
-    public:
-      MessageBase( Core dest )
-	: next_( NULL )
-	, is_enqueued_( false )
-	, is_sent_( false )
-	, destination_( dest )
-	  //	, cv_()
-      { }
 
-      virtual ~MessageBase() {
-	block_until_sent();
+      /// Active message handler to support sending messages through old aggregator
+      static void legacy_send_message_am( char * buf, size_t size, void * payload, size_t payload_size ) {
+        Grappa::impl::MessageBase::deserialize_buffer( static_cast< char * >( buf ), size );
       }
 
-      inline void block_until_sent() {
+      /// Send message through old aggregator
+      void legacy_send() {
+        const size_t message_size = this->size();
+        char buf[ message_size ];
+        this->serialize_to( buf, message_size );
+        Grappa_call_on( destination_, legacy_send_message_am, buf, message_size );
+        is_sent_ = true;
       }
-      
+
+
+
       /// Interface for message serialization.
       ///  @param p Address in buffer at which to write:
       ///    -# A 2D global address of a function that knows how to
@@ -58,44 +61,93 @@ namespace Grappa {
 
       /// Add a message to this message's list
       inline void stitch( MessageBase * m ) {
-	m->next_ = next_;
-	next_ = m;
+        m->next_ = next_;
+        next_ = m;
       }
 
       /// Chase a list of messages and serialize them into a buffer.
       /// Modifies pointer to list to support size-limited-ish aggregation
       /// TODO: make this a hard limit?
       static char * serialize_to_buffer( char * buffer, Grappa::impl::MessageBase ** message_ptr, size_t max = -1 ) {
-	size_t size = 0;
-	Grappa::impl::MessageBase * message = *message_ptr;
-	while( message ) {
-	  // add message to buffer
-	  char * new_buffer = message->serialize_to( buffer, max - size );
-	  if( new_buffer == buffer ) { // if it was too big
-	    break;                     // quit
-	  } else {
-	    // mark as sent
-	    message->is_sent_ = true;
-	    // go to next messsage and track total size
-	    Grappa::impl::MessageBase * next = message->next_;
-	    message->next_ = NULL;
-	    size += next - message;
-	    message = next;
-	  }
-	}
-	*message_ptr = message;
-	return buffer;
+        size_t size = 0;
+        Grappa::impl::MessageBase * message = *message_ptr;
+        //DVLOG(5) << "Serializing messages from " << message;
+        while( message ) {
+          //DVLOG(5) << "Serializing message " << message;
+          // add message to buffer
+          char * new_buffer = message->serialize_to( buffer, max - size );
+          if( new_buffer == buffer ) { // if it was too big
+            break;                     // quit
+          } else {
+            // mark as sent
+            message->is_sent_ = true;
+            // go to next messsage and track total size
+            Grappa::impl::MessageBase * next = message->next_;
+            message->next_ = NULL;
+            size += next - message;
+            message = next;
+          }
+          buffer = new_buffer;
+        }
+        *message_ptr = message;
+        return buffer;
       }
       
       /// Walk a buffer of received deserializers/functors and call them.
-      static void deserialize_buffer( char * buffer, size_t size ) {
-	char * end = buffer + size;
-	while( buffer != end ) {
-	  typedef char * (*Deserializer)(char *);	// generic deserializer type
-	  Deserializer fp = *(reinterpret_cast< Deserializer* >( buffer ));
-	  buffer = fp( buffer + sizeof( Deserializer ) );
-	}
+      static char * deserialize_buffer( char * buffer, size_t size ) {
+        char * end = buffer + size;
+        while( buffer < end ) {
+          typedef char * (*Deserializer)(char *);       // generic deserializer type
+          Deserializer fp = *(reinterpret_cast< Deserializer* >( buffer ));
+          buffer = fp( buffer + sizeof( Deserializer ) );
+        }
+        return buffer; /// how far did we get before we had to stop?
       }
+
+
+    public:
+      MessageBase( )
+        : next_( NULL )
+        , is_enqueued_( false )
+        , is_sent_( false )
+        , destination_( -1 )
+        , cv_()
+      { }
+
+      MessageBase( Core dest )
+        : next_( NULL )
+        , is_enqueued_( false )
+        , is_sent_( false )
+        , destination_( dest )
+        , cv_()
+      { }
+
+      // Ensure we are sent before leaving scope
+      inline virtual ~MessageBase() {
+        block_until_sent();
+      }
+
+      /// Make sure we know how big this message is
+      virtual const size_t size() const = 0;
+
+
+      inline void send() {
+        //Grappa::impl::global_rdma_aggregator.send( this );
+      }
+
+      /// Block until message can be deallocated.
+      inline void block_until_sent() {
+        // if message has not been enqueued to be sent, do so.
+        if( !is_enqueued_ ) {
+          send();
+        }
+        // now block until message is sent
+        while( !is_sent_ ) {
+          //global_scheduler.thread_suspend();
+          Grappa::wait( &cv_ );
+        }
+      }
+      
     };
     
     /// @}
