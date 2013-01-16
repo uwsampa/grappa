f43b49995f023bdc34af139ecb2b90bc76d7c16d
Jacob Nelson <nelson@cs.washington.edu>
2013-01-16 15:35:40 -0800

    Next steps to working RDMA-based Aggregator

--- system/RDMAAggregator_tests.cpp
+++ system/RDMAAggregator_tests.cpp
@@ -15,10 +15,19 @@ BOOST_AUTO_TEST_SUITE( RDMAAggregator_tests );
 
 void user_main( void * args ) 
 {
-  auto m = Grappa::scopedMessage( 0, []{ std::cout << "Test message" << std::endl; } );
+  auto m = Grappa::scopedMessage( 0, []{ LOG(INFO) << "Test message 1"; } );
+  auto n = Grappa::scopedMessage( 0, []{ LOG(INFO) << "Test message 2"; } );
+  auto o = Grappa::scopedMessage( 1, []{ LOG(INFO) << "Test message 3"; } );
+  auto p = Grappa::scopedMessage( 1, []{ LOG(INFO) << "Test message 4"; } );
 
   Grappa::impl::global_rdma_aggregator.enqueue( &m );
+  Grappa::impl::global_rdma_aggregator.enqueue( &n );
+  Grappa::impl::global_rdma_aggregator.enqueue( &o );
+  Grappa::impl::global_rdma_aggregator.enqueue( &p );
   Grappa::impl::global_rdma_aggregator.flush( 0 );
+  Grappa::impl::global_rdma_aggregator.flush( 1 );
+
+  Grappa_merge_and_dump_stats();
 }
 
 BOOST_AUTO_TEST_CASE( test1 ) {
