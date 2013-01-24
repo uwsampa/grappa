04fe232249ae0e9fd73d8b15a5fbf3e41f9b4666
Jacob Nelson <nelson@cs.washington.edu>
2013-01-24 02:00:04 -0800

    fix a few bugs in Message

--- system/MessageBase.hpp
+++ system/MessageBase.hpp
@@ -37,18 +37,29 @@ namespace Grappa {
       friend class RDMAAggregator;
 
 
-      /// Active message handler to support sending messages through old aggregator
-      static void legacy_send_message_am( char * buf, size_t size, void * payload, size_t payload_size ) {
-        Grappa::impl::MessageBase::deserialize_buffer( static_cast< char * >( buf ), size );
+
+      /// Make sure we know how big this message is
+      virtual const size_t size() const = 0;
+
+      /// Mark message as sent
+      virtual void mark_sent() {
+	is_sent_ = true;
+	Grappa::signal( &cv_ );
       }
 
+
+
+
+      /// Active message handler to support sending messages through old aggregator
+      static void legacy_send_message_am( char * buf, size_t size, void * payload, size_t payload_size );
+
       /// Send message through old aggregator
-      void legacy_send() {
+      inline void legacy_send() {
         const size_t message_size = this->size();
         char buf[ message_size ];
         this->serialize_to( buf, message_size );
         Grappa_call_on( destination_, legacy_send_message_am, buf, message_size );
-        is_sent_ = true;
+        mark_sent();
       }
 
 
@@ -61,50 +72,16 @@ namespace Grappa {
       /// @return address of the byte following the serialized message in the buffer
       virtual char * serialize_to( char * p, size_t max_size = -1 ) = 0;
 
-      /// Add a message to this message's list
-      inline void stitch( MessageBase * m ) {
-        m->next_ = next_;
-        next_ = m;
+
+      /// Walk a buffer of received deserializers/functors and call them.
+      static inline char * deserialize_and_call( char * buffer ) {
+	DVLOG(5) << "Deserializing message from " << (void*) buffer;
+	typedef char * (*Deserializer)(char *);       // generic deserializer type
+	Deserializer fp = *(reinterpret_cast< Deserializer* >( buffer ));
+	buffer = fp( buffer + sizeof( Deserializer ) );
+	return buffer;
       }
 
-      /// Chase a list of messages and serialize them into a buffer.
-      /// Modifies pointer to list to support size-limited-ish aggregation
-      /// TODO: make this a hard limit?
-      static char * serialize_to_buffer( char * buffer, Grappa::impl::MessageBase ** message_ptr, size_t max = -1 ) {
-        size_t size = 0;
-        Grappa::impl::MessageBase * message = *message_ptr;
-        //DVLOG(5) << "Serializing messages from " << message;
-        while( message ) {
-          //DVLOG(5) << "Serializing message " << message;
-          // add message to buffer
-          char * new_buffer = message->serialize_to( buffer, max - size );
-          if( new_buffer == buffer ) { // if it was too big
-            break;                     // quit
-          } else {
-            // mark as sent
-            message->is_sent_ = true;
-            // go to next messsage and track total size
-            Grappa::impl::MessageBase * next = message->next_;
-            message->next_ = NULL;
-            size += next - message;
-            message = next;
-          }
-          buffer = new_buffer;
-        }
-        *message_ptr = message;
-        return buffer;
-      }
-      
-      /// Walk a buffer of received deserializers/functors and call them.
-      static char * deserialize_buffer( char * buffer, size_t size ) {
-        char * end = buffer + size;
-        while( buffer < end ) {
-          typedef char * (*Deserializer)(char *);       // generic deserializer type
-          Deserializer fp = *(reinterpret_cast< Deserializer* >( buffer ));
-          buffer = fp( buffer + sizeof( Deserializer ) );
-        }
-        return buffer; /// how far did we get before we had to stop?
-      }
 
 
     public:
@@ -115,7 +92,7 @@ namespace Grappa {
         , destination_( -1 )
         , cv_()
 	, is_moved_( false )
-      { LOG(INFO) << "construct " << this; }
+      { DVLOG(9) << "construct " << this; }
 
       MessageBase( Core dest )
         : next_( NULL )
@@ -124,7 +101,7 @@ namespace Grappa {
         , destination_( dest )
         , cv_()
 	, is_moved_( false )
-      { LOG(INFO) << "construct " << this; }
+      { DVLOG(9) << "construct " << this; }
 
       // Ensure we are sent before leaving scope
       inline virtual ~MessageBase() {
@@ -145,23 +122,13 @@ namespace Grappa {
         , cv_( m.cv_ )
 	, is_moved_( false ) // this only tells us if the current message has been moved
       {
-	LOG(INFO) << "move " << this; 
+	DVLOG(9) << "move " << this;
 	CHECK_EQ( is_enqueued_, false ) << "Shouldn't be moving a message that has been enqueued to be sent!"
 					<< " Your compiler's return value optimization failed you here.";
 	m.is_moved_ = true; // mark message as having been moved so sending will fail
       }
 
-      /// Make sure we know how big this message is
-      virtual const size_t size() const = 0;
 
-
-      inline void send() {
-	CHECK( !is_moved_ ) << "Shouldn't be sending a message that has been moved!"
-			    << " Your compiler's return value optimization failed you here.";
-        //Grappa::impl::global_rdma_aggregator.send( this );
-        is_enqueued_ = true;
-        legacy_send();
-      }
       /// Implemented in MessageBaseImpl.hpp
       inline void enqueue();
       inline void send_immediate();
@@ -169,12 +136,12 @@ namespace Grappa {
       /// Block until message can be deallocated.
       inline void block_until_sent() {
         // if message has not been enqueued to be sent, do so.
-        if( !is_enqueued_ ) {
+	// if it has already been sent somehow, then don't worry about it.
+        if( !is_sent_ && !is_enqueued_ ) {
           enqueue();
         }
         // now block until message is sent
         while( !is_sent_ ) {
-          //global_scheduler.thread_suspend();
           Grappa::wait( &cv_ );
         }
       }
