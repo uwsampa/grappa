d4a444c0f1005e4768105c5e95cd3a722b36ff06
Brandon Holt <bholt@cs.washington.edu>
2013-01-25 14:26:51 -0800

    Make script to run grappa applications and use it for running tests.

---
+++ bin/grappa_srun.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# directory of current file
+DIR=$PWD/`dirname $BASH_SOURCE[0]`
+
+set -- $(getopt -o n:c:h -l help,nodes:,cores-per-node: -- "$@")
+
+function print_usage {
+  echo "Script to run Grappa applications."
+  echo "Usage: $BASH_SOURCE [options] -- <executable> [executable options]"
+  echo "  -n, --nodes           Number of nodes to run the Grappa job with"
+  echo "  -c, --cores-per-node  Number of cores per node"
+  echo "  -h, --help            Print this help message"
+}
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    (-n | --nodes) NODES=$2; shift;;
+    (-c | --cores-per-node) CORES_PER_NODE=$2; shift;;
+    (-h | --help) print_usage; exit;;
+    (--) shift; break;;
+    (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
+    (*)  break;;
+  esac
+  shift
+done
+
+# defaults
+NODES=${NODES-2}
+CORES_PER_NODE=${CORES_PER_NODE-1}
+
+GRAPPA_SRUN_FLAGS="--cpu_bind=verbose,rank --exclusive --label --kill-on-bad-exit --task-prolog=$DIR/grappa_srun_prolog.sh --task-epilog=$DIR/grappa_srun_epilog.sh"
+SAMPA_FLAGS="--resv-ports --partition=grappa"
+
+# echo srun --nodes=$NODES --ntasks-per-node=$CORES_PER_NODE $GRAPPA_SRUN_FLAGS $SAMPA_FLAGS -- "$@"
+eval srun --nodes=$NODES --ntasks-per-node=$CORES_PER_NODE $GRAPPA_SRUN_FLAGS $SAMPA_FLAGS -- $@
