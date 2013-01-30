2d148899c99316efc4ba09542561c0a63f321c5c
Jacob Nelson <nelson@cs.washington.edu>
2013-01-29 23:38:54 -0800

    Update tests to support heap messages, enqueue calls, and other recent changes

--- system/Message_tests.cpp
+++ system/Message_tests.cpp
@@ -9,6 +9,7 @@
 
 #include "Grappa.hpp"
 #include "Message.hpp"
+#include "ConditionVariable.hpp"
 
 BOOST_AUTO_TEST_SUITE( Message_tests );
 
@@ -23,48 +24,49 @@ void user_main( void * args )
 {
 
   LOG(INFO) << "Test -2";
+  bool x0 = false;
   {
-    bool x0 = false;
     {
       // must run on this node
       auto f0 = [&x0]{ BOOST_CHECK_EQUAL( x0, true ); };
       Grappa::Message< decltype(f0) > m0( 0, f0 );
       //auto m1 = Grappa::message( 0, [&]{ x1 = true; } );
-      m0.send();
-      x0 = true;
-    }
-    Grappa_flush( 0 );
-    Grappa_poll( );
-
-    LOG(INFO) << "Test -1";
-    x0 = false;
-    {
-      // must run on this node
-      auto m0 = Grappa::message( 0, [&x0]{ BOOST_CHECK_EQUAL( x0, true ); } );
-      m0.send();
-      x0 = true;
-    }
-    Grappa_flush( 0 );
-    Grappa_poll( );
-
-    LOG(INFO) << "Test 0";
-    x0 = false;
-    {
-      // must run on this node
-      auto m0 = Grappa::send_message( 0, [&x0]{ BOOST_CHECK_EQUAL( x0, true ); } );
-
-      // //auto l0 = [&x0]{ BOOST_CHECK_EQUAL( x0, true ); };
-      // // Grappa::SendMessage< decltype(l0) > m0( 0, l0 );
-      
-      // auto l0a = [&x0](void * useless, size_t nothing){ BOOST_CHECK_EQUAL( x0, true ); };
-      // int foo[10];
-      // auto m0 = Grappa::send_message( 0, l0a, foo, sizeof(foo) );
-
+      m0.enqueue();
       x0 = true;
     }
     Grappa_flush( 0 );
     Grappa_poll( );
   }
+
+  LOG(INFO) << "Test -1";
+  x0 = false;
+  {
+    // must run on this node
+    auto m0 = Grappa::message( 0, [&x0]{ BOOST_CHECK_EQUAL( x0, true ); } );
+    m0.enqueue();
+    x0 = true;
+  }
+  Grappa_flush( 0 );
+  Grappa_poll( );
+  
+  LOG(INFO) << "Test 0";
+  x0 = false;
+  {
+    // must run on this node
+    auto m0 = Grappa::send_message( 0, [&x0]{ BOOST_CHECK_EQUAL( x0, true ); } );
+    
+    // //auto l0 = [&x0]{ BOOST_CHECK_EQUAL( x0, true ); };
+    // // Grappa::SendMessage< decltype(l0) > m0( 0, l0 );
+    
+    // auto l0a = [&x0](void * useless, size_t nothing){ BOOST_CHECK_EQUAL( x0, true ); };
+    // int foo[10];
+    // auto m0 = Grappa::send_message( 0, l0a, foo, sizeof(foo) );
+    
+    x0 = true;
+  }
+  Grappa_flush( 0 );
+  Grappa_poll( );
+  
 
   LOG(INFO) << "Test 1";
   {
@@ -74,20 +76,20 @@ void user_main( void * args )
       auto f1 = [&x1]{ x1 = true; };
       Grappa::Message< decltype(f1) > m1( 0, f1 );
       //auto m1 = Grappa::message( 0, [&]{ x1 = true; } );
-      m1.send();
+      m1.enqueue();
     }
     Grappa_flush( 0 );
     Grappa_poll();
     BOOST_CHECK_EQUAL( x1, true );
   }
-
+  
   LOG(INFO) << "Test 2";
   {
     // must run on this node
     Grappa::Message< Check > m2( 0, Check() );
     //auto m2 = Grappa::message( 0, Check() );
     m2->x = true;
-    m2.send();
+    m2.enqueue();
     Grappa_flush( 0 );
     Grappa_poll();
   }
@@ -99,7 +101,7 @@ void user_main( void * args )
     // must run on this node
     Grappa::ExternalMessage< Check > m3( 0, &x3 );
     m3->x = true;
-    m3.send();
+    m3.enqueue();
     Grappa_flush( 0 );
     Grappa_poll();
   }
@@ -111,7 +113,7 @@ void user_main( void * args )
       //auto m4 = Grappa::send_message( 0, [&]{ x4 = true; } );
       // must run on this node
       auto f4 = [&]{ x4 = true; };
-      Grappa::SendMessage< decltype(f4) > m4( 0, f4 );
+      auto m4 = Grappa::send_message( 0, f4 );
     }
     Grappa_flush( 0 );
     Grappa_poll();
@@ -136,10 +138,30 @@ void user_main( void * args )
 
     {
       x5 = true;
-      Grappa::SendPayloadMessage< decltype(f5) > m5a( 0, f5, &x5, sizeof(x5) );
+      auto m5a = Grappa::send_message( 0, f5, &x5, sizeof(x5) );
     }
     Grappa_flush( 0 );
     Grappa_poll();
+  }
+
+  LOG(INFO) << "Test 6";
+  {
+    Grappa::ConditionVariable cv;
+    Grappa::ConditionVariable * cvp = &cv;
+
+    auto m6 = Grappa::send_heap_message( 1, [cvp] { 
+        BOOST_CHECK_EQUAL( Grappa_mynode(), 1 );
+        auto m6a = Grappa::send_heap_message( 0, [cvp] {
+            BOOST_CHECK_EQUAL( Grappa_mynode(), 0 );
+            Grappa::signal( cvp );
+          } );
+        Grappa_flush( 0 );
+        Grappa_poll();
+      } );
+    Grappa_flush( 1 );
+    Grappa_poll();
+    Grappa::wait( cvp );
+    LOG(INFO) << "Test 6 done";
   }
 
   //Grappa_merge_and_dump_stats();
