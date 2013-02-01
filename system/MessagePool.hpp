8316d75a609991531eaebc1cfbe9319928d704d4
Brandon Holt <bholt@cs.washington.edu>
2013-02-01 10:54:16 -0800

    Refactor to use common templated Pool.

--- system/MessagePool.hpp
+++ system/MessagePool.hpp
@@ -3,14 +3,14 @@
 
 namespace Grappa {
 
-  template<size_t Bytes>
-  class MessagePool {
+  template<size_t Bytes, typename Base>
+  class Pool {
     char buffer[Bytes];
     size_t allocated;
   protected:
-    impl::MessageBase* allocate(size_t sz) {
+    Base* allocate(size_t sz) {
       LOG(ERROR) << "allocating " << sz;
-      impl::MessageBase* p = reinterpret_cast<impl::MessageBase*>(buffer+allocated);
+      Base* p = reinterpret_cast<Base*>(buffer+allocated);
       allocated += sz;
       
       // TODO: implement heap allocation fallback
@@ -19,42 +19,85 @@ namespace Grappa {
     }
     
   public:
-    MessagePool(): allocated(0) {}
+    Pool(): allocated(0) {}
     
-    ~MessagePool() {
-      // call destructors of everything in MessagePool
-      iterate([](impl::MessageBase* bp){ bp->~MessageBase(); });
+    virtual ~Pool() {
+      // call destructors of everything in Pool
+      iterate([](Base* bp){ bp->~Base(); });
     }
     
     /// Takes a lambda (or really any callable) that is called repeated for
-    /// each allocated impl::MessageBase*
+    /// each allocated Base*
     template<typename F>
     void iterate(F f) {
       char * p = buffer;
       while (p < buffer+allocated) {
-        impl::MessageBase* bp = reinterpret_cast<impl::MessageBase*>(p);
+        Base* bp = reinterpret_cast<Base*>(p);
         p += bp->size();
         f(bp);
       }
     }
     
-    template<size_t B>
-    friend void* ::operator new(size_t, Grappa::MessagePool<B>&);
+    template<size_t OtherBytes, typename OtherBase>
+    friend void* ::operator new(size_t, Grappa::Pool<OtherBytes,OtherBase>&);
+  };
+  
+  template<size_t Bytes>
+  class MessagePool : public Pool<Bytes, impl::MessageBase> {
+  public:
     
     template<typename T>
     inline Message<T>* message(Core dest, T t) {
       return new (*this) Message<T>(dest, t);
     }
     
-    template<typename T>
-    inline SendMessage<T>* send_message(Core dest, T t) {
-      return new (*this) SendMessage<T>(dest, t);
+    /// Message with payload
+    template< typename T >
+    inline PayloadMessage<T> message( Core dest, T t, void * payload, size_t payload_size ) {
+      return new (*this) PayloadMessage<T>( dest, t, payload, payload_size );
     }
+    
+    /// Message with contents stored outside object
+    template< typename T >
+    inline ExternalMessage<T> message( Core dest, T * t ) {
+      return new (*this) ExternalMessage<T>( dest, t );
+    }
+    
+    /// Message with contents stored outside object as well as payload
+    template< typename T >
+    inline ExternalPayloadMessage<T> message( Core dest, T * t, void * payload, size_t payload_size ) {
+      return new (*this) ExternalPayloadMessage<T>( dest, t, payload, payload_size );
+    }
+    
+    /// Same as message, but immediately enqueued to be sent.
+    template< typename T >
+    inline SendMessage<T> send_message( Core dest, T t ) {
+      return new (*this) SendMessage<T>( dest, t );
+    }
+    
+    /// Message with payload, immediately enqueued to be sent.
+    template< typename T >
+    inline SendPayloadMessage<T> send_message( Core dest, T t, void * payload, size_t payload_size ) {
+      return new (*this) SendPayloadMessage<T>( dest, t, payload, payload_size );
+    }
+    
+    /// Message with contents stored outside object, immediately enqueued to be sent.
+    template< typename T >
+    inline SendExternalMessage<T> send_message( Core dest, T * t ) {
+      return new (*this) SendExternalMessage<T>( dest, t );
+    }
+    
+    /// 
+    template< typename T >
+    inline SendExternalPayloadMessage<T> send_message( Core dest, T * t, void * payload, size_t payload_size ) {
+      return new (*this) SendExternalPayloadMessage<T>( dest, t, payload, payload_size );
+    }
+ 
   };
 
 }
 
-template<size_t Bytes>
-void* operator new(size_t size, Grappa::MessagePool<Bytes>& a) {
+template<size_t Bytes,typename Base>
+void* operator new(size_t size, Grappa::Pool<Bytes,Base>& a) {
   return a.allocate(size);
 }
