968caa6177d8c97c9bf3815e3f8b08d4af3bd7e0
Brandon Holt <bholt@cs.washington.edu>
2013-02-12 18:14:20 -0800

    Port GlobalCompletionEvent (from GlobalTaskJoiner) and test a bit.

--- system/CompletionEvent_tests.cpp
+++ system/CompletionEvent_tests.cpp
@@ -10,6 +10,7 @@
 #include "Grappa.hpp"
 #include "Delegate.hpp"
 #include "CompletionEvent.hpp"
+#include "ParallelLoop.hpp"
 
 BOOST_AUTO_TEST_SUITE( CompletionEvent_tests );
 
@@ -54,12 +55,139 @@ void try_many_tasks() {
   BOOST_CHECK_EQUAL(foo, N);
 }
 
+GlobalCompletionEvent gce;
+
+void try_global_ce() {
+  BOOST_MESSAGE("GlobalCompletionEvent:");
+  
+  const int64_t N = 128;
+  int64_t x = 0;
+  auto xa = make_global(&x);
+  
+  BOOST_MESSAGE("  block on user_main only");
+  on_all_cores([xa]{
+    Core origin = mycore();
+    gce.enroll(N+1);
+    for (int i=0; i<N; i++) {
+      publicTask([xa,origin]{
+        delegate::fetch_and_add(xa, 1);
+        complete(make_global(&gce,origin));
+      });
+    }
+    
+    gce.complete();
+  });
+  
+  gce.wait();
+  BOOST_CHECK_EQUAL(x, N*cores());
+  
+  
+  BOOST_MESSAGE("  block in SPMD tasks");
+  on_all_cores([]{  gce.reset();  });
+  
+  x = 0;
+  on_all_cores([xa]{
+    gce.reset();
+    int y = 0;
+    auto ya = make_global(&y);
+    
+    Core origin = mycore();
+    gce.enroll(N);
+    for (int i=0; i<N; i++) {
+      publicTask([xa,ya,origin]{
+        delegate::fetch_and_add(xa, 1);
+        delegate::fetch_and_add(ya, 1);
+        complete(make_global(&gce,origin));
+      });
+    }
+    gce.wait();
+    BOOST_CHECK_EQUAL(y, N);
+  });
+  BOOST_CHECK_EQUAL(x, N*cores());
+}
+
+void rec_spawn(GlobalAddress<int64_t> xa, int N) {
+  Core origin = mycore();
+  for (int i=0; i<N/2+1; i++) {
+    gce.enroll();
+    publicTask([xa,origin]{
+      delegate::fetch_and_add(xa, 1);
+      complete(make_global(&gce,origin));
+    });
+  }
+  if (N>1) rec_spawn(xa, N-N/2-1);
+}
+
+void try_global_ce_recursive() {
+  BOOST_MESSAGE("GlobalCompletionEvent (recursive spawns):");
+  
+  const int64_t N = 128;
+  int64_t x = 0;
+  auto xa = make_global(&x);
+  
+  BOOST_MESSAGE("  block on user_main only");
+  on_all_cores([]{  gce.reset();  });
+  
+  on_all_cores([xa]{
+    gce.enroll();
+    Core origin = mycore();
+    for (int i=0; i<N; i++) {
+      gce.enroll();
+      publicTask([xa,origin]{
+        delegate::fetch_and_add(xa, 1);
+        complete(make_global(&gce,origin));
+      });
+    }
+    
+    gce.complete();
+  });
+  
+  // overload Core0 with extra work
+  rec_spawn(xa, N*2);
+  
+  gce.wait();
+  BOOST_CHECK_EQUAL(x, N*cores()+N*2);
+  
+  
+  BOOST_MESSAGE("  block in SPMD tasks");
+  on_all_cores([]{  gce.reset();  });
+  
+  x = 0;
+  on_all_cores([xa]{
+    gce.reset();
+    int y = 0;
+    auto ya = make_global(&y);
+    
+    Core origin = mycore();
+    gce.enroll(N);
+    for (int i=0; i<N; i++) {
+      publicTask([xa,ya,origin]{
+        delegate::fetch_and_add(xa, 1);
+        delegate::fetch_and_add(ya, 1);
+        complete(make_global(&gce,origin));
+      });
+    }
+    
+    if (mycore() == 0) {
+      // overload Core0 with extra work
+      rec_spawn(xa, N*2);
+    }
+    
+    gce.wait();
+    BOOST_CHECK_EQUAL(y, N);
+  });
+  BOOST_CHECK_EQUAL(x, N*cores()+N*2);
+}
+
+
 void user_main(void * args) {
   CHECK(cores() >= 2); // at least 2 nodes for these tests...
 
   try_no_tasks();
   try_one_task();
   try_many_tasks();
+  try_global_ce();
+  try_global_ce_recursive();
 }
 
 BOOST_AUTO_TEST_CASE( test1 ) {
