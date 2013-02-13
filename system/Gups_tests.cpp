e7a34c48831e1775843372fd4a35334e75cb2542
Jacob Nelson <nelson@cs.washington.edu>
2013-02-13 02:02:14 -0800

    Gups for RDMA aggregator

--- system/Gups_tests.cpp
+++ system/Gups_tests.cpp
@@ -11,12 +11,9 @@
 #include <Grappa.hpp>
 #include "ForkJoin.hpp"
 #include "GlobalAllocator.hpp"
-#include "GlobalTaskJoiner.hpp"
-#include "Array.hpp"
 
 #include <boost/test/unit_test.hpp>
 
-DEFINE_int64( repeats, 1, "Repeats" );
 DEFINE_int64( iterations, 1 << 30, "Iterations" );
 DEFINE_int64( sizeA, 1000, "Size of array that gups increments" );
 
@@ -39,90 +36,52 @@ LOOP_FUNCTION( func_stop_profiling, index ) {
 }
 
 /// Functor to execute one GUP.
-static GlobalAddress<int64_t> Array;
-static int64_t * base;
-LOOP_FUNCTOR( func_gups, index, ((GlobalAddress<int64_t>, _Array)) ) {
-  Array = _Array;
-  base = Array.localize();
+LOOP_FUNCTOR( func_gups, index, ((GlobalAddress<int64_t>, Array)) ) {
+  const uint64_t LARGE_PRIME = 18446744073709551557UL;
+  uint64_t b = (index*LARGE_PRIME) % FLAGS_sizeA;
+  Grappa_delegate_fetch_and_add_word( Array + b, 1 );
 }
 
-void func_gups_x(int64_t * p) {
-  const uint64_t LARGE_PRIME = 18446744073709551557UL;
-  /* across all nodes and all calls, each instance of index in [0.. iterations)
-  ** must be encounted exactly once */
-  //uint64_t index = random() + Grappa_mynode();//(p - base) * Grappa_nodes() + Grappa_mynode();
-  //uint64_t b = (index*LARGE_PRIME) % FLAGS_sizeA;
-  static uint64_t index = 1;
-  uint64_t b = ((Grappa_mynode() + index++) *LARGE_PRIME) & 1023;
-  //fprintf(stderr, "%d ", b);
-  ff_delegate_add( Array + b, (const int64_t &) 1 );
-}
-void validate(GlobalAddress<uint64_t> A, size_t n) {
-  int total = 0, max = 0, min = INT_MAX;
-  double sum_sqr = 0.0;
-  for (int i = 0; i < n; i++) {
-    int tmp = Grappa_delegate_read_word(A+i);
-    total += tmp;
-    sum_sqr += tmp*tmp;
-    max = tmp > max ? tmp : max;
-    min = tmp < min ? tmp : min;
-  }
-  fprintf(stderr, "Validation:  total updates %d; min %d; max %d; avg value %g; std dev %g\n",
-	  total, min, max, total/((double)n), sqrt(sum_sqr/n - ((total/n)*total/n)));
-}
 void user_main( int * args ) {
 
-  fprintf(stderr, "Entering user_main\n");
   func_start_profiling start_profiling;
   func_stop_profiling stop_profiling;
 
   GlobalAddress<int64_t> A = Grappa_typed_malloc<int64_t>(FLAGS_sizeA);
 
-  fprintf(stderr, "user_main: Allocated global array of %d integers\n", FLAGS_sizeA);
-  fprintf(stderr, "user_main: Will run %d iterations\n", FLAGS_iterations);
-
   func_gups gups( A );
 
-    double runtime = 0.0;
-    double throughput = 0.0;
-    int nnodes = atoi(getenv("SLURM_NNODES"));
-    double throughput_per_node = 0.0;
+  double runtime = 0.0;
+  double throughput = 0.0;
+  int nnodes = atoi(getenv("SLURM_NNODES"));
+  double throughput_per_node = 0.0;
 
-    Grappa_add_profiling_value( &runtime, "runtime", "s", false, 0.0 );
-    Grappa_add_profiling_integer( &FLAGS_iterations, "iterations", "it", false, 0 );
-    Grappa_add_profiling_integer( &FLAGS_sizeA, "sizeA", "entries", false, 0 );
-    Grappa_add_profiling_value( &throughput, "updates_per_s", "up/s", false, 0.0 );
-    Grappa_add_profiling_value( &throughput_per_node, "updates_per_s_per_node", "up/s", false, 0.0 );
+  Grappa_add_profiling_value( &runtime, "runtime", "s", false, 0.0 );
+  Grappa_add_profiling_integer( &FLAGS_iterations, "iterations", "it", false, 0 );
+  Grappa_add_profiling_integer( &FLAGS_sizeA, "sizeA", "entries", false, 0 );
+  Grappa_add_profiling_value( &throughput, "updates_per_s", "up/s", false, 0.0 );
+  Grappa_add_profiling_value( &throughput_per_node, "updates_per_s_per_node", "up/s", false, 0.0 );
 
-  do {
+  fork_join_custom( &start_profiling );
 
-    Grappa_memset_local(A, 0, FLAGS_sizeA);
-   
-    fork_join_custom( &start_profiling );
+  double start = wall_clock_time();
+  fork_join( &gups, 1, FLAGS_iterations );
+  double end = wall_clock_time();
 
-  
-    double start = wall_clock_time();
-    fork_join_custom( &gups );
-    printf ("Yeahoow!\n");
-    forall_local <int64_t, func_gups_x> (A, FLAGS_iterations);
-    double end = wall_clock_time();
+  fork_join_custom( &stop_profiling );
 
-    fork_join_custom( &stop_profiling );
+  runtime = end - start;
+  throughput = FLAGS_iterations / runtime;
 
-    runtime = end - start;
-    throughput = FLAGS_iterations / runtime;
+  throughput_per_node = throughput/nnodes;
 
-    throughput_per_node = throughput/nnodes;
 
-    validate(A, FLAGS_sizeA);
+  Grappa_merge_and_dump_stats();
 
-    Grappa_merge_and_dump_stats();
-
-    LOG(INFO) << "GUPS: "
+  LOG(INFO) << "GUPS: "
             << FLAGS_iterations << " updates at "
             << throughput << "updates/s ("
             << throughput/nnodes << " updates/s/node).";
-   } while (FLAGS_repeats-- > 1);
 }
 
 
