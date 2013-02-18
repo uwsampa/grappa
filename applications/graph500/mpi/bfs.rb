b0a4ac1ac35a1d46a9e4b8580588bc7de1528b3b
Brandon Holt <bholt@cs.washington.edu>
2013-02-18 01:14:44 -0800

    Igor: script allow mpi variants.

--- applications/graph500/mpi/bfs.rb
+++ applications/graph500/mpi/bfs.rb
@@ -5,15 +5,22 @@ require 'igor'
 load '../../../igor_common.rb'
 
 Igor do  
-  database '~/exp/sosp-test.db', :bfs  
+  database '~/exp/sosp.db', :bfs
+  sbatch_flags "#{@sbatch_flags} --time=2:00:00"
   
-  command "srun graph500_mpi_simple %{scale} %{edgefactor}"
+  command "srun graph500_mpi_%{mpibfs} %{scale} %{edgefactor}"
   
   params {
+    mpibfs      'simple'
     nnode       2
     ppn         1
-    scale       16
+    scale       20
     edgefactor  16
+  }
+  
+  params {
+    experiment 'variants'
+    mpibfs  'custom', 'simple', 'replicated', 'replicated_csc', 'one_sided'
   }
   
   interact # enter interactive mode
