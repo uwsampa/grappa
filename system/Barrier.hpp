3e9867d3e70229d06f6d7d87ec2666b388770093
Brandon Holt <bholt@cs.washington.edu>
2013-02-21 12:13:42 -0800

    Fix barrier (poller can just wake locally, + get to compile)

--- system/Barrier.hpp
+++ system/Barrier.hpp
@@ -3,33 +3,23 @@
 #include "ConditionVariable.hpp"
 #include "Communicator.hpp"
 
-extern ConditionVariable barrier_cv;
-
 namespace Grappa {
+  extern ConditionVariable barrier_cv;
   
   /// Mostly equivalent to Grappa_barrier_suspending but uses ConditionVariable.
-  void barrier() {
+  inline void barrier() {
+    DVLOG(5) << "entering barrier";
     global_communicator.barrier_notify();
     wait(&barrier_cv);
-  }
-  
-  namespace impl {
-    struct BarrierBroadcast {
-      void operator()() {
-        broadcast(&barrier_cv);
-      }
-    };
   }
   
   /// Called by polling thread (Grappa.cpp::poller). On barrier completion, calls `broadcast` on all cores' `barrier_cv`s.
   inline void barrier_poll() {
     // skip if no one is waiting on the barrier
-    if (barrier_cv.waiters_ == 0) {
+    if (barrier_cv.waiters_ != 0) {
+      DVLOG(5) << "trying barrier";
       if (global_communicator.barrier_try()) {
-        Message<impl::BarrierBroadcast> msgs[cores()];
-        for (Core c=0; c<cores(); c++) {
-          msgs[c].send(c);
-        }
+        broadcast(&barrier_cv);
       }
     }
   }
