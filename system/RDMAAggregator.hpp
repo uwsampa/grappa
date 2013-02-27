5ff7a39fede665be81e06fa2ff7775a391885a2b
Jacob Nelson <nelson@cs.washington.edu>
2013-02-27 01:53:26 -0800

    Snapshot of multi-buffer RDMA aggregator

--- system/RDMAAggregator.hpp
+++ system/RDMAAggregator.hpp
@@ -20,8 +20,15 @@
 //#include "PerformanceTools.hpp"
 
 #include "MessageBase.hpp"
+#include "RDMABuffer.hpp"
+
 #include "ConditionVariableLocal.hpp"
+#include "CountingSemaphoreLocal.hpp"
 #include "FullEmpty.hpp"
+
+#include "ReusePool.hpp"
+#include "ReuseList.hpp"
+
 
 #include "Statistics.hpp"
 
@@ -100,8 +107,9 @@ namespace Grappa {
       ml->pointer_ = reinterpret_cast< intptr_t >( m ); 
     }
 
-    static const int prefetch_dist = 5;
+    static const int prefetch_dist = 4;
     static const int prefetch_type = 0; // 0 (non-temporal) or 3 (L1) are probably the best choice
+    static const int remote_buffer_pool_size = 6;
 
     struct CoreData {
       // things that must be included per core:
@@ -118,22 +126,40 @@ namespace Grappa {
       // 
       
       // should we use shared memory?
-      
+
+
+      /// all this should be on one cache line
       Grappa::impl::MessageList messages_;
       Grappa::impl::PrefetchEntry prefetch_queue_[ prefetch_dist ];
 
       // lowest-numbered core that is on a node
       Core representative_core_;
-      
-      Grappa::FullEmpty< ReceiveBufferInfo > remote_buffer_info_;
 
+      int64_t pad[2]; 
+
+      ///
+      /// another cache line
+      ///
+
+      /// cache buffer addresses on the remote core
+      Grappa::impl::ReusePool< RDMABuffer, CountingSemaphore, remote_buffer_pool_size > remote_buffers_;
+      /// how many of our buffers does this core hold?
+      //int64_t remote_buffer_held_count_;
+      Grappa::CountingSemaphore remote_buffers_held_;
+
+      ///
+      /// another cache line
+      ///
       Grappa::impl::MessageBase * received_messages_;
+      int64_t pad2[7];
 
       
       CoreData() 
         : messages_()
         , prefetch_queue_() 
-        , remote_buffer_info_()
+        , representative_core_(0)
+        , remote_buffers_()
+        , remote_buffers_held_(0)
         , received_messages_(NULL)
       { }
     } __attribute__ ((aligned(64)));
@@ -151,10 +177,13 @@ namespace Grappa {
 
       bool flushing_;
 
-      /// actual aggregation buffers
-      /// TODO: one process allocate on shared pages
-      /// assume colocated cores are stored next to each other
+      /// Received (filled) buffers go here.
+      Grappa::impl::ReuseList< RDMABuffer > received_buffer_list_;
 
+      /// Send (empty) buffers go here.
+      Grappa::impl::ReuseList< RDMABuffer > free_buffer_list_;
+
+      /// per-core storage
       std::vector< CoreData > cores_;
 
 
@@ -165,6 +194,10 @@ namespace Grappa {
       /// Active message to deserialize/call the first entry of a buffer of received deserializers/functors
       static void deserialize_first_am( gasnet_token_t token, void * buf, size_t size );
       int deserialize_first_handle_;
+      
+      /// Active message to enqueue a buffer to be received
+      static void enqueue_buffer_am( gasnet_token_t token, void * buf, size_t size );
+      int enqueue_buffer_handle_;
       
       /// Chase a list of messages and serialize them into a buffer.
       /// Modifies pointer to list to support size-limited-ish aggregation
@@ -190,8 +223,18 @@ namespace Grappa {
       }
 
       /// Sender size of RDMA transmission.
-      void send_rdma( Core core, Grappa::impl::MessageList ml );
+      void send_rdma( Core core, Grappa::impl::MessageList ml, size_t estimated_size );
+      void send_rdma_old( Core core, Grappa::impl::MessageList ml );
       void send_medium( Core core, Grappa::impl::MessageList ml );
+
+      void send_with_buffers( Core core,
+                              MessageBase ** messages_to_send_ptr,
+                              RDMABuffer * local_buf,
+                              RDMABuffer * remote_buf,
+                              size_t * serialized_count_p,
+                              size_t * serialize_calls_p,
+                              size_t * serialize_null_returns_p );
+
 
       void deliver_locally( Core core,
                             MessageBase * messages_to_send );
@@ -199,11 +242,21 @@ namespace Grappa {
       /// task that is run to allocate space to receive a message      
       static void deaggregation_task( GlobalAddress< FullEmpty < ReceiveBufferInfo > > callback_ptr );
 
+      /// handles requests for buffers from sending nodes
+      void request_buffer_for_src( Core requesting_core );
+
+      /// Respond to requestor with free buffer
+      void reply_with_free_buffer( Core destination_core, RDMABuffer * b );
+
+      /// Accept an empty buffer sent by a remote host
+      void cache_buffer_for_core( Core owner, RDMABuffer * b );
+
       /// flush one destination
       bool flush_one( Core c ) {
         if( cores_[c].messages_.raw_ != 0 ) {
           Grappa::impl::MessageList ml = grab_messages( c );
-          global_rdma_aggregator.send_rdma( c, ml );
+          size_t size = cores_[c].prefetch_queue_[ ( ml.count_ ) % prefetch_dist ].size_;
+          global_rdma_aggregator.send_rdma( c, ml, size );
           return true;
         } else {
           return false;
@@ -212,11 +265,21 @@ namespace Grappa {
 
       /// Task that is constantly waiting to do idle flushes. This
       /// ensures we always have some sending resource available.
-      void idle_flush_task();
+      void idle_flusher();
+
+      /// Task that is constantly waiting to receive and
+      /// deaggregate. This ensures we always have receiving resource
+      /// available.
+      void receive_worker();
 
       /// Condition variable used to signal flushing task.
       Grappa::ConditionVariable flush_cv_;
 
+      /// flush interlock for receives
+      bool disable_flush_;
+      const size_t max_size_;
+      bool interesting_;
+      
     public:
 
       RDMAAggregator()
@@ -225,15 +288,23 @@ namespace Grappa {
         , cores_per_node_( -1 )
         , total_cores_( -1 )
         , flushing_( false )
+        , received_buffer_list_()
+        , free_buffer_list_()
         , cores_()
         , deserialize_buffer_handle_( -1 )
         , deserialize_first_handle_( -1 )
+        , enqueue_buffer_handle_( -1 )
         , flush_cv_()
+        , disable_flush_(false)
+        , max_size_( 1 << 16 )
+        , interesting_( false )
       {
+        CHECK_LE( FLAGS_target_size, max_size_ );
       }
 
       /// initialize and register with communicator
       void init();
+      void activate();
 
       // void poll( ) {
       //   // there are two places 
@@ -248,7 +319,7 @@ namespace Grappa {
       /// Enqueue message to be sent
       inline void enqueue( Grappa::impl::MessageBase * m ) {
         app_messages_enqueue++;
-        DVLOG(4) << "Enqueued message " << m << ": " << m->typestr();
+        DVLOG(5) << __func__ << ": Enqueued message " << m << ": " << m->typestr();
 
         // get destination pointer
         Core core = m->destination_;
@@ -290,7 +361,7 @@ namespace Grappa {
           spawn_send = size > FLAGS_target_size;
 
           // if it looks like we should send
-          if( spawn_send ) {
+          if( spawn_send && !disable_flush_ ) {
             swap_ml.raw_ = 0; // leave the list empty
           } else {
             // stitch in this message
@@ -302,13 +373,13 @@ namespace Grappa {
         } while( !__sync_bool_compare_and_swap( &(dest_ptr->raw_), old_ml.raw_, swap_ml.raw_ ) );
 
         /// is it time to flush?
-        if( !spawn_send ) { // no
+        if( disable_flush_ || !spawn_send ) { // no
           // now fill in prefetch pointer and size
-          dest->prefetch_queue_[ count % prefetch_dist ].size_ = size;
+          dest->prefetch_queue_[ count % prefetch_dist ].size_ = size < max_size_ ? size : max_size_-1;
           set_pointer( &(dest->prefetch_queue_[ count % prefetch_dist ]), m );
         } else {            // yes
           rdma_capacity_flushes++;
-          global_rdma_aggregator.send_rdma( core, new_ml );
+          global_rdma_aggregator.send_rdma( core, new_ml, size );
         }
       }
 
@@ -317,25 +388,29 @@ namespace Grappa {
       void send_immediate( Grappa::impl::MessageBase * m ) {
         app_messages_immediate++;
 
-        // create temporary buffer
-        const size_t size = m->serialized_size();
+        // create temporary buffer with one extra byte for terminator
+        const size_t size = m->serialized_size() + 1;
         char buf[ size ] __attribute__ ((aligned (16)));
 
         // serialize to buffer
         Grappa::impl::MessageBase * tmp = m;
         while( tmp != nullptr ) {
-          DVLOG(5) << "Serializing message from " << tmp;
+          DVLOG(5) << __func__ << ": Serializing message from " << tmp;
           char * end = aggregate_to_buffer( &buf[0], &tmp, size );
-          DVLOG(5) << "After serializing, pointer was " << tmp;
-          DCHECK_EQ( end - buf, size );
+          DVLOG(5) << __func__ << ": After serializing, pointer was " << tmp;
+          DCHECK_EQ( end - buf, size ) << __func__ << ": Whoops! Aggregated message was too long to send as immediate";
           
+          DVLOG(5) << __func__ << ": Sending " << end - buf
+                   << " bytes of aggregated messages to " << m->destination_;
+
           // send
-          global_communicator.send(  m->destination_, deserialize_buffer_handle_, buf, size );
+          global_communicator.send(  m->destination_, deserialize_first_handle_, buf, size );
         }
       }
 
       /// Flush one destination.
       void flush( Core c ) {
+        CHECK_EQ( disable_flush_, false ) << "Uh oh! Can't explicitly flush right now!";
         rdma_requested_flushes++;
         flush_one( c );
       }
