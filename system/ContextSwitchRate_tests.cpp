5934f8c13fac016e82551d3fda67e0cec6b9eef7
Brandon Myers <bdmyers@cs.washington.edu>
2013-02-28 11:13:51 -0800

    make the yielding test take more Cores

--- system/ContextSwitchRate_tests.cpp
+++ system/ContextSwitchRate_tests.cpp
@@ -9,10 +9,12 @@
 
 #include "Grappa.hpp"
 #include "CompletionEvent.hpp"
+#include "ParallelLoop.hpp"
 
 #include <string>
 
 DECLARE_uint64( num_starting_workers );
+DEFINE_uint64( num_test_workers, 4, "Number of workers for the tests");
 DEFINE_uint64( iters_per_task, 10000, "Iterations per task" );
 DEFINE_string( test_type, "sequential_updates", "options: {yields,sequential_updates, sequential_updates16" );  
 DEFINE_uint64( private_array_size, 1, "Size of private array of 8-bytes for each task" );
@@ -41,32 +43,38 @@ BOOST_AUTO_TEST_SUITE( ContextSwitchRate_tests );
 void user_main( void * args ) {
   srand((unsigned int)Grappa_walltime());
 
-  BOOST_CHECK( Grappa_nodes() == 1 );
-  
+  // must have enough threads because they all join a barrier
+  BOOST_CHECK( FLAGS_num_test_workers < FLAGS_num_starting_workers );
+
   if (FLAGS_test_type.compare("yields")==0) {
     BOOST_MESSAGE( "Test yields" );
     {
-      final = new CompletionEvent(FLAGS_num_starting_workers);
-      task_barrier = new CompletionEvent(FLAGS_num_starting_workers);
-      
+        
       double start = Grappa_walltime();
 
-      for ( uint64_t t=0; t<FLAGS_num_starting_workers; t++ ) {
-        privateTask( [t] {
-          // wait for all to start (to hack scheduler yield)
-          task_barrier->complete();
-          task_barrier->wait();
+      on_all_cores( [] {
+        final = new CompletionEvent(FLAGS_num_test_workers);
+        task_barrier = new CompletionEvent(FLAGS_num_test_workers);
 
-          // do the work
-          for ( uint64_t i=0; i<FLAGS_iters_per_task; i++ ) { 
-            Grappa_yield();
-          }
+        for ( uint64_t t=0; t<FLAGS_num_test_workers; t++ ) {
+          privateTask( [t] {
+            // wait for all to start (to hack scheduler yield)
+            task_barrier->complete();
+            task_barrier->wait();
 
-          final->complete();
-        });
-      }
+            // do the work
+            for ( uint64_t i=0; i<FLAGS_iters_per_task; i++ ) { 
+              Grappa_yield();
+            }
 
-      final->wait();
+            final->complete();
+          });
+        }
+      
+        BOOST_MESSAGE( "waiting" );
+        final->wait();
+      });
+
       double end = Grappa_walltime();
 
       double runtime = end-start;
@@ -224,7 +232,7 @@ void user_main( void * args ) {
 BOOST_AUTO_TEST_CASE( test1 ) {
 
   Grappa_init( &(boost::unit_test::framework::master_test_suite().argc),
-                &(boost::unit_test::framework::master_test_suite().argv), 1024*1024 );
+                &(boost::unit_test::framework::master_test_suite().argv) );
 
   Grappa_activate();
 
