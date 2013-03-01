c17f9c0667d84fedba8d707a5936ea8b4d275bd4
Brandon Myers <bdmyers@cs.washington.edu>
2013-03-01 00:23:20 -0800

    add problem flavor to include old/none prefetcher

--- system/runcontextswitch.rb
+++ system/runcontextswitch.rb
@@ -21,14 +21,17 @@ GARGS=' \
 params = {
     trial: [1,2,3],
     nnode: [1],
-    ppn: [6,7,8],
+    ppn: [1,2,3,4,5,6,7,8],
     #total_iterations: [51200000],
-    num_workers: [3,200,1024,4096,5000,6000,7000,8000],#[2,4,5,6,8,16,24,32,48,64,96,128,200,256,300, 512, 600,700,800,900,1024,1200,1500,1800,2048,4096,5000,6000,7000],
+    #num_total_workers: [32, 48, 56, 64, 128, 256, 512, 600, 650, 700, 750, 800, 850, 900, 950, 1100,1200,1300,1400,1600,1800,2100,2300, 2800, 3100, 3600, 4000, 4500, 5000, 5500, 6000, 6500],
+    #num_workers: expr('num_total_workers/ppn'),
+    num_workers: [1024,1500,1750,2000,2250,2500,2750,3000,3250,3500, 3750, 4250, 4500, 5500],#[2,4,5,6,8,16,24,32,48,64,96,128,200,256,300, 512, 600,700,800,900,1024,1200,1500,1800,2048,4096,5000,6000,7000],
     num_test_workers: expr('num_workers-2'),
     iters_per_task: [7000],#expr('total_iterations/num_workers'),
     actual_total_iterations: expr('iters_per_task*num_workers'),
     machine: ['cluster'],
     problem: ['yields'],
+    problem_flavor: ['no-old-prefetch'],
     private_array_size: [0]#[1,16]#,64,256,512,8192]
 }
 
