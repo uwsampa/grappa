8c983992e18641930b5dde4e7b5c099054c5e029
Brandon Holt <bholt@cs.washington.edu>
2013-03-08 12:47:47 -0800

    Hack BetwCent to do graph500 graph generation.

--- applications/CombBLAS/Applications/BetwCent.cpp
+++ applications/CombBLAS/Applications/BetwCent.cpp
@@ -44,7 +44,8 @@ THE SOFTWARE.
 #include "../CombBLAS.h"
 #include "../SpParVec.h"	// ABAB: deprecated
 #include "../DenseParVec.h"	// ABAB: deprecated
-
+// #include <boost/algorithm/string/predicate.hpp>
+#include <boost/lexical_cast.hpp>
 
 // Simple helper class for declarations: Just the numerical type is templated 
 // The index type and the sequential matrix type stays the same for the whole code
@@ -56,7 +57,110 @@ public:
 	typedef SpDCCols < int, NT > DCCols;
 	typedef SpParMat < int, NT, DCCols > MPI_DCCols;
 };
+#define EDGEFACTOR 16
 
+template <typename PARMAT>
+void Symmetricize(PARMAT & A)
+{
+	// boolean addition is practically a "logical or"
+	// therefore this doesn't destruct any links
+	PARMAT AT = A;
+	AT.Transpose();
+	A += AT;
+}
+
+void generateGraph500(unsigned scale, Dist<bool>::MPI_DCCols& Aeff) {
+	typedef SelectMaxSRing<bool, int32_t> SR;
+	typedef SpParMat < int64_t, bool, SpDCCols<int64_t,bool> > PSpMat_Bool;
+	typedef SpParMat < int64_t, bool, SpDCCols<int32_t,bool> > PSpMat_s32p64;	// sequentially use 32-bits for local matrices, but parallel semantics are 64-bits
+	typedef SpParMat < int64_t, int, SpDCCols<int32_t,int> > PSpMat_s32p64_Int;	// similarly mixed, but holds integers as upposed to booleans
+	typedef SpParMat < int64_t, int64_t, SpDCCols<int64_t,int64_t> > PSpMat_Int64;
+
+	// Declare objects
+	PSpMat_Bool A;	
+  // PSpMat_s32p64 Aeff;
+	FullyDistVec<int64_t, int64_t> degrees;	// degrees of vertices (including multi-edges and self-loops)
+	FullyDistVec<int64_t, int64_t> nonisov;	// id's of non-isolated (connected) vertices
+	OptBuf<int32_t, int64_t> optbuf;	// let indices be 32-bits
+	bool scramble = false;
+
+	double t01 = MPI_Wtime();
+	double t02;
+	DistEdgeList<int64_t> * DEL = new DistEdgeList<int64_t>();
+	double initiator[4] = {.57, .19, .19, .05};
+  
+	DEL->GenGraph500Data(initiator, scale, EDGEFACTOR, true, true );	// generate packed edges
+  // SpParHelper::Print("Generated renamed edge lists\n");
+	t02 = MPI_Wtime();
+	ostringstream tinfo;
+	tinfo << "Generation took " << t02-t01 << " seconds" << endl;
+	SpParHelper::Print(tinfo.str());
+  
+	// Start Kernel #1
+	MPI_Barrier(MPI_COMM_WORLD);
+	double t1 = MPI_Wtime();
+
+	// conversion from distributed edge list, keeps self-loops, sums duplicates
+	PSpMat_s32p64_Int * G = new PSpMat_s32p64_Int(*DEL, false); 
+	delete DEL;	// free memory before symmetricizing
+  // SpParHelper::Print("Created Sparse Matrix (with int32 local indices and values)\n");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	double redts = MPI_Wtime();
+	G->Reduce(degrees, Row, plus<int64_t>(), static_cast<int64_t>(0));	// Identity is 0 
+	MPI_Barrier(MPI_COMM_WORLD);
+	double redtf = MPI_Wtime();
+
+  // ostringstream redtimeinfo;
+  // redtimeinfo << "Calculated degrees in " << redtf-redts << " seconds" << endl;
+  // SpParHelper::Print(redtimeinfo.str());
+	A =  PSpMat_Bool(*G);			// Convert to Boolean
+	delete G;
+	int64_t removed  = A.RemoveLoops();
+
+  // ostringstream loopinfo;
+  // loopinfo << "Converted to Boolean and removed " << removed << " loops" << endl;
+  // SpParHelper::Print(loopinfo.str());
+  // A.PrintInfo();
+
+	FullyDistVec<int64_t, int64_t> * ColSums = new FullyDistVec<int64_t, int64_t>(A.getcommgrid());
+	FullyDistVec<int64_t, int64_t> * RowSums = new FullyDistVec<int64_t, int64_t>(A.getcommgrid());
+	A.Reduce(*ColSums, Column, plus<int64_t>(), static_cast<int64_t>(0)); 	
+	A.Reduce(*RowSums, Row, plus<int64_t>(), static_cast<int64_t>(0)); 	
+  // SpParHelper::Print("Reductions done\n");
+	ColSums->EWiseApply(*RowSums, plus<int64_t>());
+	delete RowSums;
+  // SpParHelper::Print("Intersection of colsums and rowsums found\n");
+
+	// TODO: seg fault in FindInds for scale 33 
+	nonisov = ColSums->FindInds(bind2nd(greater<int64_t>(), 0));	// only the indices of non-isolated vertices
+	delete ColSums;
+
+  // SpParHelper::Print("Found (and permuted) non-isolated vertices\n");  
+	nonisov.RandPerm();	// so that A(v,v) is load-balanced (both memory and time wise)
+  // A.PrintInfo();
+#ifndef NOPERMUTE
+	A(nonisov, nonisov, true);	// in-place permute to save memory	
+  // SpParHelper::Print("Dropped isolated vertices from input\n");  
+	A.PrintInfo();
+#endif
+		
+	Aeff = Dist<bool>::MPI_DCCols(A);	// Convert to 32-bit local integers
+	A.FreeMemory();
+	Symmetricize(Aeff);	// A += A';
+	SpParHelper::Print("Symmetricized\n");	
+
+	Aeff.PrintInfo();
+			
+	MPI_Barrier(MPI_COMM_WORLD);
+	double t2=MPI_Wtime();
+			
+	ostringstream k1timeinfo;
+	k1timeinfo << (t2-t1) - (redtf-redts) << " seconds elapsed for Kernel #1" << endl;
+	SpParHelper::Print(k1timeinfo.str());
+  
+	Aeff.PrintInfo();
+}
 
 int main(int argc, char* argv[])
 {
@@ -72,8 +176,9 @@ int main(int argc, char* argv[])
         {
 		if(myrank == 0)
 		{	
-                	cout << "Usage: ./betwcent <BASEADDRESS> <K4APPROX> <BATCHSIZE>" << endl;
+                	cout << "Usage: ./betwcent (<BASEADDRESS>|RMAT:<SCALE>) <K4APPROX> <BATCHSIZE>" << endl;
                 	cout << "Example: ./betwcent Data/ 15 128" << endl;
+                	cout << "     or: ./betwcent RMAT:16 15 128" << endl;
                 	cout << "Input file input.txt should be under <BASEADDRESS> in triples format" << endl;
                 	cout << "<BATCHSIZE> should be a multiple of sqrt(p)" << endl;
 			cout << "Because <BATCHSIZE> is for the overall matrix (similarly, <K4APPROX> is global as well) " << endl;
@@ -86,15 +191,23 @@ int main(int argc, char* argv[])
 		int K4Approx = atoi(argv[2]);
 		int batchSize = atoi(argv[3]);
 
-		string directory(argv[1]);		
-		string ifilename = "input.txt";
-		ifilename = directory+"/"+ifilename;
-
 		Dist<bool>::MPI_DCCols A, AT;	// construct object
-		AT.ReadDistribute(ifilename, 0);	// read it from file, note that we use the transpose of "input" data
+
+    if (strncmp(argv[1], "RMAT:", 5) == 0) {
+      unsigned scale = boost::lexical_cast<int>(argv[1]+5);
+  		SpParHelper::Print("Using Graph500 RMAT generator\n");
+      if (myrank == 0) cout << "scale: " << scale << endl;
+      generateGraph500(scale, AT);
+    } else {
+  		string directory(argv[1]);
+  		string ifilename = "input.txt";
+  		ifilename = directory+"/"+ifilename;
+
+  		AT.ReadDistribute(ifilename, 0);	// read it from file, note that we use the transpose of "input" data
+    }
 		A = AT;
 		A.Transpose();
-			
+		
 		int nPasses = (int) pow(2.0, K4Approx);
 		int numBatches = (int) ceil( static_cast<float>(nPasses)/ static_cast<float>(batchSize));
 	
@@ -124,10 +237,12 @@ int main(int argc, char* argv[])
 			single.push_back(vrtxid);		// will return ERROR if vrtxid > N (the column dimension) 
 			int locnnz = ((AT.seq())(empty,single)).getnnz();
 			int totnnz;
+      // cout << "locnnz = " << locnnz << endl;
 			MPI_Allreduce( &locnnz, &totnnz, 1, MPI_INT, MPI_SUM, (AT.getcommgrid())->GetColWorld());
 					
 			if(totnnz > 0)
 			{
+        if (myrank == 0) cout << "picked " << vrtxid << endl;
 				candidates.push_back(vrtxid);
 				++vertices;
 			}
@@ -172,8 +287,7 @@ int main(int argc, char* argv[])
 			SpParHelper::Print("Exploring via BFS...\n");
 			while( fringe.getnnz() > 0 )
 			{
-				nsp += fringe;
-				Dist<bool>::MPI_DCCols * level = new Dist<bool>::MPI_DCCols( fringe ); 
+				nsp += fringe;Dist<bool>::MPI_DCCols * level = new Dist<bool>::MPI_DCCols( fringe ); 
 				bfs.push_back(level);
 
 				fringe = PSpGEMM<PTBOOLINT>(AT, fringe);
