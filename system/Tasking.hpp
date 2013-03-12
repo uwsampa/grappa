352e4bd609edbe96a3e4f0ed5e436612306d255d
Brandon Holt <bholt@cs.washington.edu>
2013-03-11 17:18:24 -0700

    Add back STATIC_ASSERT_SIZE_8.

--- system/Tasking.hpp
+++ system/Tasking.hpp
@@ -29,6 +29,8 @@
 
 #define STATIC_ASSERT_SIZE_8(type) static_assert(sizeof(type) == 8, "Size of "#type" must be 8 bytes.")
 
+#define STATIC_ASSERT_SIZE_8(type) static_assert(sizeof(type) == 8, "Size of "#type" must be 8 bytes.")
+
 DECLARE_uint64( num_starting_workers );
 
 
