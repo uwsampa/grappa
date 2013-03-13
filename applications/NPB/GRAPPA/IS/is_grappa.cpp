fb138fcc5f5e4a4cb82aa61f57c8c020d1307b51
Brandon Holt <bholt@cs.washington.edu>
2013-03-12 22:05:30 -0700

    intsort: Implementing rank

--- applications/NPB/GRAPPA/IS/is_grappa.cpp
+++ applications/NPB/GRAPPA/IS/is_grappa.cpp
@@ -18,9 +18,41 @@
 
 using namespace Grappa;
 
-////////////
+
+/// Quick and dirty growable bucket that is padded to take up a full Grappa block so 
+/// that malloc'ing a global array round-robins buckets to nodes to keep it as even
+/// as possible (for small numbers of buckets this is especially important)
+struct bucket_t {
+  uint64_t * v;
+  size_t nelems;
+  size_t maxelems;
+  char pad[block_size-sizeof(uint64_t*)-sizeof(size_t)*2];
+  bucket_t(): v(NULL), nelems(0) { memset(pad, 0x55, sizeof(pad)); }
+  ~bucket_t() { delete [] v; }
+  void reserve(size_t nelems) {
+    maxelems = nelems;
+    if (v != NULL) delete [] v;
+    v = new uint64_t[nelems];
+    if (!v) {
+      LOG(ERROR) << "Unable to allocate bucket of " << nelems << " elements."; exit(1);
+    }
+  }
+  const uint64_t& operator[](size_t i) const { return v[i]; }
+  uint64_t& operator[](size_t i) {
+    CHECK( i < maxelems ) << "i = " << i << ", maxelems: " << maxelems;
+    return v[i];
+  }
+  void append(uint64_t val) {
+    v[nelems] = val;
+    nelems++;
+    CHECK(nelems <= maxelems);
+  }
+  size_t size() { return nelems; }
+};
+
+
+//////////////////////
 // Globals
-////////////
 
 static const int MIN_PROCS = 1;
 static const int MAX_PROCS = 1024;
@@ -37,11 +69,22 @@ int64_t nkeys;
 int nbuckets;
 key_t maxkey;
 
+#define LOBITS (log2maxkey - log2buckets)
+
 double my_seed;
 
 NPBClass npbclass;
 
 GlobalAddress<key_t> key_array;
+
+// local version on each node, which then gets local copy of global state
+std::vector<size_t> counts;
+std::vector<size_t> bucket_ranks;
+std::vector<Core> bucket_cores;
+
+//////////////////////
+
+
 
 void init_seed() {
   my_seed = find_my_seed(  Grappa::mycore(), 
@@ -63,8 +106,81 @@ inline int next_seq_element() {
     return k*x;
 }
 
+template< typename T >
+inline void print_array(const char * name, std::vector<T> v) {
+  std::stringstream ss; ss << name << ": [";
+  for (size_t i=0; i<v.size(); i++) ss << " " << v[i];
+  ss << " ]"; VLOG(1) << ss.str();
+}
+template< typename T >
+inline void print_array(const char * name, GlobalAddress<T> base, size_t nelem) {
+  std::stringstream ss; ss << name << ": [";
+  for (size_t i=0; i<nelem; i++) ss << " " << delegate::read(base+i);
+  ss << " ]"; VLOG(1) << ss.str();
+}
+
+
 void rank(int iteration) {
-  // do nothing
+  // key_array, nkeys already available on all cores
+  
+  call_on_all_cores([]{
+    counts.resize(nbuckets);
+    bucket_ranks.resize(nbuckets);
+    for (size_t i=0; i<nbuckets; i++) {
+      counts[i] = 0;
+    }
+    bucket_cores.resize(nbuckets);
+  });
+  
+  // histogram to find out how many fall into each bucket
+  forall_localized(key_array, nkeys, [](int64_t i, key_t& k){
+    size_t b = k >> LOBITS;
+    counts[b]++;
+  });
+  // print_array("counts", counts);
+  
+  // allreduce everyone's counts & compute global bucket_ranks (prefix sum)
+  on_all_cores([]{
+    // all nodes get total counts put into their counts array
+    allreduce_inplace<size_t,collective_add>(&counts[0], counts.size());
+    
+    // everyone computes prefix sum over buckets locally
+    // bucket_ranks[0] = 0;
+    // for (size_t i=1; i<nbuckets; i++) {
+    //   bucket_ranks[i] = bucket_ranks[i-1] + counts[i-1];
+    // }
+  
+    // Determine Redistibution of keys: accumulate the bucket size totals 
+    // till this number surpasses NUM_KEYS (which the average number of keys
+    // per processor).  Then all keys in these buckets go to processor 0.
+    // Continue accumulating again until supassing 2*NUM_KEYS. All keys
+    // in these buckets go to processor 1, etc.  This algorithm guarantees
+    // that all processors have work ranking; no processors are left idle.
+    // The optimum number of buckets, however, does not result in as high
+    // a degree of load balancing (as even a distribution of keys as is
+    // possible) as is obtained from increasing the number of buckets, but
+    // more buckets results in more computation per processor so that the
+    // optimum number of buckets turns out to be 1024 for machines tested.
+    // Note that process_bucket_distrib_ptr1 and ..._ptr2 hold the bucket
+    // number of first and last bucket which each processor will have after   
+    // the redistribution is done.
+    
+    size_t bucket_sum_accumulator = 0;
+    size_t nkeys_per_core = nkeys / cores();
+    Core c = 0;
+    for(int i=0; i<nbuckets; i++ ) {
+      bucket_ranks[i] = bucket_sum_accumulator;
+      bucket_cores[i] = c; // assign this bucket to core 'c'
+      bucket_sum_accumulator += counts[i];
+      if (bucket_sum_accumulator >= (c+1)*nkeys_per_core) {
+        c++; // once this core's full, go up to the next one
+      }
+    }
+    CHECK_EQ(bucket_sum_accumulator, nkeys);
+  });
+  
+  print_array("bucket_ranks", bucket_ranks);
+  print_array("bucket_cores", bucket_cores);
 }
 
 void user_main(void * ignore) {
@@ -73,7 +189,8 @@ void user_main(void * ignore) {
   
   //  Printout initial NPB info 
   printf( "NAS Parallel Benchmarks 3.3 -- IS Benchmark in Grappa\n");
-  printf( "nkeys:       %ld  (class %c)\n", (long)nkeys, npb_class_char(npbclass));
+  printf( "nkeys:       %ld  (class %c, 2^%d)\n", (long)nkeys, npb_class_char(npbclass), scale);
+  printf( "maxkey:      %ld (2^%d)\n", maxkey, log2maxkey);
   printf( "niterations: %d\n", MAX_ITERATIONS);
   printf( "cores:       %d\n", cores());
 
@@ -106,7 +223,8 @@ void user_main(void * ignore) {
   // Do one interation for free (i.e., untimed) to guarantee initialization of  
   // all data and code pages and respective tables
   rank( 1 );
-
+  return;
+  
   if( npbclass != NPBClass::S ) printf( "\n   iteration\n" );
 
   total_time = Grappa_walltime();
