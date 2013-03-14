ab8f8a4d5bbcfc9087cd8a4940e1b8a0030c0437
Brandon Myers <bdmyers@cs.washington.edu>
2013-03-14 00:36:31 -0700

    make default Grappa tick subject to update interval

--- system/Timestamp.hpp
+++ system/Timestamp.hpp
@@ -13,6 +13,10 @@
 
 #include "common.hpp"
 
+/// Increasing the value of this parameter may
+/// reduce scheduler overhead.
+DECLARE_uint64( timestamp_tick_freq );
+
 /// Timestamps are 64-bit signed integers. Theoretically this should
 /// allow us to do overflow detection, but we ignore it for now. This
 /// will still allow many years of operation.
@@ -21,6 +25,18 @@ extern Grappa_Timestamp Grappa_current_timestamp;
 
 /// Grab a snapshot of the current value of the timestamp counter.
 static inline Grappa_Timestamp Grappa_tick() {
+  Grappa_Timestamp old_timestamp = Grappa_current_timestamp; 
+  static int64_t count = FLAGS_timestamp_tick_freq;
+  if( count-- > 0 ) {
+    Grappa_current_timestamp;
+  } else {
+    Grappa_current_timestamp = rdtsc();
+    count = FLAGS_timestamp_tick_freq;
+  }
+  return old_timestamp;
+}
+
+static inline Grappa_Timestamp Grappa_force_tick() {
   Grappa_Timestamp old_timestamp = Grappa_current_timestamp; 
   Grappa_current_timestamp = rdtsc();
   return old_timestamp;
