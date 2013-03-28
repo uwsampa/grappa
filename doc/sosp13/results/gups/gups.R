f6fa126a12bd0c30a653fa1e9fa1bb7673352dcc
Brandon Myers <bdmyers@cs.washington.edu>
2013-03-28 14:41:54 -0700

    aspect and legend

--- doc/sosp13/results/gups/gups.R
+++ doc/sosp13/results/gups/gups.R
@@ -2,7 +2,7 @@
 library(sqldf)
 library(ggplot2)
 library(scales)
-library(reshape)
+#library(reshape)
 
 # if you want to load external fonts (e.g. Humor Sans)
 # library(extrafont)
@@ -20,15 +20,16 @@ source("../common.R")
 ##   d$loop_threshold <- factor(d$loop_threshold)
 ##   return(d)
 ## }
-
+setwd('C:/cygwin/home/bdmyers/grappa/doc/sosp13/results/gups')
 dd <- read.csv(file='gups.csv',header=TRUE,sep=",")
-ddd = melt(x, id.vars=1)
+#ddd = melt(x, id.vars=1)
 
 
 p <- ggplot(dd, aes(x=Nodes, y=GUPs, color=Aggregation))+
   #aes(x=nnode, y=mops_total, ymax=2048, group=problem, color=version, shape=ppn, label=mops_total))+
   ggtitle("GUPS")+
   sosp_theme+
+  opts(aspect.ratio=.65,legend.position=c(0.8,0.6))+
   geom_line()+
   geom_point()+ # show points
   # stat_summary(fun.y="max", geom="line")+ # show line with max of each line
