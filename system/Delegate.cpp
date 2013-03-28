ce8481e34cba8f0b8bbd9e6e3e6b6ce20d716801
Jacob Nelson <nelson@cs.washington.edu>
2013-03-27 20:19:14 -0700

    summarizing statistic for round trip delegate latency

--- system/Delegate.cpp
+++ system/Delegate.cpp
@@ -19,6 +19,8 @@
 GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, flat_combiner_fetch_and_add_amount, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, delegate_ops_short_circuited, 0);
 
+GRAPPA_DEFINE_STAT(SummarizingStatistic<double>, delegate_op_roundtrip_latency, 0.0);
+
 /// Descriptor for delegate operation.
 /// Used to hold state while waiting for reply.
 struct memory_descriptor {
