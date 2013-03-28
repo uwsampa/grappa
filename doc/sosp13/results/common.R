f7f8ba6069d1684736c2cc73a956edcf7f7743ba
Brandon Holt <bholt@cs.washington.edu>
2013-03-27 20:31:51 -0700

    Add common.R file.

---
+++ doc/sosp13/results/common.R
@@ -0,0 +1,13 @@
+sosp_theme <- theme(
+  panel.background = element_rect(fill="white"),
+  panel.border = element_rect(fill=NA, color="grey50"),
+  panel.grid.major = element_line(color="grey80", size=0.3),
+  panel.grid.minor = element_line(color="grey90", size=0.3),
+  strip.background = element_rect(fill="grey90", color="grey50"),
+  strip.background = element_rect(fill="grey80", color="grey50"),
+  axis.ticks = element_line(colour="black"),
+  panel.grid = element_line(colour="black"),
+  axis.text.y = element_text(colour="black"),
+  axis.text.x = element_text(colour="black"),
+  text = element_text(size=16, family="Helvetica")
+)
