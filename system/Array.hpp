e00ca0fe5271aa2c7617092b861731f92960eb66
Brandon Holt <bholt@cs.washington.edu>
2013-04-14 22:40:46 -0700

    Add util::print_array.

--- system/Array.hpp
+++ system/Array.hpp
@@ -81,6 +81,29 @@ void prefix_sum(GlobalAddress<T> array, size_t nelem) {
   // not implemented
 }
 
+namespace util {
+  
+  template< int VERBOSITY = 1, typename T = decltype(nullptr)>
+  inline void print_array(const char * name, T * base, size_t nelem, int width = 10) {
+    std::stringstream ss; ss << "\n" << name << ": [";
+    for (size_t i=0; i<nelem; i++) {
+      if (i % width == 0) ss << "\n  ";
+      ss << " " << base[i];
+    }
+    ss << "\n]"; VLOG(VERBOSITY) << ss.str();
+  }
+  
+  template< int VERBOSITY = 1, typename T = decltype(nullptr) >
+  inline void print_array(const char * name, GlobalAddress<T> base, size_t nelem, int width = 10) {
+    std::stringstream ss; ss << "\n" << name << ": [";
+    for (size_t i=0; i<nelem; i++) {
+      if (i % width == 0) ss << "\n  ";
+      ss << " " << delegate::read(base+i);
+    }
+    ss << "\n]"; VLOG(VERBOSITY) << ss.str();
+  }
+}
+
 } // namespace Grappa
 
 /// Legacy: @see { Grappa::memset() }
