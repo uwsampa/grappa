46910f5572e5aa16818ea0f963b89a7f09eae937
Brandon Myers <bdmyers@cs.washington.edu>
2013-04-21 14:27:43 -0700

    print tuple

--- applications/join/Tuple.hpp
+++ applications/join/Tuple.hpp
@@ -2,11 +2,14 @@
 #define TUPLE_HPP
 
 #include <stdint.h>
+#include <iostream>
 
 #define TUPLE_LEN 2
 struct Tuple {
   int64_t columns[TUPLE_LEN];
 };
+
+std::ostream& operator<< (std::ostream& o, Tuple& t);
 
 #endif // TUPLE_HPP
 
