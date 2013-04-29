12590e410f9d7702f03614464c44f66d26e27d1d
Brandon Myers <bdmyers@cs.washington.edu>
2013-04-29 09:17:45 -0700

    fix the file IO

--- applications/join/relation_IO.hpp
+++ applications/join/relation_IO.hpp
@@ -31,17 +31,19 @@ void readTuples( std::string fn, GlobalAddress<Tuple> tuples, uint64_t numTuples
 std::ifstream testfile(fn);
     std::string line;
     int fin = 0;
-    if (testfile.is_open()) {
-      while (testfile.good() && fin<numTuples) {
-        std::getline( testfile, line );
-        Incoherent<Tuple>::WO lr(tuples+fin, 1);
-        std::vector<std::string> tokens = split( line, '\t' );
-        (*lr).columns[0] = std::stoi(tokens[0]);
-        (*lr).columns[1] = std::stoi(tokens[1]);
-        fin++;
-      }
-      testfile.close();
+    CHECK( testfile.is_open() );
+
+    while (testfile.good() && fin<numTuples) {
+      std::getline( testfile, line );
+      Incoherent<Tuple>::WO lr(tuples+fin, 1);
+      std::vector<std::string> tokens = split( line, '\t' );
+      VLOG(5) << tokens[0] << "->" << tokens[1];
+      (*lr).columns[0] = std::stoi(tokens[0]);
+      (*lr).columns[1] = std::stoi(tokens[1]);
+      fin++;
     }
+    CHECK( fin == numTuples );
+    testfile.close();
 }
 
 #endif // RELATIONIO_HPP
