0e1ccccc6ee6f8e3861dda09c51069dcc482109f
Brandon Myers <bdmyers@cs.washington.edu>
2013-05-09 17:37:46 -0700

    finish fixing stats trace

--- system/SummarizingStatistic.cpp
+++ system/SummarizingStatistic.cpp
@@ -4,37 +4,37 @@
 namespace Grappa {
 
 #ifdef VTRACE_SAMPLED
-  template <> inline void SummarizingStatistic<int>::vt_sample() const {
+  template <> void SummarizingStatistic<int>::vt_sample() const {
     VT_COUNT_SIGNED_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
     VT_COUNT_DOUBLE_VAL(vt_counter_stddev, stddev());
   }
-  template <> inline void SummarizingStatistic<int64_t>::vt_sample() const {
+  template <> void SummarizingStatistic<int64_t>::vt_sample() const {
     VT_COUNT_SIGNED_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
     VT_COUNT_DOUBLE_VAL(vt_counter_stddev, stddev());
   }
-  template <> inline void SummarizingStatistic<unsigned>::vt_sample() const {
+  template <> void SummarizingStatistic<unsigned>::vt_sample() const {
     VT_COUNT_UNSIGNED_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
     VT_COUNT_DOUBLE_VAL(vt_counter_stddev, stddev());
   }
-  template <> inline void SummarizingStatistic<uint64_t>::vt_sample() const {
+  template <> void SummarizingStatistic<uint64_t>::vt_sample() const {
     VT_COUNT_UNSIGNED_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
     VT_COUNT_DOUBLE_VAL(vt_counter_stddev, stddev());
   }
-  template <> inline void SummarizingStatistic<double>::vt_sample() const {
+  template <> void SummarizingStatistic<double>::vt_sample() const {
     VT_COUNT_DOUBLE_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
     VT_COUNT_DOUBLE_VAL(vt_counter_stddev, stddev());
   }
-  template <> inline void SummarizingStatistic<float>::vt_sample() const {
+  template <> void SummarizingStatistic<float>::vt_sample() const {
     VT_COUNT_DOUBLE_VAL(vt_counter_value, value_);
     VT_COUNT_UNSIGNED_VAL(vt_counter_count, n);
     VT_COUNT_DOUBLE_VAL(vt_counter_mean, mean);
