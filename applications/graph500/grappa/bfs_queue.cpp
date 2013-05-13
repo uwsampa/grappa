19db06e37b47b8327a5e4592a6718649d67ec1b4
Brandon Holt <bholt@cs.washington.edu>
2013-05-13 14:44:25 -0700

    Add bfs counter stats to bfs_local

--- applications/graph500/grappa/bfs_queue.cpp
+++ applications/graph500/grappa/bfs_queue.cpp
@@ -12,6 +12,9 @@ csr_graph g;
 GlobalAddress<range_t> eoff;
 GlobalAddress<long> bfs_tree;
 
+GRAPPA_DECLARE_STAT(SimpleStatistic<uint64_t>, bfs_vertex_visited);
+GRAPPA_DECLARE_STAT(SimpleStatistic<uint64_t>, bfs_edge_visited);
+
 double make_bfs_tree(csr_graph * g_in, GlobalAddress<int64_t> _bfs_tree, int64_t root) {
   static_assert(sizeof(long) == sizeof(int64_t), "Can't use long as substitute for int64_t");
   LOG_FIRST_N(INFO,1) << "bfs_version: 'queue'";
@@ -40,8 +43,10 @@ double make_bfs_tree(csr_graph * g_in, GlobalAddress<int64_t> _bfs_tree, int64_t
   
   while (!frontier->empty()) {
     forall_localized(frontier->begin(), frontier->size(), [frontier,next](long si, long& sv) {
+      ++bfs_vertex_visited;
       auto r = delegate::read(eoff+sv);
       forall_localized_async(g.xadj+r.start, r.end-r.start, [sv,next](long ei, long& ev) {
+        ++bfs_edge_visited;
         if (delegate::compare_and_swap(bfs_tree+ev, -1, sv)) {
           next->push(ev);
         }
