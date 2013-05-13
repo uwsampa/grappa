8e310197837adb200e9a4e504b693ab499d5a544
Brandon Holt <bholt@cs.washington.edu>
2013-05-13 14:44:52 -0700

    Working version of Beamer BFS with GlobalVector.

--- applications/graph500/grappa/bfs_beamer_queue.cpp
+++ applications/graph500/grappa/bfs_beamer_queue.cpp
@@ -8,28 +8,50 @@
 
 using namespace Grappa;
 
-DEFINE_double(beamer_alpha, 1.0, "Beamer BFS parameter for switching to bottom-up.");
-DEFINE_double(beamer_beta, 1.0, "Beamer BFS parameter for switching back to top-down.");
+DECLARE_double(beamer_alpha);
+DECLARE_double(beamer_beta);
 
-csr_graph g;
-GlobalAddress<range_t> eoff;
-GlobalAddress<long> bfs_tree;
+GRAPPA_DECLARE_STAT(SimpleStatistic<uint64_t>, bfs_vertex_visited);
+GRAPPA_DECLARE_STAT(SimpleStatistic<uint64_t>, bfs_edge_visited);
+
+// little helper for iterating over things numerous enough to need to be buffered
+#define for_buffered(i, n, start, end, nbuf) \
+  for (size_t i=start, n=nbuf; i<end && (n = MIN(nbuf, end-i)); i+=nbuf)
+
+struct BFSParent {
+  int64_t depth  : 16;
+  int64_t parent : 48;
+  
+  BFSParent(): depth(-1), parent(-1) {}
+  BFSParent(short depth, int64_t parent): depth(depth), parent(parent) {}
+  
+  bool operator==(const BFSParent& o) const { return o.depth == depth && o.parent == parent; }
+};
+
+static csr_graph g;
+static GlobalAddress<range_t> eoff;
+static GlobalAddress<BFSParent> bfs_tree;
+static int64_t current_depth;
+
+static const size_t NBUF = 64;
 
 double make_bfs_tree(csr_graph * g_in, GlobalAddress<int64_t> _bfs_tree, int64_t root) {
   static_assert(sizeof(long) == sizeof(int64_t), "Can't use long as substitute for int64_t");
-  LOG_FIRST_N(INFO,1) << "bfs_version: 'queue'";
+  LOG_FIRST_N(INFO,1) << "bfs_version: 'beamer_queue'";
   
   auto& _g = *g_in;
   call_on_all_cores([_g,_bfs_tree]{
     g = _g;
     eoff = static_cast<GlobalAddress<range_t>>(g.xoff);
-    bfs_tree = _bfs_tree;
+    auto bt = _bfs_tree;
+    bfs_tree = static_cast<GlobalAddress<BFSParent>>(bt);
+    current_depth = 0;
   });
 
   auto frontier = GlobalVector<long>::create(g.nv);
   auto next     = GlobalVector<long>::create(g.nv);
-
-  DVLOG(1) << "makebfs_tree(" << root << ")";
+  
+  DVLOG(1) << "make_bfs_tree (" << root << ")";
   
   double t = timer();
   
@@ -37,19 +59,63 @@ double make_bfs_tree(csr_graph * g_in, GlobalAddress<int64_t> _bfs_tree, int64_t
   frontier->push(root);
   
   // initialize bfs_tree to -1
-  Grappa::memset(bfs_tree, -1,  g.nv);
+  Grappa::memset(bfs_tree, BFSParent(),  g.nv);
   // parent of root is self
-  delegate::write(bfs_tree+root, root);
+  delegate::write(bfs_tree+root, BFSParent(0, root));
+  
+  bool top_down = true;
+  size_t prev_nf = -1;
+  int64_t remaining_edges = g.nadj;
   
   while (!frontier->empty()) {
-    forall_localized(frontier->begin(), frontier->size(), [frontier,next](long si, long& sv) {
-      auto r = delegate::read(eoff+sv);
-      forall_localized_async(g.xadj+r.start, r.end-r.start, [sv,next](long ei, long& ev) {
-        if (delegate::compare_and_swap(bfs_tree+ev, -1, sv)) {
-          next->push(ev);
+    call_on_all_cores([]{ current_depth++; });
+    auto nf = frontier->size();
+    if (top_down && nf > remaining_edges/FLAGS_beamer_alpha && nf > prev_nf) {
+      VLOG(2) << "switching to bottom-up";
+      top_down = false;
+    } else if (!top_down && nf < g.nv/FLAGS_beamer_beta && nf < prev_nf) {
+      VLOG(2) << "switching to top-down";
+      top_down = true;
+    }
+    
+    if (top_down) {
+      VLOG(2) << "top_down";
+      // top-down level
+      forall_localized(frontier->begin(), frontier->size(), [frontier,next](long si, long& sv) {
+        ++bfs_vertex_visited;
+        auto r = delegate::read(eoff+sv);
+        forall_localized_async(g.xadj+r.start, r.end-r.start, [sv,next](long ei, long& ev) {
+          ++bfs_edge_visited;
+          // if (delegate::compare_and_swap(bfs_tree+ev, -1, sv)) {
+          if (delegate::compare_and_swap(bfs_tree+ev, BFSParent(), BFSParent(current_depth, sv))) {
+            next->push(ev);
+          }
+        });
+      });
+    } else {
+      // bottom-up level
+      forall_localized(bfs_tree, g.nv, [next](int64_t sv, BFSParent& p){
+        if (p.depth != -1) return;
+        ++bfs_vertex_visited;
+        auto r = delegate::read(eoff+sv);
+        for_buffered(j, nc, r.start, r.end, NBUF) {
+          int64_t adj_buf[NBUF];
+          Incoherent<int64_t>::RO cadj(g.xadj+j, nc, adj_buf);
+          for (size_t k=0; k<nc; k++) {
+            ++bfs_edge_visited;
+            const int64_t ev = cadj[k];
+            
+            if (delegate::call(bfs_tree+ev,
+              [](BFSParent* t){ return t->depth != -1 && t->depth < current_depth; }
+            )) {
+              p = {current_depth, ev};
+              next->push(sv);
+              return;
+            }
+          }
         }
       });
-    });
+    }
     std::swap(frontier, next);
     next->clear();
   }
@@ -58,6 +124,16 @@ double make_bfs_tree(csr_graph * g_in, GlobalAddress<int64_t> _bfs_tree, int64_t
   
   frontier->destroy();
   next->destroy();
+  
+  // convert tree-entries back to just ints (drop depth)
+  forall_localized(bfs_tree, g.nv, [](int64_t i, BFSParent& p){
+    int64_t * t = reinterpret_cast<int64_t*>(&p);
+    if (p.depth == -1) {
+      *t = -1;
+    } else {
+      *t = p.parent;
+    }
+  });
   
   return t;
 }
