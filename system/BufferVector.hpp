7b33118a34b9bf75b5c25cce951ca55d8123dbe7
Brandon Holt <bholt@cs.washington.edu>
2013-05-18 14:10:10 -0700

    Initial GlobalHashTable (taken from Myers' `MatchesDHT`)

---
+++ system/BufferVector.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "Grappa.hpp"
+#include "LocaleSharedMemory.hpp"
+
+#include <stdlib.h>
+
+namespace Grappa {
+
+/// Vector that exposes its storage array as read-only.
+/// increases in size as elements are added.
+/// 
+/// Only supports insertions. Insertions allowed only
+/// while vector is in write only (OpMode::WO) mode and 
+/// getting the buffer pointer only allowed in read only mode
+/// 
+template < typename T >
+class BufferVector {
+
+  private:
+    T * buf;
+    size_t size;
+    uint64_t nextIndex;
+
+    enum class OpMode { RO, WO };
+    OpMode mode;
+
+  public:
+    BufferVector( size_t capacity = 2 ) 
+      : buf ( locale_alloc<T>(capacity) )
+      , mode( OpMode::WO )
+      , nextIndex( 0 )
+      , size( capacity ) { }
+
+    ~BufferVector() {
+      locale_free(buf);
+    }
+
+    void setWriteMode() {
+      CHECK( mode != OpMode::WO ) << "already in OpMode::WO mode";
+      mode = OpMode::WO;
+    }
+
+    void setReadMode() {
+      CHECK( mode != OpMode::RO ) << "already in OpMode::RO mode";
+      mode = OpMode::RO;
+    }
+
+    void insert( const T& v ) {
+      CHECK( mode == OpMode::WO ) << "Must be in OpMode::WO mode to insert";
+
+      if ( nextIndex == size ) {
+        // expand the size of the buffer
+        size_t newsize = size * 2;
+        T * newbuf = locale_alloc<T>(newsize);
+        memcpy( newbuf, buf, size*sizeof(T) );
+        locale_free( buf );
+        size = newsize;
+        buf = newbuf;
+      }
+
+      // store element and increment index
+      buf[nextIndex++] = v;
+    }
+
+    GlobalAddress<T> getReadBuffer() const {
+      CHECK( mode == OpMode::RO ) << "Must be in OpMode::RO mode to see buffer";
+      return make_global( /*static_cast<const T*>*/(buf) );
+    }
+
+    size_t getLength() const {
+      return nextIndex; 
+    }
+
+    /* to close the safety loop, OpMode::RO clients should release buffers,
+     * allowing setWriteMode() to check that all buffers are released
+     * but currently we will avoid this extra communication
+     *
+     * If we want more fine-grained concurrent R/W then it is better
+     * not to expose this getBuffer type of interface 
+     */
+
+    template < typename T1 >
+    friend std::ostream& operator<<( std::ostream& o, const BufferVector<T1>& v );
+};
+
+template< typename T >
+std::ostream& operator<<( std::ostream& o, const BufferVector<T>& v ) {
+  o << "BV(" 
+             << "size=" << v.size << ", "
+             << "nextIndex=" << v.nextIndex << /*", "
+             << "mode=" << v.mode==BufferVector<T>::OpMode::RO?"RO":"WO" <<*/ ")[";
+
+  // print contents; ignores RO/WO functionality
+  for (uint64_t i=0; i<v.nextIndex; i++) {
+    int64_t r;
+    o << v.buf[i] << ",";
+  }
+  o << "]";
+  return o;
+}
+
+} // namespace Grappa
---
+++ system/GlobalHashTable.hpp
@@ -0,0 +1,184 @@
+#pragma once
+
+#include "Grappa.hpp"
+#include "GlobalAllocator.hpp"
+#include "ParallelLoop.hpp"
+#include "BufferVector.hpp"
+#include "Statistics.hpp"
+
+#include <list>
+
+// for all hash tables
+//GRAPPA_DEFINE_STAT(MaxStatistic<uint64_t>, max_cell_length, 0);
+
+namespace Grappa {
+
+// Hash table for joins
+// * allows multiple copies of a Key
+// * lookups return all Key matches
+template <typename K, typename V, uint64_t (*HF)(K)> 
+class GlobalHashTable {
+protected:
+  struct Entry {
+    K key;
+    BufferVector<V> * vs;
+    Entry( K key ) : key(key), vs(new BufferVector<V>( 16 )) {}
+    Entry( K key, BufferVector<V> * vs ): key(key), vs(vs) {}
+    Entry(Entry&& e): key(e.key), vs(e.vs) { DVLOG(1) << "Entry moved."; }
+    ~Entry() { if (vs != nullptr) delete vs; }
+  };
+  
+  struct Cell {
+    std::list<Entry> * entries;
+    Cell() : entries( nullptr ) {}
+  };
+
+  // private members
+  GlobalAddress<GlobalHashTable> self;
+  GlobalAddress< Cell > base;
+  size_t capacity;
+
+  char _pad[block_size - sizeof(self)-sizeof(base)-sizeof(capacity)];
+
+  uint64_t computeIndex( K key ) {
+    return HF(key) % capacity;
+  }
+
+  // for creating local GlobalHashTable
+  GlobalHashTable( GlobalAddress<GlobalHashTable> self, GlobalAddress<Cell> base, size_t capacity ) {
+    this->self = self;
+    this->base = base;
+    this->capacity = capacity;
+  }
+  
+public:
+  // for static construction
+  GlobalHashTable( ) {}
+  
+  static GlobalAddress<GlobalHashTable> create(size_t total_capacity) {
+    auto base = global_alloc<Cell>(total_capacity);
+    auto self = mirrored_global_alloc<GlobalHashTable>();
+    call_on_all_cores([self,base,total_capacity]{
+      new (self.localize()) GlobalHashTable(self, base, total_capacity);
+    });
+    forall_localized(base, total_capacity, [](int64_t i, Cell& c) { new (&c) Cell(); });
+    return self;
+  }
+  
+  void destroy() {
+    auto self = this->self;
+    forall_localized(this->base, this->capacity, [](int64_t i, Cell& c){
+      if (c.entries != nullptr) { delete c.entries; }
+    });
+    global_free(this->base);
+    call_on_all_cores([self]{ self->~GlobalHashTable(); });
+    global_free(self);
+  }
+    
+  void global_set_RO() {
+    forall_localized(base, capacity, [](int64_t i, Cell& c){
+      // if cell is not hit then no action
+      if (c.entries == nullptr) return;
+      
+      for (Entry& e : *c.entries) {
+        e.vs->setReadMode();
+      }
+    });
+  }
+
+  uint64_t lookup ( K key, GlobalAddress<V> * vals ) {          
+    uint64_t index = computeIndex( key );
+    GlobalAddress< Cell > target = base + index; 
+    
+    struct lookup_result {
+      GlobalAddress<V> matches;
+      size_t num;
+    };
+
+    auto result = delegate::call(target, [key](Cell* c) {
+      lookup_result lr;
+      lr.num = 0;
+
+      // first check if the cell has any entries;
+      // if not then the lookup returns nothing
+      if (c->entries == nullptr) return lr;
+      
+      for (Entry& e : *c->entries) {
+        if ( e.key == key ) {  // typename K must implement operator==
+          lr.matches = e.vs->getReadBuffer();
+          lr.num = e.vs->getLength();
+          break;
+        }
+      }
+
+      return lr;
+    });
+
+    *vals = result.matches;
+    return result.num;
+  } 
+
+  // Inserts the key if not already in the set
+  // Shouldn't be used with `insert`.
+  //
+  // returns true if the set already contains the key
+  bool insert_unique( K key ) {
+    uint64_t index = computeIndex( key );
+    GlobalAddress< Cell > target = base + index; 
+    return delegate::call( target, [key](Cell* c) {
+      // TODO: have an additional version that returns void to upgrade to call_async
+
+      // if first time the cell is hit then initialize
+      if ( c->entries == nullptr ) {
+        c->entries = new std::list<Entry>();
+      }
+
+      // find matching key in the list
+      for (Entry& e : *c->entries) {
+        if ( e.key == key ) {
+          // key found so no insert
+          return true;
+        }
+      }
+
+      // this is the first time the key has been seen so add it to the list
+      // TODO: cleanup since sharing insert* code here, we are just going to store an empty vector perhaps a different module
+      c->entries->push_back(Entry(key,nullptr));
+
+      return false;
+    });
+  }
+
+
+  void insert( K key, V val ) {
+    uint64_t index = computeIndex( key );
+    GlobalAddress< Cell > target = base + index; 
+
+    delegate::call(target, [key, val](Cell* c) { // TODO: upgrade to call_async
+      // list of entries in this cell
+
+      // if first time the cell is hit then initialize
+      if ( c->entries == nullptr ) {
+        c->entries = new std::list<Entry>();
+      }
+
+      // find matching key in the list
+      for (Entry& e : *c->entries) {
+        if ( e.key == key ) {
+          // key found so add to matches
+          e.vs->insert( val );
+          return;
+        }
+      }
+
+      // this is the first time the key has been seen
+      // so add it to the list
+      Entry newe( key );
+      newe.vs->insert( val );
+      c->entries->push_back( newe );
+    });
+  }
+
+};
+
+} // namespace Grappa
