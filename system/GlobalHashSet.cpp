dcb1a44c887f8efc279bc5cea4e7631c3e0583c1
Brandon Holt <bholt@cs.washington.edu>
2013-05-19 15:09:24 -0700

    GlobalHashSet: Implement flat_combining version

--- system/GlobalHashSet.cpp
+++ system/GlobalHashSet.cpp
@@ -3,3 +3,4 @@
 // GRAPPA_DEFINE_STAT(MaxStatistic<uint64_t>, max_cell_length, 0);
 
 GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, cell_traversal_length, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hash_set_inserts_flattened, 0);
