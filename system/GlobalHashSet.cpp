73930ce024dacead8c902f8261921c62047b8000
Brandon Holt <bholt@cs.washington.edu>
2013-05-19 21:40:52 -0700

    Change GlobalHashSet stats to match GlobalVector (ops/msgs)

--- system/GlobalHashSet.cpp
+++ system/GlobalHashSet.cpp
@@ -3,4 +3,5 @@
 // GRAPPA_DEFINE_STAT(MaxStatistic<uint64_t>, max_cell_length, 0);
 
 GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, cell_traversal_length, 0);
-GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hash_set_inserts_flattened, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_insert_ops, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_insert_msgs, 0);
