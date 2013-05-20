e3429249bfdf1e337d16448838e79eda6e3770d1
Brandon Holt <bholt@cs.washington.edu>
2013-05-20 14:39:35 -0700

    Some scripty stuff.

---
+++ system/igor_queue.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require_relative 'igor_datastructs'
+
+Igor do
+  include Isolatable
+  
+  @dbtable = :queue
+  
+  isolate "GlobalVector_tests.test"
+  
+  GFLAGS.merge!({
+    ntrials: [1],
+    nelems: [1024],
+  })
+  command @test_cmd['GlobalVector_tests', '--perf']
+  
+  params {
+    scale 10
+    nelems expr('2**scale')
+    ntrials 1
+  }
+  
+  interact
+end
