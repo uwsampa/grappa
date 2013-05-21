8ab2ee1abf0470a4806129a7748f15c364d715df
Brandon Holt <bholt@cs.washington.edu>
2013-05-21 00:30:15 -0700

    CC: first phase of connected components compiles and runs.

--- applications/suite/grappa/connectedComponentsKahan.cpp
+++ applications/suite/grappa/connectedComponentsKahan.cpp
@@ -11,14 +11,20 @@ using namespace Grappa;
 DECLARE_int64(cc_hash_size);
 
 struct ColoredVertex {
-  bool visited: 1;
-  long color: 63;
+  union {
+    struct {
+      bool visited: 1;
+      long color: 63;
+    };
+    intptr_t raw_; // unnecessary; just to ensure alignment
+  };
+  
   ColoredVertex(): visited(false), color(-1) {}
 };
 
 static graph g;
 
-static GlobalAddress<graphint> colors;
+static GlobalAddress<ColoredVertex> colors;
 static GlobalAddress<graphint> visited;
 static GlobalAddress<color_t> marks;
 static graphint nchanged;
@@ -28,23 +34,30 @@ GlobalCompletionEvent gce;
 
 struct Edge {
   graphint start, end;
-  static uint64_t identity_hash(const Edge& e) {
-    return e.start ^ e.end;
-  }
+  bool operator==(const Edge& e) const { return e.start == start && e.end == end; }
   friend std::ostream& operator<<(std::ostream& o, const Edge& e);
 };
 std::ostream& operator<<(std::ostream& o, const Edge& e) {
   o << "{" << e.start << "," << e.end << "}";
   return o;
 }
+namespace std {
+  template <> struct hash<Edge> {
+    typedef size_t result_type;
+    typedef Edge argument_type;
+    size_t operator()(Edge const & e) const noexcept {
+      return static_cast<size_t>(e.start ^ e.end);
+    }
+  };
+}
 
-using EdgeHashSet = GlobalHashSet<Edge,Edge::&identity_hash,&gce>;
+using EdgeHashSet = GlobalHashSet<Edge>;
 static GlobalAddress<EdgeHashSet> component_edges;
 
 template< GlobalCompletionEvent * GCE >
 void explore(graphint v) {
   // delegate probably short-circuited because we try to spawn task at ColoredVertex
-  auto mycolor = delegate::call(colors+v, [](ColoredVertex * c) {
+  auto mycolor = delegate::call(colors+v, [v](ColoredVertex * c) {
     graphint mycolor;
     if (!c->visited) {
       c->visited = true;
@@ -55,20 +68,30 @@ void explore(graphint v) {
     return mycolor;
   });
   // visit neighbors of v
-  graphint _c[2]; Incoherent<graphint>:RO c(g.edgeStart+v, 2, _c); c.start_acquire();
+  graphint _c[2]; Incoherent<graphint>::RO c(g.edgeStart+v, 2, _c);
   
   forall_localized_async<GCE>(g.endVertex+c[0], c[1]-c[0], [mycolor](graphint& ev){
     // TODO: make async
-    delegate::call(colors+ev, [mycolor,ev](ColoredVertex * ec){
+    GCE->enroll();
+    Core origin = mycore();
+    auto colors_ev = colors+ev;
+    send_message(colors_ev.core(), [origin,mycolor,colors_ev]{
+      auto ec = colors_ev.pointer();
+      size_t ev = colors_ev - colors;
       if (!ec->visited) {
         ec->visited = true;
         ec->color = mycolor;
-        publicTask<GCE>([ev]{ explore<GCE>(ev); });
+        publicTask([ev,origin]{
+          explore<GCE>(ev);
+          complete(make_global(GCE,origin));
+        });
       } else {
         // already had color
-        privateTask<GCE>([ec,mycolor]{
+        // TODO: avoid spawning task? (i.e. make insert() async)
+        privateTask([ec,mycolor,origin]{
           Edge edge = (ec->color > mycolor) ? Edge{mycolor,ec->color} : Edge{ec->color,mycolor};
-          component_edges.insert<GCE>(edge);          
+          component_edges->insert(edge);
+          complete(make_global(GCE,origin));
         });
       }
     });
@@ -82,12 +105,13 @@ void explore(graphint v) {
 graphint connectedComponents(graph * in_g) {
   const auto NV = in_g->numVertices;
   
-  auto _component_edges = GlobalHashSet<EdgeHashSet>::create(FLAGS_cc_hash_size);
+  auto _component_edges = EdgeHashSet::create(FLAGS_cc_hash_size);
   
-  auto _colors = global_alloc<graphint>(NV);
+  auto _colors = global_alloc<ColoredVertex>(NV);
   auto _visited = global_alloc<graphint>(NV);
   auto _in_g = *in_g;
-  call_on_all_cores([_colors,_visited,_in_g]{
+  call_on_all_cores([_colors,_visited,_in_g,_component_edges]{
+    component_edges = _component_edges;
     colors = _colors;
     visited = _visited;
     g = _in_g;
@@ -95,7 +119,7 @@ graphint connectedComponents(graph * in_g) {
   
   ///////////////////////////////////////////////////////////////
   // Find component edges
-  forall_localized<&gce>(colors, NV, [](int64_t v, ColoredVertex& c){
+  forall_localized<&gce,256>(colors, NV, [](int64_t v, ColoredVertex& c){
     explore<&gce>(v);
   });
   
