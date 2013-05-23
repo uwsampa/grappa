16a35c448d3823107d7b0355b634d25dd3008bf7
Brandon Holt <bholt@cs.washington.edu>
2013-05-23 15:40:28 -0700

    SummarizingStatistic: Add min and max.

--- system/SummarizingStatisticImpl.hpp
+++ system/SummarizingStatisticImpl.hpp
@@ -28,29 +28,34 @@ namespace Grappa {
       
       send_heap_message(c, [remote_stat, combined_addr, &ce] {
           SummarizingStatistic<T>* s = remote_stat.pointer();
-          T s_value = s->value_;
-          size_t s_n = s->n;
-          double s_mean = s->mean;
-          double s_M2 = s->M2;
-
-          send_heap_message(combined_addr.node(), [combined_addr, s_value, s_n, s_mean, s_M2, &ce] {
+          struct {
+            T value_, min, max; size_t n; double mean, M2;
+          } sp = {
+            s->value_, s->min, s->max, s->n, s->mean, s->M2
+          };
+          
+          send_heap_message(combined_addr.node(), [combined_addr, sp, &ce] {
               SummarizingStatistic<T>* combined_ptr = combined_addr.pointer();
-              if( s_n > 0 ) { // update only if we have accumulated samples
-                combined_ptr->value_ += s_value;
+              if( sp.n > 0 ) { // update only if we have accumulated samples
+                combined_ptr->value_ += sp.value_;
                 if( 0 == combined_ptr->n ) { // overwrite if we are the first samples
-                  combined_ptr->n = s_n;
-                  combined_ptr->mean = s_mean;
-                  combined_ptr->M2 = s_M2;
+                  combined_ptr->n = sp.n;
+                  combined_ptr->mean = sp.mean;
+                  combined_ptr->M2 = sp.M2;
+                  combined_ptr->min = sp.min;
+                  combined_ptr->max = sp.max;
                 } else {
-                  size_t new_n = s_n + combined_ptr->n;
-                  double delta = s_mean - combined_ptr->mean;
-                  double new_mean = combined_ptr->mean + delta * ( static_cast<double>(s_n) / new_n );
-                  double new_M2 = combined_ptr->M2 + s_M2 + 
-                    delta * delta * combined_ptr->n * s_n / new_n;
+                  size_t new_n = sp.n + combined_ptr->n;
+                  double delta = sp.mean - combined_ptr->mean;
+                  double new_mean = combined_ptr->mean + delta * ( static_cast<double>(sp.n) / new_n );
+                  double new_M2 = combined_ptr->M2 + sp.M2 + 
+                    delta * delta * combined_ptr->n * sp.n / new_n;
                   
                   combined_ptr->n = new_n;
                   combined_ptr->mean = new_mean;
                   combined_ptr->M2 = new_M2;
+                  combined_ptr->max = std::max(combined_ptr->max, sp.max);
+                  combined_ptr->min = std::min(combined_ptr->min, sp.min);
                 }
               }
               ce.complete();
