65e98c6e996c4b7488046e5975c79af463cfdacb
Brandon Holt <bholt@cs.washington.edu>
2013-05-31 01:24:54 -0700

    GlobalVector: cleanup a bunch, fix stats for pop,push,&deq.

--- system/GlobalVector.cpp
+++ system/GlobalVector.cpp
@@ -2,5 +2,10 @@
 
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_push_ops, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_push_msgs, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_pop_ops, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_pop_msgs, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_deq_ops, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, global_vector_deq_msgs, 0);
 GRAPPA_DEFINE_STAT(SummarizingStatistic<double>, global_vector_push_latency, 0);
+GRAPPA_DEFINE_STAT(SummarizingStatistic<double>, global_vector_pop_latency, 0);
 GRAPPA_DEFINE_STAT(SummarizingStatistic<double>, global_vector_deq_latency, 0);
