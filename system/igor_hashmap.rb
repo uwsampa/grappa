5e962507b363ef71ee036c42bc6011fb6a47fa6b
Brandon Holt <bholt@cs.washington.edu>
2013-06-03 00:56:16 -0700

    Updated igor scripts for hash{map,set}

--- system/igor_hashmap.rb
+++ system/igor_hashmap.rb
@@ -6,12 +6,5 @@ Igor do
   
   command @test_cmd['GlobalHash_tests', '--map_perf']
   
-  params {
-    log_nelems 10; nelems expr('2**log_nelems')
-    log_max_key 10; max_key expr('2**log_max_key')
-    log_global_hash_size 10; global_hash_size expr('2**log_global_hash_size')
-    ntrials 1
-  }
-  
   interact
 end
