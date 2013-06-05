9d9e6d5c41d0325c0188a1d7422ffc33e1487404
Brandon Holt <bholt@cs.washington.edu>
2013-06-05 11:03:36 -0700

    Add plots for BFS and CC performance.

--- doc/pgas13/data/plot.bfs.R
+++ doc/pgas13/data/plot.bfs.R
@@ -4,21 +4,21 @@ source("common.R")
 
 q2 <- "select * from bfs where bfs_version like 'beamer%'"
 
-d <- db(q2, c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks"))
+d <- db(q2, c("nnode", "periodic_poll_ticks", "flat_combining"))
 d$global_vector_push_ops_per_msg <- with(d, global_vector_push_ops/global_vector_push_msgs)
 
 d$version_combining <- paste(d$bfs_version,":flat<",d$flat_combining,">",sep="")
 d$mteps <- d$max_teps/1e6
 
-d <- melt(d, measure=c("global_vector_push_ops_per_msg", "mteps")) 
+d.melt <- melt(d, measure=c("global_vector_push_ops_per_msg", "mteps")) 
 
-g <- ggplot(subset(d,
-    ppn == 16
+g <- ggplot(subset(d.melt,
+    ppn == 16 & scale == 26
   ), aes(
     x=num_starting_workers,
     y=value,
     color=x(version_combining,ppn),
-    shape=aggregator_autoflush_ticks,
+    # shape=aggregator_autoflush_ticks,
     group=x(version_combining,ppn)
     # label=nnode~ppn,
   ))+
@@ -29,26 +29,39 @@ g <- ggplot(subset(d,
   ylab("")+
   expand_limits(y=0)+
   my_theme
+ggsave(plot=g, filename="plots/bfs_mess.pdf", width=10, height=8)
 
-ggsave(plot=g, filename="plots/bfs_mess.pdf", scale=1.2)
+d$nw <- d$num_starting_workers
+
+d$fc_version <- sapply(paste(d$bfs_version,d$flat_combining,sep='_'),switch,
+  beamer_0='async', beamer_1='async', beamer_queue_0='none', beamer_queue_1='full'
+)
+# d$fc_version <- paste(d$bfs_version,d$flat_combining,sep='_')
+# d$fc_version
 
 g <- ggplot(subset(d,
-    ppn == 16 & scale == 26 & num_starting_workers == 1024
+    ppn == 16 & scale == 26
+    & (num_starting_workers == 1024 & grepl('full|none', fc_version) 
+      | num_starting_workers >= 256 & grepl('async', fc_version))
+    & aggregator_autoflush_ticks > 100000
   ), aes(
     x=nnode,
-    y=value,
-    color=x(version_combining,ppn),
-    shape=aggregator_autoflush_ticks,
-    group=x(version_combining,ppn)
-    # label=nnode~ppn,
+    y=harmonic_mean_teps/1e6,
+    color=fc_version,
+    # shape=,
+    linetype=fc_version,
+    group=x(fc_version,num_starting_workers),
+    label=x(num_starting_workers,aggregator_autoflush_ticks),
   ))+
   geom_point()+
-  geom_smooth(aes(linetype=flat_combining))+
-  # facet_grid(~variable~ppn, scales="free", labeller=label_bquote(.(prettify(x))))+
-  facet_grid(~variable~scale~nnode, scales="free", labeller=label_pretty)+
-  ylab("")+
-  expand_limits(y=0)+
+  # geom_text(size=2,position="jitter")+
+  geom_smooth(size=1)+
+  xlab("Nodes")+
+  scale_color_discrete(name="Flat Combining")+
+  scale_linetype_discrete(name="Flat Combining")+
+  # facet_grid(nw~., scales="free", labeller=label_pretty)+
+  ylab("MTEPS")+expand_limits(y=0)+
   my_theme
 
-ggsave(plot=g, filename="plots/bfs_perf.pdf", scale=1.2)
+ggsave(plot=g, filename="plots/bfs_perf.pdf", width=7, height=5)
 
--- doc/pgas13/data/plot.cc.R
+++ doc/pgas13/data/plot.cc.R
@@ -7,7 +7,6 @@ d <- db(
    ",
   c("nnode", "ppn",
     "num_starting_workers",
-    "aggregator_autoflush_ticks",
     "periodic_poll_ticks",
     "flat_combining",
     "cc_concurrent_roots"
@@ -20,7 +19,7 @@ d$nroots <- d$cc_concurrent_roots
 d$gce_flats <- with(d, gce_total_remote_completions/gce_completions_sent)
 
 d$fc_version <- sapply(paste('v',d$flat_combining,d$cc_insert_async,sep=''),switch,
-  v0NA='none', v1NA='fc', v11='async', v10='fc', v00='none'
+  v0NA='none', v1NA='fc', v11='async', v10='fc', v00='none', '??'
 )
 
 g <- ggplot(subset(d, cc_hash_size <= 16384 & scale == 26 & ppn == 16), aes(
@@ -28,41 +27,49 @@ g <- ggplot(subset(d, cc_hash_size <= 16384 & scale == 26 & ppn == 16), aes(
     y=mteps,
     color=nroots,
     shape=fc_version,
-    # label=trial,
-    group=x(cc_concurrent_roots,fc_version)
+    label=x(aggregator_autoflush_ticks/1e6,num_starting_workers),
+    group=x(cc_concurrent_roots,fc_version,aggregator_autoflush_ticks),
+    linetype=fc_version,
   ))+
   geom_point()+
   # geom_smooth(aes(linetype=fc_version),stat=stat_smooth(se=FALSE))+
   # geom_smooth(aes(linetype=fc_version),fill=NA)+
-  geom_line(aes(linetype=fc_version))+
-  # geom_text(size=2,hjust=-0.2,vjust=1)+
+  geom_line()+
+  geom_text(size=2,hjust=-0.2,vjust=1)+
   facet_grid(~cc_hash_size~ppn~scale~nnode, scales="free", labeller=label_pretty)+
   # ylab("")+
   expand_limits(y=0)+
+  theme(strip.text=element_text(size=rel(0.4)),
+        axis.text.x=element_text(size=rel(0.75)))+
   my_theme
 # ggsave(plot=g, filename="plots/cc_mess.pdf", scale=1.4)
 
 g <- ggplot(subset(d, 
     cc_hash_size == 16384 & scale == 26 & ppn == 16
-    & num_starting_workers == 2048 & cc_concurrent_roots == 2048
+    & ( fc_version == 'async' & num_starting_workers == 2048 & aggregator_autoflush_ticks == 5e5
+      | fc_version == 'fc' & num_starting_workers == 2048 & aggregator_autoflush_ticks == 5e5
+      | fc_version == 'none' & num_starting_workers == 2048 & aggregator_autoflush_ticks == 1e5
+    )
+    & cc_concurrent_roots == 2048
+    # & aggregator_autoflush_ticks >= 500000
   ), aes(
     x=nnode,
     y=mteps,
     color=fc_version,
     shape=fc_version,
     linetype=fc_version,
-    group=x(cc_concurrent_roots,fc_version),
+    group=x(cc_concurrent_roots,fc_version,aggregator_autoflush_ticks,num_starting_workers),
+    label=x(aggregator_autoflush_ticks/1e6,num_starting_workers),
   ))+
-  geom_point()+
-  # geom_smooth(aes(linetype=fc_version),stat=stat_smooth(se=FALSE))+
-  # geom_smooth(aes(linetype=fc_version),fill=NA)+
-  geom_line()+
-  xlab("Nodes")+
+  # geom_point()+
+  geom_smooth(size=1)+
   # geom_text(size=2,hjust=-0.2,vjust=1)+
+  # geom_line()+
+  xlab("Nodes")+
   # facet_grid(scales="free", labeller=label_pretty)+
   ylab("MTEPS")+expand_limits(y=0)+
   my_theme
-ggsave(plot=g, filename="plots/cc_perf.pdf", width=8, height=6)
+ggsave(plot=g, filename="plots/cc_perf.pdf", width=7, height=5)
 
 # g <- ggplot(d.m, aes(
 #     x=cc_concurrent_roots,
@@ -97,7 +104,7 @@ g.t <- ggplot(subset(d.t, cc_hash_size == 16384 & ppn == 16),
   my_theme
 # ggsave(plot=g.t, filename="plots/cc_times.pdf", width=16, height=10)
 
-d.stat <- melt(d, measure=c('mteps', 'ce_remote_completions', 'DelegateStats_ops', 'gce_flats'))
+d.stat <- melt(d, measure=c('mteps', 'ce_remote_completions', 'cc_set_size', 'gce_flats'))
 g.stat <- ggplot(subset(d.stat, scale == 26 & fc_version != 'async'), aes(
     x=num_starting_workers,
     y=value,
