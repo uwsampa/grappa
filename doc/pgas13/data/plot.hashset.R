4f508ca726bcad0349e38c801ca29a0633b857f1
Brandon Holt <bholt@cs.washington.edu>
2013-06-06 15:30:04 -0700

    Put stack/queue and hash{set/map} plots together.

--- doc/pgas13/data/plot.hashset.R
+++ doc/pgas13/data/plot.hashset.R
@@ -5,7 +5,7 @@ source("common.R")
 # d <- sqldf("select * from test where ppn == 16 and nnode == 8 and scale == 28", dbname="test.db")
 # f <- c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks")
 # d[f] <- lapply(d[f], factor)
-d <- db("select * from hashset where log_nelems == 28",
+dset <- db("select * from hashset where log_nelems == 28",
       c("nnode",
         "num_starting_workers",
         "aggregator_autoflush_ticks",
@@ -29,15 +29,19 @@ dmap <- db("select * from hashmap where log_nelems == 28",
         "fraction_lookups"
       ), "pgas.sqlite")
 
+dq <- db("select * from queue where ppn == 16",
+        c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks", "flat_combining","fraction_push","ppn"))
+dst <- db("select * from stack where ppn == 16",
+      c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks", "flat_combining","fraction_push","ppn"))
 
-d$ops_per_msg <- with(d, (hashset_insert_ops+hashset_lookup_ops)/(hashset_insert_msgs+hashset_lookup_msgs))
-d$throughput <- with(d, (hashset_insert_ops+hashset_lookup_ops)/trial_time_mean)
+dset$ops_per_msg <- with(dset, (hashset_insert_ops+hashset_lookup_ops)/(hashset_insert_msgs+hashset_lookup_msgs))
+dset$throughput <- with(dset, (hashset_insert_ops+hashset_lookup_ops)/trial_time_mean)
 
-d$fc_version <- sapply(paste('v',d$flat_combining,d$insert_async,sep=''),switch,
+dset$fc_version <- sapply(paste('v',dset$flat_combining,dset$insert_async,sep=''),switch,
   v0NA='none', v1NA='local', v11='async', v10='local', v00='none', v01='?'
 )
 
-d.melt <- melt(d, measure=c("ops_per_msg", "throughput"))
+d.melt <- melt(dset, measure=c("ops_per_msg", "throughput"))
 
 g <- ggplot(subset(d.melt,
     version == 'fc_looks'
@@ -62,7 +66,7 @@ g <- ggplot(subset(d.melt,
 ggsave(plot=g, filename="plots/hashset_thru.pdf", width=16, height=10)
 
 
-gg <- ggplot(subset(d, log_nelems==28 & ppn==16 & num_starting_workers==8192
+gg <- ggplot(subset(dset, log_nelems==28 & ppn==16 & num_starting_workers==8192
                     & log_max_key == 14 & version == 'fc_looks'
   ),aes(
     x=nnode,
@@ -120,3 +124,83 @@ gg <- ggplot(subset(dmap, log_nelems==28 & ppn==16 & num_starting_workers==8192
 
 ggsave(plot=gg, filename="plots/hashmap_perf.pdf", width=7, height=5)
 
+
+#####################
+# Combined
+dset <- within(dset,
+  mix <- sapply(fraction_lookups, switch, '0'='100% insert', '0.5'='50% insert, 50% lookup')
+  )
+dmap <- within(dmap,
+  mix <- sapply(fraction_lookups, switch, '0'='100% insert', '0.5'='50% insert, 50% lookup')
+)
+
+# dq <- within(dq,
+#   mix <- sapply(sel('.',fraction_pushes), select, .1='all insert', .0.5='50% mix')
+# )
+# dst <- within(dst,
+#   mix <- sapply(sel('.',fraction_pushes), select, .1='all push', .0.5='50% mix')
+# )
+
+# dmap$mix <- sapply(dmap$)
+  dq$struct <- sapply(  dq$nnode, function(i){ return('GlobalQueue') })
+ dst$struct <- sapply( dst$nnode, function(i){ return('GlobalQueue') })
+dmap$struct <- sapply(dmap$nnode, function(i){ return('GlobalHashMap') })
+dset$struct <- sapply(dset$nnode, function(i){ return('GlobalHashSet') })
+
+d.c <- merge(dmap, dset, all=T)
+
+gg <- ggplot(subset(d.c, log_nelems==28 & ppn==16 & num_starting_workers==8192
+                    & log_max_key == 14
+                     # & version == 'fc_looks'
+  ),aes(
+    x=nnode,
+    y=throughput,
+    color=x(fc_version,version,aggregator_autoflush_ticks),
+    group=x(fc_version,fraction_lookups,version,aggregator_autoflush_ticks),
+    shape=mix,
+    linetype=mix,
+  ))+
+  # geom_point()+
+  # geom_line()+
+  geom_smooth(size=1)+
+  # facet_grid(log_nelems~., scales="free", labeller=label_pretty)+
+  facet_grid(.~struct, scales="free_x")+
+  xlab("Nodes")+
+  # scale_x_continuous(breaks=c(8,16,32,48,64))+
+  scale_color_discrete(name="Flat Combining")+
+  scale_linetype_discrete(name="Fraction lookups")+
+  scale_shape_discrete(name="Fraction lookups")+
+  ylab("Throughput (ops/sec)")+expand_limits(y=0)+my_theme+
+  theme(strip.text=element_text(size=rel(0.7)),
+        axis.text.x=element_text(size=rel(0.85)))
+
+ggsave(plot=gg, filename="plots/hash_perf.pdf", width=10, height=5)
+
+# d.c <- merge(dmap, dset, all=T)
+# 
+# gg <- ggplot(subset(d.c, log_nelems==28 & ppn==16 & num_starting_workers==8192
+#                     & log_max_key == 14 & version == 'fc_looks'
+#   ),aes(
+#     x=nnode,
+#     y=throughput,
+#     color=fc_version,
+#     group=x(fc_version,version,fraction_lookups),
+#     shape=fraction_lookups,
+#     linetype=fraction_lookups,
+#   ))+
+#   # geom_point()+
+#   # geom_line()+
+#   geom_smooth(size=1)+
+#   # facet_grid(log_nelems~., scales="free", labeller=label_pretty)+
+#   facet_grid(.~struct, scales="free_x")+
+#   xlab("Nodes")+
+#   # scale_x_continuous(breaks=c(8,16,32,48,64))+
+#   scale_color_discrete(name="Flat Combining")+
+#   scale_linetype_discrete(name="Fraction lookups")+
+#   scale_shape_discrete(name="Fraction lookups")+
+#   ylab("Throughput (ops/sec)")+expand_limits(y=0)+my_theme+
+#   theme(strip.text=element_text(size=rel(0.7)),
+#         axis.text.x=element_text(size=rel(0.85)))
+# 
+# ggsave(plot=gg, filename="plots/all.pdf", width=10, height=5)
+# 
--- doc/pgas13/data/plot.stack.R
+++ doc/pgas13/data/plot.stack.R
@@ -10,41 +10,52 @@ db <- function(query, factors, db="pgas.sqlite") {
 
 d <- db("select * from stack where ppn == 16",
       c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks", "flat_combining", "ppn", "fraction_push"))
+dq <- db("select * from queue where ppn == 16",
+    c("nnode", "num_starting_workers", "aggregator_autoflush_ticks", "periodic_poll_ticks", "flat_combining", "ppn", "fraction_push"))
 
 d$ops_per_msg <- with(d, (global_vector_push_ops+global_vector_pop_ops)/(global_vector_push_msgs+global_vector_pop_msgs))
 d$throughput <- with(d, nelems/trial_time_mean)
+
+dq$throughput <- with(dq, nelems/trial_time_mean)
 
 d$fc <- with(d, x(flat_combining,flat_combining_local_only))
 
 d$fc_version <- sapply(paste('v',d$flat_combining,d$flat_combining_local_only,sep=''),switch,
   v0NA='none', v1NA='full', v10='full', v11='local', v00='none'
 )
+dq <- within(dq,
+fc_version <- sapply(paste('v',flat_combining,flat_combining_local_only,sep=''),switch,
+  v0NA='none', v1NA='full', v10='full', v11='local', v00='none'
+))
 
-d.melt <- melt(d, measure=c("global_vector_push_ops", "global_vector_pop_ops", "ops_per_msg", "throughput"))
+d.melt <- melt(d, measure=c("global_vector_push_latency", "global_vector_pop_latency", "ops_per_msg", "throughput"))
 
 g <- ggplot(subset(d.melt,
-    nnode == 16 & log_nelems == 27
+    # & nnode == 16 
+    log_nelems == 28
   ), aes(
-    x=num_starting_workers,
+    x=nnode,
     y=value,
-    color=x(fc_version,fraction_push),
+    color=fc_version,
     shape=x(ppn,version),
-    group=x(flat_combining,fraction_push,ppn,version),
+    group=x(fc_version,fraction_push,version),
+    linetype=fraction_push,
     # label=global_vector_buffer,
   ))+
   geom_point()+
   geom_line()+
   # geom_text()+
-  facet_grid(variable~log_nelems+nnode+version, scales="free", labeller=label_pretty)+
+  facet_grid(variable~log_nelems, scales="free", labeller=label_pretty)+
   ylab("")+expand_limits(y=0)+my_theme+
   theme(strip.text=element_text(size=rel(0.4)),
         axis.text.x=element_text(size=rel(0.65)))
 
-# ggsave(plot=g, filename="plots/stack_thru.pdf", width=20, height=10)
+ggsave(plot=g, filename="plots/stack_thru.pdf", width=12, height=10)
 
 d$nnode <- as.numeric(as.character(d$nnode))
-g <- ggplot(subset(d, log_nelems==28 & ppn==16 & num_starting_workers==2048
-                      & ((version == 'fixed_random' & fraction_push != 0.5) | (version == 'matching_better' & fraction_push == 0.5))
+g <- ggplot(subset(d,
+      log_nelems==28 & ppn==16 & num_starting_workers==2048
+    & ((version == 'fixed_random' & fraction_push != 0.5) | (version == 'matching_better' & fraction_push == 0.5))
   ), aes(
     x=nnode,
     y=throughput,
@@ -67,3 +78,50 @@ g <- ggplot(subset(d, log_nelems==28 & ppn==16 & num_starting_workers==2048
 
 ggsave(plot=g, filename="plots/stack_perf.pdf", width=7, height=5)
 
+
+#####################
+# Combined
+# d <- within(d,
+#   mix <- sapply(fraction_push, switch, '1'='100% push', '0.5'='50% push, 50% pop', '?')
+# )
+# dq <- within(dq,
+#   mix <- sapply(fraction_push, switch, '1'='100% pop', '0.5'='50% push, 50% pop', '?')
+# )
+dq$mix <- sapply(dq$fraction_push, function(f){ return(ifelse(f == '1', '100% push', '50% push, 50%pop')) })
+d$mix <- sapply(d$fraction_push, function(f){ return(ifelse(f == '1', '100% push', '50% push, 50%pop')) })
+
+d$struct <- sapply( d$nnode, function(i){ return('GlobalStack') })
+dq$struct <- sapply(  dq$nnode, function(i){ return('GlobalQueue') })
+
+d.c <- merge(dq, d, all=T)
+
+gg <- ggplot(subset(d.c, 
+  log_nelems==28 & ppn==16 & num_starting_workers==2048
+  & ( (version == 'fixed_random' & (fraction_push != 0.5 | struct == 'GlobalQueue')) 
+    | (version == 'matching_better' & fraction_push == 0.5 & struct == 'GlobalStack'))
+  ),aes(
+    x=nnode,
+    y=throughput,
+    color=x(fc_version),
+    group=x(fc_version,version,fraction_push),
+    linetype=mix,
+  ))+
+  # geom_point()+
+  # geom_line()+
+  geom_smooth(size=1)+
+  # facet_grid(log_nelems~., scales="free", labeller=label_pretty)+
+  facet_grid(.~struct, scales="free_x")+
+  xlab("Nodes")+
+  # scale_x_continuous(breaks=c(8,16,32,48,64))+
+  scale_color_discrete(name="Flat Combining")+
+  scale_linetype_discrete(name="Operation mix")+
+  ylab("Throughput (ops/sec)")+expand_limits(y=0)+my_theme+
+  theme(strip.text=element_text(size=rel(0.7)),
+        axis.text.x=element_text(size=rel(0.85)))
+
+ggsave(plot=gg, filename="plots/vector_perf.pdf", width=10, height=5)
+subset(d.c, select=c('struct','nnode','version','mix'),
+  log_nelems==28 & ppn==16 & num_starting_workers==2048
+  & ( (version == 'fixed_random' & fraction_push != 0.5) 
+    | (version == 'matching_better' & fraction_push == 0.5))
+)
