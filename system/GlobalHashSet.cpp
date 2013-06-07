734eb8cbb39a18ea5f9871eb536f0f59a9c828ab
Brandon Holt <bholt@cs.washington.edu>
2013-06-07 01:04:50 -0700

    Add stat to count # of matched lookups.

--- system/GlobalHashSet.cpp
+++ system/GlobalHashSet.cpp
@@ -7,3 +7,4 @@ GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_insert_ops, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_insert_msgs, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_lookup_ops, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_lookup_msgs, 0);
+GRAPPA_DEFINE_STAT(SimpleStatistic<size_t>, hashset_matched_lookups, 0);
