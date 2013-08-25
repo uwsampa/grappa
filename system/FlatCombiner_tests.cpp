3e06a26bcd5684fe5476a788a1f6e82a11f06c9f
Brandon Holt <bholt@cs.washington.edu>
2013-08-25 16:43:55 -0700

    update FlatCombiner_tests to test real GlobalCounter

--- system/FlatCombiner_tests.cpp
+++ system/FlatCombiner_tests.cpp
@@ -13,68 +13,11 @@
 // #include "GlobalVector.hpp"
 #include "Statistics.hpp"
 #include "FlatCombiner.hpp"
+#include "GlobalCounter.hpp"
 
 using namespace Grappa;
 
 BOOST_AUTO_TEST_SUITE( FlatCombiner_tests );
-
-const Core MASTER = 0;
-
-class GlobalCounter {
-public:
-  
-  struct Master {
-    long count;
-  } master;
-  
-  GlobalAddress<GlobalCounter> self;
-  
-  struct Proxy {
-    GlobalCounter* outer;
-    long delta;
-    
-    Proxy(GlobalCounter* outer): outer(outer), delta(0) {}
-    Proxy* clone_fresh() { return locale_new<Proxy>(outer); }
-    
-    void sync() {
-      auto s = outer->self;
-      auto d = delta;
-      delegate::call(MASTER,[s,d]{ s->master.count += d; });
-    }
-    
-    bool is_full() { return false; }
-  };
-  FlatCombiner<Proxy> comb;
-  
-  char pad[block_size - sizeof(comb)-sizeof(self)-sizeof(master)];
-  
-public:
-  GlobalCounter(long initial_count = 0): comb(locale_new<Proxy>(this)) {
-    master.count = initial_count;
-  }
-  
-  void incr(long d = 1) {
-    comb.combine([d](Proxy& p){
-      p.delta += d;
-    });
-  }
-  
-  long count() {
-    auto s = self;
-    return delegate::call(MASTER, [s]{ return s->master.count; });
-  }
-  
-  static GlobalAddress<GlobalCounter> create(long initial_count = 0) {
-    auto a = mirrored_global_alloc<GlobalCounter>();
-    call_on_all_cores([a]{ new (a.localize()) GlobalCounter(0); });
-    return a;
-  }
-  
-  void destroy() {
-    auto a = self;
-    call_on_all_cores([a]{ a->~GlobalCounter(); });
-  }
-};
 
 void user_main( void * ignore ) {
   
