3c073637025a3173548e9645649e505b963dddf2
Brandon Holt <bholt@cs.washington.edu>
2013-09-08 17:00:35 -0700

    refactor graph500/main_new to allow selection btwn multiple benchmarks, start on new CC

--- applications/graph500/grappa/main_new.cpp
+++ applications/graph500/grappa/main_new.cpp
@@ -19,7 +19,10 @@
 
 using namespace Grappa;
 
+/////////////////////////////////
+// Declarations
 double make_bfs_tree(GlobalAddress<Graph<VertexP>> g_in, GlobalAddress<int64_t> _bfs_tree, int64_t root);
+long cc_benchmark(GlobalAddress<Graph<VertexP>> g);
 
 /////////////////////////////////
 // Options/flags
@@ -27,6 +30,8 @@ double make_bfs_tree(GlobalAddress<Graph<VertexP>> g_in, GlobalAddress<int64_t> 
 DEFINE_int64(scale, 16, "Graph500 scale (graph will have ~2^scale vertices)");
 DEFINE_int64(edgefactor, 16, "Approximate number of edges in graph will be 2*2^(scale)*edgefactor");
 DEFINE_int64(nbfs, 8, "Number of BFS traversals to do");
+
+DEFINE_string(bench, "bfs", "Specify what graph benchmark to execute.");
 
 DEFINE_bool(verify, true, "Do verification. Note: `--noverify` is equivalent to `--verify=(false|no|0)`");
 
@@ -66,6 +71,48 @@ static void choose_bfs_roots(GlobalAddress<Graph<VertexP>> g, int * nbfs, int64_
   }
 }
 
+void bfs_benchmark(tuple_graph& tg, GlobalAddress<Graph<VertexP>> g, int nroots) {
+  GlobalAddress<int64_t> bfs_tree = global_alloc<int64_t>(g->nv);
+  
+  int64_t bfs_roots[NBFS_max];
+  choose_bfs_roots(g, &nroots, bfs_roots);
+  
+  // build bfs tree for each root
+  for (int64_t i=0; i < nroots; i++) {
+    Grappa::memset(bfs_tree, -1, g->nv);
+    
+    if (i == 0) Statistics::start_tracing();
+    
+    bfs_time[i] = make_bfs_tree(g, bfs_tree, bfs_roots[i]);
+
+    if (i == 0) Statistics::stop_tracing();
+    
+    VLOG(1) << "make_bfs_tree time: " << bfs_time[i];
+    
+    if (FLAGS_verify) {
+      VLOG(1) << "Verifying bfs " << i << "...";
+      
+      GRAPPA_TIME_LOG("verify_time") {
+        bfs_nedge[i] = verify_bfs_tree(bfs_tree, g->nv-1, bfs_roots[i], &tg);
+      }
+      
+      if (bfs_nedge[i] < 0) {
+        LOG(ERROR) << "bfs " << i << " from root " << bfs_roots[i] << " failed verification: " << bfs_nedge[i];
+        exit(1);
+      } else {
+        VLOG(0) << "bfs_time[" << i << "] = " << bfs_time[i];
+      }
+    } else {
+      LOG(ERROR) << "warning: skipping verify. Approximating nedge with `nadj/2`.";
+      bfs_nedge[i] = g->nadj/2;
+    }
+  }
+  
+  global_free(bfs_tree);
+  /* Print results. */
+  output_results(FLAGS_scale, 1<<FLAGS_scale, FLAGS_edgefactor, A, B, C, D, generation_time, construction_time, nroots, bfs_time, bfs_nedge);
+}
+
 void user_main(void * ignore) {
   double t, start_time;
   start_time = walltime();
@@ -91,53 +138,19 @@ void user_main(void * ignore) {
   
   // Graph::dump(g);
   
-  GlobalAddress<int64_t> bfs_tree = global_alloc<int64_t>(g->nv);
-  
-  int nroots = FLAGS_nbfs;
-  int64_t bfs_roots[NBFS_max];
-  choose_bfs_roots(g, &nroots, bfs_roots);
-  
-  // build bfs tree for each root
-  for (int64_t i=0; i < nroots; i++) {
-    Grappa::memset(bfs_tree, -1, g->nv);
-    
-    if (i == 0) call_on_all_cores([]{ Grappa_start_profiling(); });
-    
-    bfs_time[i] = make_bfs_tree(g, bfs_tree, bfs_roots[i]);
-
-    if (i == 0) call_on_all_cores([]{ Grappa_stop_profiling(); });
-    
-    VLOG(1) << "make_bfs_tree time: " << bfs_time[i];
-    
-    if (FLAGS_verify) {
-      VLOG(1) << "Verifying bfs " << i << "...";
-      t = walltime();
-      bfs_nedge[i] = verify_bfs_tree(bfs_tree, g->nv-1, bfs_roots[i], &tg);
-      t = walltime() - t;
-      VLOG(1) << "verify_time: " << t;
-      
-      if (bfs_nedge[i] < 0) {
-        LOG(ERROR) << "bfs " << i << " from root " << bfs_roots[i] << " failed verification: " << bfs_nedge[i];
-        exit(1);
-      } else {
-        VLOG(0) << "bfs_time[" << i << "] = " << bfs_time[i];
-      }
-    } else {
-      LOG(ERROR) << "warning: skipping verify. Approximating nedge with `nadj/2`.";
-      bfs_nedge[i] = g->nadj/2;
-    }
+  // choose benchmark
+  if (FLAGS_bench.find("bfs") != std::string::npos) {
+    bfs_benchmark(tg, g, FLAGS_nbfs);
+  }
+  if (FLAGS_bench.find("cc") != std::string::npos) {
+    cc_benchmark(g);
   }
   
-  global_free(bfs_tree);
   Grappa::Statistics::merge_and_print(std::cout);
-  fflush(stdout);
   
   g->destroy();
   global_free(tg.edges);
-  
-  /* Print results. */
-  output_results(FLAGS_scale, 1<<FLAGS_scale, FLAGS_edgefactor, A, B, C, D, generation_time, construction_time, nroots, bfs_time, bfs_nedge);
-  
+    
   LOG(INFO) << "total_runtime: " << walltime() - start_time;
 }
 
