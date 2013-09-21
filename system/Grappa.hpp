d398f8065bd746f807e47b0336f3b1e5d351975b
Jacob Nelson <nelson@cs.washington.edu>
2013-09-21 01:51:29 -0700

    Add init() and finalize() methods in grappa namespace

--- system/Grappa.hpp
+++ system/Grappa.hpp
@@ -32,6 +32,15 @@ extern TaskingScheduler global_scheduler;
 extern void failure_function();
 
 }
+
+/// Initialize Grappa. Call in SPMD context before running Grappa
+/// code. Running Grappa code before calling init() is illegal.
+void init( int * argc_p, char ** argv_p[], size_t size = -1 );
+
+/// Clean up Grappa. Call in SPMD context after all Grappa code
+/// finishes. Running Grappa code after calling finalize() is illegal.
+void finalize();
+
 }
 
 /// pointer to parent pthread
