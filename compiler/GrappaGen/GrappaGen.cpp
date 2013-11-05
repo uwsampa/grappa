e923d2d4041ceb941097caa5671812a026c215ce
Brandon Holt <bholt@cs.washington.edu>
2013-11-04 20:03:28 -0800

    add initial compiler prototype

---
+++ compiler/GrappaGen/GrappaGen.cpp
@@ -0,0 +1,190 @@
+#define DEBUG_TYPE "grappa_gen"
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/ADT/Statistic.h>
+#include <llvm/Support/Debug.h>
+#include <llvm/PassManager.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+
+#include <sstream>
+#include <set>
+#include <map>
+
+// I'm not sure why, but I can't seem to enable debug output in the usual way
+// (with command line flags). Here's a hack.
+#undef DEBUG
+#define DEBUG(s) s
+
+using namespace llvm;
+
+//STATISTIC(grappaGlobalRefs, "calls to Grappa's distributed shared memory system");
+
+namespace {
+  
+  static const int GLOBAL_SPACE = 100;
+
+  bool isaGlobalPointer(Type* type) {
+    PointerType* pt = dyn_cast<PointerType>(type);
+    if( pt && pt->getAddressSpace() == GLOBAL_SPACE ) return true;
+    return false;
+  }
+
+  //////////////////////////////////////
+  /// From llvmGlobalToWide.cpp
+  AllocaInst* makeAlloca(llvm::Type* type,
+                         const char* name,
+                         Instruction* insertBefore)
+  {
+    // It's important to alloca at the front of the function in order
+    // to avoid having an alloca in a loop which is a good way to achieve
+    // stack overflow.
+    Function *func = insertBefore->getParent()->getParent();
+    BasicBlock* entryBlock = & func->getEntryBlock();
+    
+    if( insertBefore->getParent() == entryBlock ) {
+      // Add before specific instruction in entry block.
+    } else if(llvm::Instruction *i = func->getEntryBlock().getTerminator()) {
+      // Add before terminator in entry block.
+      insertBefore = i;
+    } else {
+      // Add at the end of entry block.
+      insertBefore = NULL;
+    }
+    
+    AllocaInst *tempVar;
+    
+    if( insertBefore ) {
+      tempVar = new AllocaInst(type, name, insertBefore);
+    } else {
+      tempVar = new AllocaInst(type, name, entryBlock);
+    }
+    
+    return tempVar;
+  }
+
+  Constant* createSizeof(Type* type)
+  {
+//    assert( !containsGlobalPointers(info, type) );
+    return ConstantExpr::getSizeOf(type);
+  }
+  
+  void myReplaceInstWithInst(Instruction* Old, Instruction* New)
+  {
+    assert(Old->getParent());
+    assert(New->getParent());
+    Old->replaceAllUsesWith(New);
+    if(Old->hasName() && !New->hasName()) New->takeName(Old);
+    Old->eraseFromParent();
+  }
+  
+  struct GrappaGen : public FunctionPass {
+    static char ID;
+    
+    Constant* delegate_read_fn;
+//    std::map<Type*,Constant*> delegate_reads;
+//    Module* module;
+    Constant* get_fn;
+    Type *void_ty, *void_ptr_ty, *void_gptr_ty;
+    
+    GrappaGen() : FunctionPass(ID) { }
+
+    virtual bool runOnFunction(Function &F) {
+      bool changed = false;
+      errs() << "processing fn: " << F.getName() << "\n";
+      
+      std::vector<LoadInst*> todo;
+      
+      for (auto& bb : F) {
+        for (auto& inst : bb) {
+          switch ( inst.getOpcode() ) {
+          case Instruction::Load:
+//            grappaGlobalRefs++;
+            auto orig_ld = cast<LoadInst>(&inst);
+            if (orig_ld->getPointerAddressSpace() == GLOBAL_SPACE) {
+              todo.push_back(orig_ld);
+            }
+            break;
+          }
+        }
+      }
+      
+      for (auto* orig_ld : todo) {
+        auto ty = orig_ld->getType();
+        auto gptr = orig_ld->getPointerOperand();
+        auto alloc = makeAlloca(ty, "", orig_ld);
+        
+        errs() << "types:\n  ";
+        alloc->getAllocatedType()->dump(); errs() << "\n  ";
+        void_ptr_ty->dump(); errs() << "\n  ";
+        void_gptr_ty->dump(); errs() << "\n";
+        
+        auto void_alloc = new BitCastInst(alloc, void_ptr_ty, "", orig_ld);
+        auto void_gptr = new BitCastInst(gptr, void_gptr_ty, "", orig_ld);
+        auto szof = createSizeof(ty);
+        
+        Value* args[] = { void_alloc, void_gptr, szof };
+        auto call = CallInst::Create(get_fn, args, "", orig_ld);
+        
+        // Now load from alloc'd area
+        auto new_ld = new LoadInst(alloc, "", orig_ld->isVolatile(), orig_ld->getAlignment(), orig_ld->getOrdering(), orig_ld->getSynchScope(), orig_ld);
+        
+        myReplaceInstWithInst(orig_ld, new_ld);
+        
+        //              if (!module->getFunction("delegate_read_int")) { errs() << "unable to find!\n"; }
+        //              if (delegate_reads.count(ty) == 0) {
+        //                delegate_reads[ty] = module->getOrInsertFunction("delegate_read_int",
+        //                                                                 ty, ptr->getType(), NULL);
+        //              }
+        //              auto fn = delegate_reads[ty];
+        errs() << "global load(";
+        ty->dump();
+        errs() << ")\n";
+        changed = true;
+      }
+      
+      return changed;
+    }
+
+    virtual bool doInitialization(Module& module) {
+      errs() << "initializing\n";
+      get_fn = module.getFunction("grappa_get");
+      void_ptr_ty = Type::getInt8PtrTy(module.getContext(), 0);
+      void_gptr_ty = Type::getInt8PtrTy(module.getContext(), GLOBAL_SPACE);
+
+//      module = &m;
+//      auto int_ty = m.getTypeByName("int");
+//      delegate_read_fn = m.getOrInsertFunction("delegate_read_int", int_ty, );
+      return false;
+    }
+  
+    virtual bool doFinalization(Module &M) {
+      return true;
+    }
+
+    ~GrappaGen() {
+      llvm::errs() << "closing\n";
+    }
+  };
+  
+  char GrappaGen::ID = 0;
+  
+  //////////////////////////////
+  // Register optional pass
+  static RegisterPass<GrappaGen> X(
+    "grappa-gen", "Grappa Code Generator",
+    false, false
+  );
+  
+  //////////////////////////////
+  // Register as default pass
+  static void registerGrappaGen(const PassManagerBuilder&, PassManagerBase& PM) {
+    fprintf(stderr, "Registered GrappaGen pass!\n");
+    PM.add(new GrappaGen());
+  }
+  static RegisterStandardPasses GrappaGenRegistration(PassManagerBuilder::EP_OptimizerLast, registerGrappaGen);
+}
---
+++ compiler/setup_llvm.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'fileutils'; require 'ostruct'; require 'optparse'
+include FileUtils
+opt = OpenStruct.new
+opt.prefix = "#{Dir.pwd}/install"
+
+OptionParser.new {|p|
+  p.on("--prefix=/install/path"){|_| opt.prefix = _ }
+}.parse!(ARGV)
+
+system "git clone http://llvm.org/git/llvm.git"
+system "git clone http://llvm.org/git/clang.git llvm/tools/clang"
+# system "git clone http://llvm.org/git/clang-tools-extra.git llvm/tools/clang/tools/extra"
+# system "git clone http://llvm.org/git/libcxx.git llvm/projects/libcxx"
+
+# find prefix for current GCC so we can have clang use its stdlib headers, etc
+gcc_dir = `which gcc`[/(.*)\/bin\/gcc/,1]
+
+cd(*mkdir_p("llvm/build")) {
+  # system "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=#{opt.prefix}"
+  
+  # build LLVM & Clang using autoconf/make so we can use '--with-gcc-toolchain' to setup include directories
+  system "../configure --enable-optimized --enable-debug-symbols --with-gcc-toolchain=#{gcc_dir} --prefix=#{opt.prefix}"
+  system "make -j"
+  system "make install"
+  
+  # install cmake files so we can use find_package(LLVM)
+  cd(*mkdir_p("cmake")) {
+    system "cmake -DCMAKE_INSTALL_PREFIX=#{opt.prefix} ../.."
+    system "cmake -P cmake/modules/cmake_install.cmake"
+  }
+}
