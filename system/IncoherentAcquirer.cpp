70c7723eb3c1285e086c8dcc933346c2f9868e85
Brandon Myers <bdmyers@cs.washington.edu>
2013-11-16 16:29:54 -0800

    fix max min stats and consolidate in cache acquirer

--- system/IncoherentAcquirer.cpp
+++ system/IncoherentAcquirer.cpp
@@ -15,15 +15,9 @@
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_ams, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_ams_bytes, 0);
 GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_blocked, 0);
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_blocked_ticks_total, 0);
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_network_ticks_total, 0);
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_wakeup_ticks_total, 0);
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_blocked_ticks_max, std::numeric_limits<uint64_t>::min());
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_blocked_ticks_min, std::numeric_limits<uint64_t>::min());
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_network_ticks_max, std::numeric_limits<uint64_t>::min());
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_network_ticks_min, std::numeric_limits<uint64_t>::min());
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_wakeup_ticks_max, std::numeric_limits<uint64_t>::min());
-GRAPPA_DEFINE_STAT(SimpleStatistic<uint64_t>, acquire_wakeup_ticks_min, std::numeric_limits<uint64_t>::min());
+GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, acquire_blocked_ticks_total, 0);
+GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, acquire_network_ticks_total, 0);
+GRAPPA_DEFINE_STAT(SummarizingStatistic<uint64_t>, acquire_wakeup_ticks_total, 0);
 
     
 void IAStatistics::count_acquire_ams( uint64_t bytes ) {
@@ -38,23 +32,11 @@ void IAStatistics::record_wakeup_latency( int64_t start_time, int64_t network_ti
   int64_t wakeup_latency = current_time - network_time;
   acquire_blocked_ticks_total += blocked_latency;
   acquire_wakeup_ticks_total += wakeup_latency;
-  if( blocked_latency > acquire_blocked_ticks_max ) 
-    acquire_blocked_ticks_max = blocked_latency;
-  if( blocked_latency < acquire_blocked_ticks_min ) 
-    acquire_blocked_ticks_min = blocked_latency;
-  if( wakeup_latency > acquire_wakeup_ticks_max )
-    acquire_wakeup_ticks_max = wakeup_latency;
-  if( wakeup_latency < acquire_wakeup_ticks_min )
-    acquire_wakeup_ticks_min = wakeup_latency;
 }
 
 void IAStatistics::record_network_latency( int64_t start_time ) { 
   int64_t current_time = Grappa::timestamp();
   int64_t latency = current_time - start_time;
   acquire_network_ticks_total += latency;
-  if( latency > acquire_network_ticks_max )
-    acquire_network_ticks_max = latency;
-  if( latency < acquire_network_ticks_min )
-    acquire_network_ticks_min = latency;
 }
 
