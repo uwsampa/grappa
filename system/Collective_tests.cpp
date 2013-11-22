1181e16571f4ba527d4dafed036d7cc5800243dc
Brandon Myers <bdmyers@cs.washington.edu>
2013-11-22 14:27:27 -0800

    reducers for localizable addresses

--- system/Collective_tests.cpp
+++ system/Collective_tests.cpp
@@ -9,6 +9,8 @@
 #include "Grappa.hpp"
 #include "Communicator.hpp"
 #include "Collective.hpp"
+#include "GlobalAllocator.hpp"
+#include "Addressing.hpp"
 
 // Tests the functions in Collective.hpp
 
@@ -16,6 +18,19 @@ BOOST_AUTO_TEST_SUITE( Collective_tests );
 
 static int global_x;
 
+
+struct TestObj {
+  int64_t ignore;
+  int64_t c;
+
+} GRAPPA_BLOCK_ALIGNED;
+TestObj operator+( const TestObj& o1, const TestObj& o2 ) {
+  return {0, o1.c + o2.c};
+}
+
+int64_t accessObj(GlobalAddress<TestObj> g) {
+  return g->c;
+}
 
 void user_main( int * ignore ) {
   BOOST_MESSAGE("testing allreduce");
@@ -31,6 +46,32 @@ void user_main( int * ignore ) {
   int total_x = Grappa::reduce<int,collective_add>(&global_x);
   Core n = Grappa::cores();
   BOOST_CHECK_EQUAL(total_x, n*(n+1)/2);
+
+  GlobalAddress<TestObj> replIntAddr = Grappa::symmetric_global_alloc<TestObj>();
+  Grappa::on_all_cores([replIntAddr] {
+    replIntAddr->c = Grappa::mycore() + 1;
+  });
+  BOOST_MESSAGE("testing dynamic reduce");
+  {
+    auto result = Grappa::reduce<TestObj,collective_add>(replIntAddr);
+    BOOST_CHECK_EQUAL(result.c, n*(n+1)/2);
+  }
+
+  BOOST_MESSAGE("testing dynamic reduce with accessor");
+  {
+    int64_t result = Grappa::reduce<int64_t, TestObj,collective_add,accessObj>(replIntAddr);
+    BOOST_CHECK_EQUAL(result, n*(n+1)/2);
+  }
+
+
+  // Test fails: sadly localization of a member of block-aligned does not work
+  //BOOST_MESSAGE("testing dynamic reduce without accessor");
+  //{
+  //  int64_t result = Grappa::reduce<int64_t,collective_add>(global_pointer_to_member(replIntAddr, &TestObj::c) );
+  //  BOOST_CHECK_EQUAL(result, n*(n+1)/2);
+  //}
+
+
   
   BOOST_MESSAGE("testing allreduce_inplace");
   Grappa::on_all_cores([]{
