9b02ef95e869bd41f24981110dbf9a8bf9ba8b69
Brandon Holt <bholt@cs.washington.edu>
2013-12-09 00:42:16 -0800

    minor changes to fix up some tests

--- system/GlobalAllocator_tests.cpp
+++ system/GlobalAllocator_tests.cpp
@@ -20,47 +20,36 @@ BOOST_AUTO_TEST_SUITE( GlobalAllocator_tests );
 const size_t local_size_bytes = 1 << 14;
 
 BOOST_AUTO_TEST_CASE( test1 ) {
-  Grappa::init( GRAPPA_TEST_ARGS );
+  Grappa::init( GRAPPA_TEST_ARGS, local_size_bytes );
   Grappa::run([]{
-    Grappa::on_all_cores([] {
-      GlobalAddress< int8_t > a = Grappa::global_alloc( 1 );
-      LOG(INFO) << "got pointer " << a.pointer();
+    GlobalAddress< int8_t > a = Grappa::global_alloc( 1 );
+    LOG(INFO) << "got pointer " << a.pointer();
 
-      GlobalAddress< int8_t > b = Grappa::global_alloc( 1 );
-      LOG(INFO) << "got pointer " << b.pointer();
+    GlobalAddress< int8_t > b = Grappa::global_alloc( 1 );
+    LOG(INFO) << "got pointer " << b.pointer();
 
-      GlobalAddress< int8_t > c = Grappa::global_alloc( 8 );
-      LOG(INFO) << "got pointer " << c.pointer();
+    GlobalAddress< int8_t > c = Grappa::global_alloc( 8 );
+    LOG(INFO) << "got pointer " << c.pointer();
 
-      GlobalAddress< int8_t > d = Grappa::global_alloc( 1 );
-      LOG(INFO) << "got pointer " << d.pointer();
+    GlobalAddress< int8_t > d = Grappa::global_alloc( 1 );
+    LOG(INFO) << "got pointer " << d.pointer();
 
-  //    LOG(INFO) << *global_allocator;
+    BOOST_CHECK_EQUAL( global_allocator->total_bytes(), local_size_bytes );
+    BOOST_CHECK_EQUAL( global_allocator->total_bytes_in_use(), 1 + 1 + 8 + 1 );
 
-      if( Grappa::mycore() == 0 ) {
-        BOOST_CHECK_EQUAL( global_allocator->total_bytes(), local_size_bytes );
-        BOOST_CHECK_EQUAL( global_allocator->total_bytes_in_use(), 1 + 8 + 8 + 1 );
-      }
+    LOG(INFO) << "freeing pointer " << c.pointer();
+    Grappa::global_free( c );
 
-      LOG(INFO) << "freeing pointer " << c.pointer();
-      Grappa::global_free( c );
+    LOG(INFO) << "freeing pointer " << a.pointer();
+    Grappa::global_free( a );
 
-      LOG(INFO) << "freeing pointer " << a.pointer();
-      Grappa::global_free( a );
+    LOG(INFO) << "freeing pointer " << d.pointer();
+    Grappa::global_free( d );
 
-      LOG(INFO) << "freeing pointer " << d.pointer();
-      Grappa::global_free( d );
+    LOG(INFO) << "freeing pointer " << b.pointer();
+    Grappa::global_free( b );
 
-      LOG(INFO) << "freeing pointer " << b.pointer();
-      Grappa::global_free( b );
-
-      if( Grappa::mycore() == 0 ) {
-        BOOST_CHECK_EQUAL( global_allocator->total_bytes_in_use(), 0 );
-      }
-    });
-  
     BOOST_CHECK_EQUAL( global_allocator->total_bytes_in_use(), 0 );
-  //  LOG(INFO) << *global_allocator;
   
     LOG(INFO) << "done!";
   });
--- system/Semaphore_tests.cpp
+++ system/Semaphore_tests.cpp
@@ -14,67 +14,59 @@
 
 BOOST_AUTO_TEST_SUITE( Semaphore_tests );
 
+using namespace Grappa;
+
 void yield() {
-  Worker * thr = Grappa::impl::global_scheduler.get_current_thread();
-  Grappa::privateTask( [thr] {
-      Grappa::impl::global_scheduler.thread_wake( thr );
+  Worker * thr = impl::global_scheduler.get_current_thread();
+  privateTask( [thr] {
+      impl::global_scheduler.thread_wake( thr );
     });
-  Grappa::impl::global_scheduler.thread_suspend();
+  impl::global_scheduler.thread_suspend();
 }
 
-
-void user_main( void * args ) 
-{
-  Grappa::CountingSemaphore s(1);
-
-  // bit vector
-  int data = 1;
-  int count = 0;
-
-  Grappa::privateTask( [&s,&data,&count] { 
-      for( int i = 0; i < 3; ++i ) {
-        Grappa::decrement( &s );
-        data <<= 1;
-        data |= 1;
-        count++;
-        Grappa::increment( &s );
-        yield();
-      }
-    } );
-
-  Grappa::privateTask( [&s,&data,&count] { 
-      for( int i = 0; i < 3; ++i ) {
-        Grappa::decrement( &s );
-        data <<= 1;
-        count++;
-        Grappa::increment( &s );
-        yield();
-      }
-    } );
-
-  // other thread runs and blocks
-  while( count < 6 ) {
-    DVLOG(5) << Grappa::impl::global_scheduler.get_current_thread() << ": Checking count=" << count;
-    yield();
-  }
-
-  BOOST_CHECK_EQUAL( count, 6 );
-
-  Grappa::Statistics::merge_and_print();
-  //Grappa::Statistics::print();
-}
 
 BOOST_AUTO_TEST_CASE( test1 ) {
+  Grappa::init( GRAPPA_TEST_ARGS );
+  Grappa::run([]{
+    CountingSemaphore s(1);
 
-  Grappa_init( &(boost::unit_test::framework::master_test_suite().argc),
-	       &(boost::unit_test::framework::master_test_suite().argv),
-	       (1L << 20) );
+    // bit vector
+    int data = 1;
+    int count = 0;
 
-  Grappa_activate();
+    privateTask( [&s,&data,&count] { 
+        for( int i = 0; i < 3; ++i ) {
+          decrement( &s );
+          data <<= 1;
+          data |= 1;
+          count++;
+          increment( &s );
+          yield();
+        }
+      } );
 
-  Grappa_run_user_main( &user_main, (void*)NULL );
+    privateTask( [&s,&data,&count] { 
+        for( int i = 0; i < 3; ++i ) {
+          decrement( &s );
+          data <<= 1;
+          count++;
+          increment( &s );
+          yield();
+        }
+      } );
 
-  Grappa_finish( 0 );
+    // other thread runs and blocks
+    while( count < 6 ) {
+      DVLOG(5) << impl::global_scheduler.get_current_thread() << ": Checking count=" << count;
+      yield();
+    }
+
+    BOOST_CHECK_EQUAL( count, 6 );
+  
+    Statistics::merge_and_print();
+    //Statistics::print();
+  });
+  Grappa::finalize();
 }
 
 BOOST_AUTO_TEST_SUITE_END();
