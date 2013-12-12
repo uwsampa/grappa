575e7ea692caa43a62ad3363388b820d89d36176
Brandon Holt <bholt@cs.washington.edu>
2013-12-11 19:58:31 -0800

    More tricky test case (loops).

--- compiler/test/branch.cpp
+++ compiler/test/branch.cpp
@@ -5,6 +5,27 @@
 
 using namespace Grappa;
 
+void foo(long& x) {
+  long global* xa = make_global(&x);
+  long y = 0;
+  long global* ya = make_global(&y);
+  
+  if (*xa < 9) {
+    *ya = *xa + 6;
+  } else {
+    (*ya)++;
+    while (*ya <= 1) {
+      (*ya)++;
+      if (*xa) {
+        *xa = 1;
+      }
+    }
+  }
+  
+  CHECK_EQ(x, 1);
+  CHECK_EQ(y, 2);
+}
+
 int main(int argc, char* argv[]) {
   init(&argc, &argv);
   run([]{
@@ -28,6 +49,8 @@ int main(int argc, char* argv[]) {
     
     VLOG(0) << "x => " << x;
     CHECK_EQ(x, 9);
+    
+    foo(x);
     
     Statistics::merge_and_dump_to_file();
   });
