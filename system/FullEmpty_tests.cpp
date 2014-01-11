cf7227f9a8f8f2dd9380160b6218f66116c7a90e
Brandon Holt <bholt@cs.washington.edu>
2014-01-10 20:47:37 -0800

    Add in FullEmpty test

--- system/FullEmpty_tests.cpp
+++ system/FullEmpty_tests.cpp
@@ -55,78 +55,69 @@ void test_remote_fe(int trial) {
   ce->wait();
 }
 
-void user_main( void * args ) 
-{
-  CompletionEvent ce;
-  for (int i=0; i<100; i++) {
-    test_remote_fe(i);
-  }
-  ce.wait();
-  
-  Grappa::FullEmpty< int64_t > fe_int;
-
-  BOOST_CHECK_EQUAL( fe_int.readXX(), 0 );
-
-  fe_int.writeXF( 1 );
-
-  BOOST_CHECK_EQUAL( fe_int.full(), true );
-  BOOST_CHECK_EQUAL( fe_int.readXX(), 1 );
-
-  BOOST_CHECK_EQUAL( fe_int.readFE(), 1 );
-  fe_int.writeEF( 2 );
-  BOOST_CHECK_EQUAL( fe_int.readFE(), 2 );
-  fe_int.writeEF( 3 );
-  BOOST_CHECK_EQUAL( fe_int.readFE(), 3 );
-  fe_int.writeEF( 1 );
-
-  return;
-
-  Grappa::privateTask( [&] { 
-      int64_t temp = 0;
-      while( (temp = fe_int.readFE()) != 1 ) { 
-	fe_int.writeEF( temp ); 
-      }
-
-      fe_int.writeEF( 2 );
-
-      while( (temp = fe_int.readFE()) != 3 ) { 
-	fe_int.writeEF( temp ); 
-      }
-
-      fe_int.writeEF( 4 );
-
-    } );
-
-  int64_t temp = 0;
-  
-  while( (temp = fe_int.readFE()) != 2 ) { 
-    fe_int.writeEF( temp ); 
-  }
-  
-  fe_int.writeEF( 3 );
-  
-  while( (temp = fe_int.readFE()) != 4 ) { 
-    fe_int.writeEF( temp ); 
-  }
-  
-  fe_int.writeEF( 5 );
-
-  BOOST_CHECK_EQUAL( fe_int.readFE(), 5 );
-
-  Grappa_merge_and_dump_stats();
-}
-
 BOOST_AUTO_TEST_CASE( test1 ) {
+  Grappa::init( GRAPPA_TEST_ARGS );
+  Grappa::run([]{
 
-  Grappa_init( &(boost::unit_test::framework::master_test_suite().argc),
-	       &(boost::unit_test::framework::master_test_suite().argv),
-	       (1L << 20) );
+    CompletionEvent ce;
+    for (int i=0; i<100; i++) {
+      test_remote_fe(i);
+    }
+    ce.wait();
+  
+    Grappa::FullEmpty< int64_t > fe_int;
 
-  Grappa_activate();
+    BOOST_CHECK_EQUAL( fe_int.readXX(), 0 );
 
-  Grappa_run_user_main( &user_main, (void*)NULL );
+    fe_int.writeXF( 1 );
 
-  Grappa_finish( 0 );
+    BOOST_CHECK_EQUAL( fe_int.full(), true );
+    BOOST_CHECK_EQUAL( fe_int.readXX(), 1 );
+
+    BOOST_CHECK_EQUAL( fe_int.readFE(), 1 );
+    fe_int.writeEF( 2 );
+    BOOST_CHECK_EQUAL( fe_int.readFE(), 2 );
+    fe_int.writeEF( 3 );
+    BOOST_CHECK_EQUAL( fe_int.readFE(), 3 );
+    fe_int.writeEF( 1 );
+
+    return;
+
+    Grappa::privateTask( [&] { 
+        int64_t temp = 0;
+        while( (temp = fe_int.readFE()) != 1 ) { 
+  	fe_int.writeEF( temp ); 
+        }
+
+        fe_int.writeEF( 2 );
+
+        while( (temp = fe_int.readFE()) != 3 ) { 
+  	fe_int.writeEF( temp ); 
+        }
+
+        fe_int.writeEF( 4 );
+
+      } );
+
+    int64_t temp = 0;
+  
+    while( (temp = fe_int.readFE()) != 2 ) { 
+      fe_int.writeEF( temp ); 
+    }
+  
+    fe_int.writeEF( 3 );
+  
+    while( (temp = fe_int.readFE()) != 4 ) { 
+      fe_int.writeEF( temp ); 
+    }
+  
+    fe_int.writeEF( 5 );
+
+    BOOST_CHECK_EQUAL( fe_int.readFE(), 5 );
+
+    Grappa::Statistics::merge_and_print();
+  });
+  Grappa::finalize();
 }
 
 BOOST_AUTO_TEST_SUITE_END();
