ef9b12db896db271d89a5e69bb72b9c42f2ac2b9
Brandon Holt <bholt@cs.washington.edu>
2014-01-17 15:08:41 -0800

    add symmetric ptr test

---
+++ compiler/test/symmetric.cpp
@@ -0,0 +1,38 @@
+#include <Grappa.hpp>
+#include <Primitive.hpp>
+#include <Collective.hpp>
+#include <GlobalAllocator.hpp>
+
+using namespace Grappa;
+
+struct Foo {
+  long x, y;
+  
+  void bar() {
+    printf("%ld, %ld\n", x, y);
+  }
+  
+} GRAPPA_BLOCK_ALIGNED;
+
+int main(int argc, char* argv[]) {
+  init(&argc, &argv);
+  run([]{
+    
+    Foo symmetric* g = symmetric_global_alloc<Foo>();
+    
+    on_all_cores([=]{
+      
+      g->x = mycore();
+      g->y = 12345;
+      
+      g->bar();
+      
+    });
+    
+    for (Core c=0; c<cores(); c++) {
+      CHECK_EQ( delegate::call(c, [=]{ return gaddr(g)->x; }), c );
+    }
+    
+  });
+  finalize();
+}
