6cc2ce2e2588f0f2644f982b0296ebaec47f3e72
Brandon Holt <bholt@cs.washington.edu>
2014-01-21 21:00:30 -0800

    remove applications/CombBLAS

--- applications/CombBLAS/graph500-1.2/kronecker.h
+++
@@ -1,7 +0,0 @@
-#if !defined(KRONECKER_HEADER_)
-#define KRONECKER_HEADER_
-
-void kronecker_edgelist (int64_t *IJ, int64_t nedge, int64_t SCALE,
-			 double A, double B, double C);
-
-#endif /* KRONECKER_HEADER_ */
