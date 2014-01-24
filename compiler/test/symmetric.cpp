ea3a4c3ae7fa08b9cf36fc8da7720125c00acaed
Brandon Holt <bholt@cs.washington.edu>
2014-01-23 23:45:22 -0800

    add test of symmetric* with extracted delegate; works with -O1, fails with -O3

--- compiler/test/symmetric.cpp
+++ compiler/test/symmetric.cpp
@@ -8,30 +8,39 @@ using namespace Grappa;
 struct Foo {
   long x, y;
   
-  void bar() {
-    printf("%ld, %ld\n", x, y);
+  void bar(long z) {
+    printf("x: %ld, y: %ld, z: %ld\n", x, y, z);
   }
   
 } GRAPPA_BLOCK_ALIGNED;
+
+long z;
 
 int main(int argc, char* argv[]) {
   init(&argc, &argv);
   run([]{
     
-    Foo symmetric* g = symmetric_global_alloc<Foo>();
+    Foo symmetric* s = symmetric_global_alloc<Foo>();
     
     on_all_cores([=]{
       
-      g->x = mycore();
-      g->y = 12345;
+      s->x = mycore();
+      s->y = 12345;
       
-      g->bar();
+      s->bar(0);
+      
+      z = 7 * mycore();
       
     });
     
     for (Core c=0; c<cores(); c++) {
-      CHECK_EQ( delegate::call(c, [=]{ return symm_addr(g)->x; }), c );
+      CHECK_EQ( delegate::call(c, [=]{ return symm_addr(s)->x; }), c );
     }
+    
+    // test delegates with symmetric addresses
+    long global* z1 = make_global(&z, 1);
+    
+    s->bar(*z1);
     
   });
   finalize();
