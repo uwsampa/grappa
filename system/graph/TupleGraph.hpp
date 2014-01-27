f3152d4a49c427b8337750e6cc6ef706acb3cef8
Brandon Holt <bholt@cs.washington.edu>
2014-01-26 21:07:05 -0800

    Working on getting Graph500 TupleGraph generator together

--- system/graph/TupleGraph.hpp
+++ system/graph/TupleGraph.hpp
@@ -1,8 +1,19 @@
 
-class TupleGraph {
-public:
+namespace Grappa {
+
+  class TupleGraph {
+  public:    
+    struct Edge { int64_t v0, v1; };
+
+    GlobalAddress<packed_edge> edges;
+    int64_t nedge; /* Number of edges in graph, in both cases */
   
-  
-  GlobalAddress<packed_edge> edges;
-  int64_t nedge; /* Number of edges in graph, in both cases */
-};
+    static TupleGraph generate_kronecker(int scale, int64_t desired_nedge, 
+                                         uint64_t seed1, uint64_t seed2);
+    
+  protected:
+    TupleGraph(int64_t nedge): edges(global_alloc<Edge>(nedge)), nedge(nedge) {}
+    
+  };
+
+}
