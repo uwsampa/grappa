9af1e5918091e3491cb68fd7efa11929c3d97dbd
Brandon Holt <bholt@cs.washington.edu>
2014-01-29 16:52:05 -0800

    implement verifier & choose_root (and uncover issues with pass)

--- compiler/apps/bfs.cpp
+++ compiler/apps/bfs.cpp
@@ -28,11 +28,124 @@ struct BFSVertex : public Vertex {
   Data* operator->() { return reinterpret_cast<Data*>(vertex_data); }
 };
 
+int64_t nedge_traversed;
+
 int64_t verify(TupleGraph tg, SymmetricAddress<Graph<BFSVertex>> g, int64_t root) {
-  delegate::call(g->vs+root, [=](BFSVertex* v){
-    v->parent = root;
+  
+  auto get_level = [g](int64_t j){
+    return delegate::call(g->vs+j, [](BFSVertex& v){ return v->level; });
+  };
+  auto get_parent = [g](int64_t j){
+    return delegate::call(g->vs+j, [](BFSVertex& v){ return v->parent; });
+  };
+  
+  // check root
+  delegate::call(g->vs+root, [=](BFSVertex& v){
+    CHECK_EQ(v->parent, root);
   });
-  CHECK_EQ(  );
+  
+  // compute levels
+  delegate::call(g->vs+root, [](BFSVertex& v){ v->level = 0; });
+  
+  forall(g->vs, g->nv, [=](int64_t i, BFSVertex& v){
+    if (v->level >= 0) return;
+    
+    if (v->parent >= 0 && i != root) {
+      int64_t parent = i;
+      int64_t nhop = 0;
+      int64_t next_parent;
+      
+      while (parent >= 0 && get_level(parent) < 0 && nhop < g->nv) {
+        next_parent = get_parent(parent);
+        CHECK_NE(parent, next_parent);
+        parent = next_parent;
+        ++nhop;
+      }
+      
+      CHECK_LT(nhop, g->nv);
+      CHECK_GE(parent, 0);
+      
+      // now assign levels until we meet an already-leveled vertex
+      nhop += get_level(parent);
+      parent = i;
+      while (get_level(parent) < 0) {
+        CHECK_GT(nhop, 0);
+        parent = delegate::call(g->vs+parent, [=](BFSVertex& v){
+          v->level = nhop;
+          return v->parent;
+        });
+        nhop--;
+      }
+      CHECK_EQ(nhop, get_level(parent));
+    }
+  });
+  
+  call_on_all_cores([]{ nedge_traversed = 0; });
+  
+  // verify levels & parents match
+  forall(tg.edges, tg.nedge, [=](TupleGraph::Edge& e){
+    auto max_bfsvtx = g->nv - 1;
+    auto i = e.v0, j = e.v1;
+    
+    int64_t lvldiff;
+    
+    if (i < 0 || j < 0) return;
+    CHECK(!(i > max_bfsvtx && j <= max_bfsvtx)) << "Error!";
+    CHECK(!(j > max_bfsvtx && i <= max_bfsvtx)) << "Error!";
+    if (i > max_bfsvtx) // both i & j are on the same side of max_bfsvtx
+      return;
+    
+    // All neighbors must be in the tree.
+    auto ti = get_parent(i), tj = get_parent(j);
+    CHECK(!(ti >= 0 && tj < 0)) << "Error! ti=" << ti << ", tj=" << tj;
+    CHECK(!(tj >= 0 && ti < 0)) << "Error! ti=" << ti << ", tj=" << tj;
+    if (ti < 0) // both i & j have the same sign
+      return;
+    
+    /* Both i and j are in the tree, count as a traversed edge.
+     
+     NOTE: This counts self-edges and repeated edges.  They're
+     part of the input data.
+     */
+    nedge_traversed++;
+    
+    auto mark_seen = [g](int64_t i){
+      delegate::call(g->vs+i, [](BFSVertex& v){ v->seen = true; });
+    };
+    
+    // Mark seen tree edges.
+    if (i != j) {
+      if (ti == j)
+        mark_seen(i);
+      if (tj == i)
+        mark_seen(j);
+    }
+    lvldiff = get_level(i) - get_level(j);
+    /* Check that the levels differ by no more than one. */
+    CHECK(!(lvldiff > 1 || lvldiff < -1)) << "Error, levels differ by more than one! " << "(k = " << ", lvl[" << i << "]=" << get_level(i) << ", lvl[" << j << "]=" << get_level(j) << ")";
+  });
+  
+  nedge_traversed = Grappa::reduce<int64_t,collective_add>(&nedge_traversed);
+  
+  // check that every BFS edge was seen & that there's only one root
+  forall(g->vs, g->nv, [=](int64_t i, BFSVertex& v){
+    if (i != root) {
+      CHECK(!(v->parent >= 0 && !v->seen)) << "Error!";
+      CHECK_NE(v->parent, i);
+    }
+  });
+  
+  // everything checked out!
+  return nedge_traversed;
+}
+
+template< typename T >
+int64_t choose_root(Graph<T> symmetric* g) {
+  int64_t root;
+  do {
+    root = random() % g->nv;
+  } while (g->vs[root].nadj == 0);
+  return root;
 }
 
 int main(int argc, char* argv[]) {
@@ -49,28 +162,34 @@ int main(int argc, char* argv[]) {
     });
     
     // intialize parent to -1
-    forall(symm_addr(g), [](BFSVertex& v){ v.parent(-1); });
+    forall(symm_addr(g), [](BFSVertex& v){ v->parent = -1; });
     
-    int64_t root = 0;
-    g->vs[root]->parent = 0;
+    int64_t root = choose_root(g);
+    g->vs[root]->parent = root;
+    
+    *f_tail++ = root;
     
     double t = walltime();
     
     on_all_cores([=]{
-      joiner.enroll();
-      barrier();
+      int64_t level = 0;
       
       auto vs = as_ptr(g->vertices());
       
       int64_t next_level_total;
       
       do {
+        if (mycore() == 0) VLOG(1) << "level " << level;
         f_level = f_tail;
+        joiner.enroll();
+        barrier();
         
         while (f_head < f_level) {
           auto i = *f_head++;  // pop off frontier
+          VLOG(1) << "  " << i;
           
           for (auto j : vs[i].adj_iter()) {
+            VLOG(1) << "    -> " << j;
 //            spawn<&joiner>([=]{
               if (vs[j]->parent == -1) {
                 vs[j]->parent = i;
