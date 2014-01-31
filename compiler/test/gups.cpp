71ed2c289126a8c75934a4e1320569e286e31a75
Brandon Holt <bholt@cs.washington.edu>
2014-01-31 11:48:44 -0800

    fix compiler gups to use new grappa API

--- compiler/test/gups.cpp
+++ compiler/test/gups.cpp
@@ -1,21 +1,15 @@
 // make -j TARGET=gups1.exe mpi_run GARGS=" --sizeB=$(( 1 << 28 )) --loop_threshold=1024" PPN=8 NNODE=12
 
-#include <Primitive.hpp>
 #include <Grappa.hpp>
-#include <Collective.hpp>
-#include <ParallelLoop.hpp>
-#include <Delegate.hpp>
-#include <AsyncDelegate.hpp>
-#include <GlobalAllocator.hpp>
-#include <Array.hpp>
+#include <Primitive.hpp>
 
 using namespace Grappa;
 
 DEFINE_int64( sizeA, 1 << 30, "Size of array that GUPS increments" );
 DEFINE_int64( sizeB, 1 << 20, "Number of iterations" );
 
-GRAPPA_DEFINE_STAT( SimpleStatistic<double>, gups_runtime, 0.0 );
-GRAPPA_DEFINE_STAT( SimpleStatistic<double>, gups_throughput, 0.0 );
+GRAPPA_DEFINE_METRIC( SimpleMetric<double>, gups_runtime, 0.0 );
+GRAPPA_DEFINE_METRIC( SimpleMetric<double>, gups_throughput, 0.0 );
 
 int main( int argc, char * argv[] ) {
   init( &argc, &argv );
@@ -26,13 +20,13 @@ int main( int argc, char * argv[] ) {
     Grappa::memset( gaddr(A), 0, FLAGS_sizeA );
 
     int64_t global* B = global_alloc<int64_t>(FLAGS_sizeB);
-    forall_localized( gaddr(B), FLAGS_sizeB, [](int64_t& b) {
+    forall( gaddr(B), FLAGS_sizeB, [](int64_t& b) {
       b = random() % FLAGS_sizeA;
     });
 
     double start = walltime();
 
-    forall_global_public(0, FLAGS_sizeB, [=](int64_t i){
+    forall(0, FLAGS_sizeB, [=](int64_t i){
       A[B[i]]++;
     });
 
