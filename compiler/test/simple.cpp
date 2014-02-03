0621f0f5f61deccfea3d95d96d63e613f327efb3
Brandon Holt <bholt@cs.washington.edu>
2014-02-02 21:11:25 -0800

    Add simple test (doesn't include Grappa to keep it smaller)

---
+++ compiler/test/simple.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define global    __attribute__((address_space(100)))
+#define symmetric __attribute__((address_space(200)))
+#define async     __attribute__((annotate("async")))
+
+struct Vertex {
+  Vertex global* adj;
+  long nadj;
+};
+
+struct Graph {
+  Vertex global* vs;
+  long nv;
+  
+  Graph(long nv): vs(reinterpret_cast<Vertex global*>(new Vertex[nv])), nv(nv) {}
+  
+  static Graph symmetric* create(long nv) {
+    return reinterpret_cast<Graph symmetric*>(new Graph(nv));
+  }
+};
+
+template< typename T >
+T global* make_global(T* a) { return reinterpret_cast<T global*>(a); }
+
+template< typename T >
+T symmetric* make_symmetric(T* a) { return reinterpret_cast<T symmetric*>(a); }
+
+
+async int main(int argc, char* argv[]) {
+  
+  auto g = Graph::create(10);
+  
+  printf("%ld\n", g->nv);
+  
+  printf("%ld\n", g->vs[6].nadj);
+  
+  printf("%ld\n", g->vs[6].adj[3].nadj);
+  
+  long x = argc;
+  long global* a = make_global(new long);
+  long global* b = make_global(new long);
+  
+  *a += x + *b;
+  
+}
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//<pid 19269 SIGSEGV (signal 11)>
+/////////////////////////////////////////////////////////////
