31e3052cc34c012c9391b73191ade5a181557ecb
Brandon Holt <bholt@cs.washington.edu>
2014-02-24 22:40:08 -0800

    let us know if we've started gups

--- compiler/apps/gups.cpp
+++ compiler/apps/gups.cpp
@@ -15,6 +15,8 @@ GRAPPA_DEFINE_METRIC( SimpleMetric<double>, gups_throughput, 0.0 );
 int main(int argc, char* argv[]) {
   init(&argc, &argv);
   run([]{
+    LOG(INFO) << "running";
+    
     int64_t global* A = global_alloc<int64_t>(FLAGS_sizeA);
     Grappa::memset( gaddr(A), 0, FLAGS_sizeA );
 
@@ -24,6 +26,7 @@ int main(int argc, char* argv[]) {
       b = random() % FLAGS_sizeA;
     });
     
+    LOG(INFO) << "starting timed portion";
     double start = walltime();
     
     forall(gaddr(B), FLAGS_sizeB, [=](int64_t& b){
@@ -40,7 +43,7 @@ int main(int argc, char* argv[]) {
     global_free(gaddr(A));
     
     if (FLAGS_metrics) Metrics::merge_and_print();
-    
+      
   });
   finalize();
 }
