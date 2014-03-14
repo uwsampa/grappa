05be7f4fa9a2be35f5a9bf57274ac1b9ad3b40fa
Brandon Holt <bholt@cs.washington.edu>
2014-03-14 01:15:47 -0700

    add igor_cc script

---
+++ compiler/apps/igor_cc.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+require 'igor'
+
+# inherit parser, sbatch_flags
+require_relative_to_symlink '../../util/igor_common.rb'
+
+Igor do
+  include Isolatable
+  
+  database '~/exp/oopsla14.sqlite', :cc
+
+  exes = %w[ cc.putget.exe
+           cc.ext.exe
+           cc.hand.exe
+         ]
+  
+  params { exe *exes }
+  
+  isolate exes
+  
+  GFLAGS.merge!({
+    v: 0,
+    scale: 16,
+    concurrent_roots: 8
+  })
+  GFLAGS.delete :flat_combining
+  
+  params.merge!(GFLAGS)
+  
+  @c = -> cl { %Q[ %{tdir}/grappa_srun --no-freeze-on-error
+    -- %{tdir}/%{exe} --metrics
+    #{GFLAGS.expand}
+  ].gsub(/\s+/,' ') }
+  command @c['cc.ext.exe']
+  
+  sbatch_flags << "--time=10:00"
+  
+  expect :ncomponents, :components_time
+  
+  interact # enter interactive mode
+end
