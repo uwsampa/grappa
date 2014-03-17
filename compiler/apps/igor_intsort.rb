2e2f29f0da2f08548c75061c0032d01dbed1e5f1
Brandon Holt <bholt@cs.washington.edu>
2014-03-17 15:41:04 -0700

    add igor_intsort

---
+++ compiler/apps/igor_intsort.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'igor'
+require_relative_to_symlink '../igor_db'
+
+Igor do
+  include Isolatable
+  
+  database(table: :intsort)
+  
+  exes = %w[ intsort.putget.exe
+           intsort.ext.exe
+           intsort.hand.exe
+         ]
+  
+  params { exe *exes }
+  
+  isolate exes
+  
+  GFLAGS.merge!({
+    v: 0,
+    cls: 16,
+  })
+  GFLAGS.delete :flat_combining
+  
+  params.merge!(GFLAGS)
+  
+  @c = ->{ %Q[ %{tdir}/grappa_srun --no-freeze-on-error
+    -- %{tdir}/%{exe} --metrics
+    #{GFLAGS.expand}
+  ].gsub(/\s+/,' ') }
+  command @c[]
+  
+  sbatch_flags << "--time=10:00"
+  
+  expect :rank_time_mean
+  
+  interact # enter interactive mode
+end
