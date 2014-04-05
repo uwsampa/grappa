ef2c83f53f412d2062c0f5c28ad64c296f9a0c4a
Brandon Holt <bholt@cs.washington.edu>
2014-04-04 22:30:48 -0700

    igor for graphlab pagerank

---
+++ applications/graphlab/igor_graphlab_pagerank.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require 'igor'
+
+Igor do
+  include Isolatable
+  
+  database '~/exp/osdi.sqlite', :pagerank
+  
+  params { version 'grappa' }
+  
+  isolate 'pagerank.exe'
+  
+  GFLAGS.merge!({
+    path: '/pic/projects/grappa/twitter/bintsv4/twitter-all.bintsv4',
+    format: 'bintsv4',
+  })
+  GFLAGS.delete :flat_combining
+  
+  params.merge!(GFLAGS)
+  
+  @c = -> { %Q[ %{tdir}/grappa_srun --no-freeze-on-error
+    -- %{tdir}/%{exe} --metrics
+    #{GFLAGS.expand}
+  ].gsub(/\s+/,' ') }
+  command @c[]
+  
+  sbatch_flags << "--time=10:00"
+  
+  params {
+    nnode 16
+    ppn   16
+    num_starting_workers 512
+    loop_threshold 1024
+    trial 1
+  }
+  
+  expect :total_time
+  
+  interact # enter interactive mode
+end
