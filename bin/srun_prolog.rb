02a0328df2c61ba8456e391a5c0de85dc65694be
Jacob Nelson <nelson@cs.washington.edu>
2014-04-20 14:45:57 -0700

    change default mvapich2 flags based on gups experiments

--- bin/srun_prolog.rb
+++ bin/srun_prolog.rb
@@ -22,12 +22,14 @@ ENV["VT_MPITRACE"] = "no"
 ## set MVAPICH2 options to avoid keeping around malloced memory
 ## (and some performance tweaks which may be irrelevant)
 ENV["MV2_USE_LAZY_MEM_UNREGISTER"] = "0"
-ENV["MV2_ON_DEMAND_THRESHOLD"] = "3000"
+ENV["MV2_HOMOGENEOUS_CLUSTER"] = "1"
 
-ENV["MV2_DEFAULT_MAX_SEND_WQE"] = "2"
-ENV["MV2_DEFAULT_MAX_RECV_WQE"] = "2"
-ENV["MV2_MAX_INLINE_SIZE"] = "0"
-ENV["MV2_NUM_RDMA_BUFFER"] = "4"
+ENV["MV2_USE_RDMA_FAST_PATH"] = "0"
+
+ENV["MV2_SRQ_MAX_SIZE"] = "8192"
+#ENV["MV2_USE_XRC"] = "1" # doesn't seem to work with 1.9b on pal
+
+#ENV["MV2_USE_MCAST"] = "1" # doesn't always work on pal
 
 ## set MVAPICH2 options to avoid keeping around malloced memory
 ENV["OMPI_MCA_mpi_leave_pinned"] = "0"
