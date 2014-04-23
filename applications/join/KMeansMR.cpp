3580735ae4a5bd51365fa2de643c91363a284f81
Brandon Myers <bdmyers@cs.washington.edu>
2014-04-22 19:52:55 -0700

    debugged Kmeans because C++ sucks

--- applications/join/KMeansMR.cpp
+++ applications/join/KMeansMR.cpp
@@ -1,16 +1,30 @@
 #include "MapReduce.hpp"
 #include <cmath>
+#include <limits>
 
 
-#define SIZE 10
+#define SIZE 8
 
 using namespace Grappa;
 
-template <typename Size>
+template <int Size>
 class Vector {
-  float[Size] data;
+  private:
+    float data[Size];
   
   public:
+    Vector() { }
+    Vector(std::vector<float>& el) {
+      for (int i=0; i<Size; i++) {
+        data[i] = el[i];
+      }
+    }
+    Vector(float all) {
+      for (int i=0; i<Size; i++) {
+        data[i] = all;
+      }
+    }
+
     Vector& operator+=(const Vector& rhs) {
       for (int i=0; i<Size; i++) {
         this->data[i] += rhs.data[i];
@@ -40,15 +54,25 @@ class Vector {
       }
       return sumdist;
     }
-};
+} GRAPPA_BLOCK_ALIGNED;
+
+
+template <int Size>
+std::ostream& operator<<(std::ostream& o, Vector<Size> const& v) {
+  return o<<"V(TODO)";
+}
 
 typedef int32_t clusterid_t;
 
-template <typename Size>
+template <int Size>
 struct Cluster {
   Vector<Size> center;
   clusterid_t id;
 };
+template <int Size>
+std::ostream& operator<<(std::ostream& o, Cluster<Size> const& c) {
+  return o << "(" << c.id << ", " << c.center << ")";
+}
 
 struct means_aligned {
   std::vector<Vector<SIZE>> means;
@@ -56,32 +80,34 @@ struct means_aligned {
 GlobalAddress<means_aligned> means;
 
 
-template <typename Size>
+template <int Size>
 clusterid_t find_cluster(Vector<Size>& p) {
-  clusterid_t = 0;
-  for (auto m : means.localize()) {
-    int64_t min_dist;
-    clusterid_t min_idx;
-    auto cur_dist = p.sq_dist(means[clusterid_t]);
+  clusterid_t c = 0;
+  int64_t min_dist = LLONG_MAX;
+  clusterid_t argmin_idx = -1;
+  DVLOG(5) << "find cluster in means of size " << means->means.size();
+  for (auto m : means->means) {
+    auto cur_dist = p.sq_dist(m);
     if ( cur_dist < min_dist ) {
       min_dist = cur_dist;
-      min_idx = clusterid_t;
+      argmin_idx = c;
     }
+    ++c;
   }
 
-  return min_idx;
+  return argmin_idx;
 } 
 
 
-template <typename Size=SIZE>
-void KMeansMap( const MapperContext<clusterid_t,Vector<SIZE>,Cluster>& ctx, Vector& p ) {
+template <int Size=SIZE>
+void KMeansMap( const MapperContext<clusterid_t,Vector<Size>,Cluster<Size>>& ctx, Vector<Size>& p ) {
   auto closest = find_cluster( p );
   ctx.emitIntermediate( closest, p );
 }
 
-template <typename Size=SIZE>
+template <int Size=SIZE>
 void KMeansReduce( Reducer<clusterid_t,Vector<Size>,Cluster<Size>>& ctx, clusterid_t id, std::vector<Vector<Size>> points ) {
-  Vector<Size> center = 0; 
+  Vector<Size> center(0); 
   for ( auto local_it = points.begin(); local_it!= points.end(); ++local_it ) {
     center += *local_it; 
   }
@@ -93,38 +119,66 @@ void KMeansReduce( Reducer<clusterid_t,Vector<Size>,Cluster<Size>>& ctx, cluster
   emit( ctx, res );
 }
 
-GlobalCompletionEvent broadcast_gce;
 
 void kmeans() {
-  int K = 10;
-  int numred = cores();
+  int K = 2;
+  size_t numred = cores();
   
   // read in points
-  auto points = global_alloc<Point<SIZE>>(100);
+  int numpoints = 100;
+  auto points = global_alloc<Vector<SIZE>>(numpoints);
   //TODO
+  forall(points, numpoints, [=](int64_t i, Vector<SIZE>& p) {
+      float ff = i;
+      std::vector<float> v;
+      for (int j = 0; j<SIZE; j++) v.push_back(ff+j);
+      p = Vector<SIZE>(v);
+      });
 
   // allocate space for current means
-  means = symmetric_global_alloc<Vector<SIZE>*>(means);
-  on_all_cores([=] {
-      means->means = std::vector<Vector<SIZE>>>();
+  means = symmetric_global_alloc<means_aligned>();
+  call_on_all_cores([=] {
+      means->means = std::vector<Vector<SIZE>>();
   });
 
+  // pick K of the points to be centers
+  std::vector<Vector<SIZE>> initial_means;
+  for (int c=0; c<K; c++) {
+    // TODO actually randomly pick a center without replacement
+    int idx = c;
+    initial_means.push_back(delegate::read(points + idx));
+  }
+  // broadcast initial centers (poor mans broadcast)
+  for (auto m : initial_means) {
+    call_on_all_cores([=] {
+      means->means.push_back(m);
+    });
+  }
+  call_on_all_cores([=] {
+    CHECK( means->means.size() == K ) << means->means.size() << " != " << K;
+  });
 
-  float tempDist = 1f;
-  float convergeDist = 0.1f;
+  float tempDist = 1;
+  float convergeDist = 0.1;
+  uint32_t iter = 0;
   while ( tempDist > convergeDist ) {
-    GlobalAddress<Reducer<clusterid_t,Vector<SIZE>,Cluster<SIZE>> iter_result = 
-      MapReduceJobExecute(points, numred, &KMeansMap, &KMeansReduce); 
+    GlobalAddress<Reducer<clusterid_t,Vector<SIZE>,Cluster<SIZE>>> iter_result = 
+      MapReduceJobExecute<Vector<SIZE>,clusterid_t,Vector<SIZE>,Cluster<SIZE>>(points, numpoints, numred, &KMeansMap<SIZE>, &KMeansReduce<SIZE>); 
 
     std::vector<Vector<SIZE>> oldMeans(means->means);
 
     // send means to all nodes using
     // poor man's all-to-all
-    forall<&broadcast_gce>(iter_result, numred, [=](Cluster<Size> e) {
-        call_on_all_cores([=] {
-          means->means[e.id] = e.center;
-        });
-    })
+    forall(iter_result, numred, [=](Reducer<clusterid_t,Vector<SIZE>,Cluster<SIZE>>& r) {
+        for (Cluster<SIZE> clust : *(r.result)) {
+          call_on_all_cores([=] {
+            DVLOG(4) << "saving to means[" << clust.id << "]";
+            means->means[clust.id] = clust.center;
+          });
+        }        
+        r.result->clear();
+    });
+    global_free(iter_result); 
 
     // how much did all centers move?
     float newDist = 0.0f;
@@ -132,5 +186,16 @@ void kmeans() {
       newDist += means->means[i].sq_dist(oldMeans[i]);
     }
     tempDist = newDist;
+    LOG(INFO) << "iteration " << iter << ": dist=" << tempDist;
+
+    ++iter;
   }
+}
+
+int main(int argc, char** argv) {
+  Grappa::init(&argc, &argv);
+  Grappa::run([=] {
+      kmeans();
+  });
+  Grappa::finalize();
 }
