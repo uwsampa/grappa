96fadb38a5457118799058e888c50d0f775722d9
Brandon Holt <bholt@cs.washington.edu>
2014-04-29 23:44:07 -0700

    start/stop tracing in Pagerank app

--- applications/graphlab/pagerank.cpp
+++ applications/graphlab/pagerank.cpp
@@ -137,12 +137,14 @@ int main(int argc, char* argv[]) {
       
       if (i == 0) {
         Metrics::reset_all_cores();
+        Metrics::start_tracing();
         total_time.reset(); // don't count the first one
         total_rank = 0;
         forall(g, [](G::Vertex& v){ total_rank += v->rank; });
         std::cerr << "total_rank: " << total_rank << "\n";
       }      
     }
+    Metrics::stop_tracing();
     
     LOG(INFO) << "-- pagerank done";
     
