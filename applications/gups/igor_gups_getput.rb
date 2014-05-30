db3fa30048683ea7cfce9043564eaafc3e471afb
Jacob Nelson <nelson@cs.washington.edu>
2014-05-30 10:04:27 -0700

    add cmake commands for scurve

--- applications/gups/igor_gups_getput.rb
+++ applications/gups/igor_gups_getput.rb
@@ -7,12 +7,12 @@ Igor do
   
   database '~/osdi.sqlite', :pagerank
   
-  params { version 'grappa' }
+  params { version 'grappa-getput' }
   
   isolate 'gups-getput.exe'
   
   GFLAGS.merge!({
-      sizeB: 2**30
+      sizeB: 2**32
   })
   GFLAGS.delete :flat_combining
   
@@ -27,12 +27,12 @@ Igor do
   sbatch_flags << "--time=10:00"
   
   params {
-    nnode 64
-    ppn   16
-    num_starting_workers 512, 1024
-    aggregator_autoflush_ticks 12.5e6.to_i, 25e6.to_i, 50e6.to_i
-    periodic_poll_ticks 2e5.to_i 1e6.to_i
-    loop_threshold 16 256 1024
+    nnode 16
+    ppn   32
+    num_starting_workers 1024, 1536, 2048
+    aggregator_autoflush_ticks 1e6.to_i, 2e6.to_i, 3e6.to_i, 4e6.to_i, 5e6.to_i, 6e6.to_i, 7e6.to_i, 10e6.to_i
+    periodic_poll_ticks 1e6.to_i
+    loop_threshold 16
     shared_pool_chunk_size 2**15
     global_heap_fraction 0.2
     # max_iterations 10
