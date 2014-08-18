a3d915b18bdaa72aacb2ea7cf811e9897eafcfe7
York Wei <york.wei@gmail.com>
2014-08-18 16:26:31 -0700

    writing all result tuples in bin

--- applications/join/relation_io.hpp
+++ applications/join/relation_io.hpp
@@ -126,14 +126,13 @@ tuple_graph readEdges( std::string fn, int64_t numTuples ) {
 
 // assumes that for object T, the address of T is the address of its fields
 template <typename T>
-size_t writeTuplesUnordered( std::string fn, std::vector<T> * vec,
+void writeTuplesUnordered( std::string fn, std::vector<T> * vec,
 			     int64_t numfields ) {
   // we get just the size of the fields (since T is a padded data type)
   size_t row_size_bytes = sizeof(int64_t) * numfields;
   VLOG(2) << "row_size_bytes=" << row_size_bytes; 
   std::string data_path = FLAGS_relations+"/"+fn;
-  size_t ntuples = 1; //(*vec).size() ; 
-  VLOG(1) << fn << " has " << ntuples << " rows"; 
+  size_t ntuples = (*vec).size(); 
 
   
   size_t offset_counter;
@@ -149,16 +148,16 @@ size_t writeTuplesUnordered( std::string fn, std::vector<T> * vec,
     VLOG(5) << "opening addr " << &data_path_char; 
     VLOG(5) << "opening " << data_path_char; 
 
-    auto tuples = Grappa::global_alloc<T>(ntuples);
+    //    auto tuples = Grappa::global_alloc<T>(ntuples);
     
     // find my array split
     auto local_start = vec;
     auto local_end = vec + ntuples;
     size_t local_count = local_end - local_start;
 
+    VLOG(1) << *vec;
     // reserve a file split
-    int64_t offset = Grappa::delegate::fetch_and_add( offset_counter_addr, local_count );
-    VLOG(2) << "result offset " << offset;
+    //    int64_t offset = Grappa::delegate::fetch_and_add( offset_counter_addr, local_count );
     std::ofstream data_file(data_path_char, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
     CHECK( data_file.is_open() ) << data_path_char << " failed to open";
     VLOG(5) << "writing";
@@ -166,33 +165,25 @@ size_t writeTuplesUnordered( std::string fn, std::vector<T> * vec,
 
     int j = 0;
     int i = 0;
-    while (j < offset) {
+    while (j < (*local_start).size()) {
       while (i < (*local_start)[j].numFields()) {
 	//	data_file <<  (*local_start)[j].get(i) << " ";
 	int64_t val = (*local_start)[j].get(i);
 	data_file.write((char*)&val, sizeof(val));
-
+	VLOG(1) << val;
 	i++;
       }
+      //      VLOG(1) << (*local_start)[j];
       j++;
+      i = 0;
     }
+    VLOG(1) << i << " " << j;
 
-    /*
-    int i = 0;
-    while (i < (*local_start).size()) {
-      //      data_file << (*local_start)[i] << "\n";
-      data_file.write( (char*)(local_start + i), sizeof(T));
-      i++;
-    }
-    */
     data_file.close();
     
     VLOG(4) << "local first row: " << local_start;
     });
-
-  return ntuples;
 }
-
 
 // assumes that for object T, the address of T is the address of its fields
 template <typename T>
