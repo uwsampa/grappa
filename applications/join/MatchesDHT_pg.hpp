b010ba0acce008db72cdd2406e44f2c1dc469087
Brandon Myers <bdmyers@cs.washington.edu>
2015-05-06 17:41:00 -0700

    compiler error

--- applications/join/MatchesDHT_pg.hpp
+++ applications/join/MatchesDHT_pg.hpp
@@ -52,7 +52,7 @@ class MatchesDHT_pg {
     struct Cell {
       GlobalAddress<ListNode> entries;
 
-      Cell() : entries( Grappa::make_global(NULL) ) {}
+      Cell() : entries( make_global((ListNode*)NULL) ) {}
     };
 
     struct lookup_result {
