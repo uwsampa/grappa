3ec1284b9e4044cbcfe551c2d02e58e0a4571731
Brandon Myers <bdmyers@cs.washington.edu>
2015-05-13 15:18:18 -0700

    add MAX as an aggregate

--- applications/join/Aggregates.hpp
+++ applications/join/Aggregates.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 
 namespace Aggregates {
   template < typename State, typename UV >
@@ -10,6 +11,15 @@ namespace Aggregates {
     State COUNT(const State& sofar, const UV& nextval) {
       return sofar + 1;
     }
+
+  // keep MAX macro from being used here
+#pragma push_macro("MAX")
+#undef MAX
+  template <typename State, typename UV >
+    State MAX(const State& sofar, const UV& nextval) {
+      return std::max(sofar, nextval); 
+    }
+#pragma pop_macro("MAX")
 
   template <typename N>
   N Zero() {
