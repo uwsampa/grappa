4007e0eb90c2020ac97e3e5068a54d8f8aea384a
Jacob Nelson <nelson@cs.washington.edu>
2015-08-16 01:14:05 -0700

    rerelease under BSD license

--- system/GlobalCompletionEvent.cpp
+++ system/GlobalCompletionEvent.cpp
@@ -1,24 +1,35 @@
 ////////////////////////////////////////////////////////////////////////
-// This file is part of Grappa, a system for scaling irregular
-// applications on commodity clusters. 
-
-// Copyright (C) 2010-2014 University of Washington and Battelle
-// Memorial Institute. University of Washington authorizes use of this
-// Grappa software.
-
-// Grappa is free software: you can redistribute it and/or modify it
-// under the terms of the Affero General Public License as published
-// by Affero, Inc., either version 1 of the License, or (at your
-// option) any later version.
-
-// Grappa is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// Affero General Public License for more details.
-
-// You should have received a copy of the Affero General Public
-// License along with this program. If not, you may obtain one from
-// http://www.affero.org/oagpl.html.
+// Copyright (c) 2010-2015, University of Washington and Battelle
+// Memorial Institute.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//     * Redistributions of source code must retain the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials
+//       provided with the distribution.
+//     * Neither the name of the University of Washington, Battelle
+//       Memorial Institute, or the names of their contributors may be
+//       used to endorse or promote products derived from this
+//       software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// UNIVERSITY OF WASHINGTON OR BATTELLE MEMORIAL INSTITUTE BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
 ////////////////////////////////////////////////////////////////////////
 
 #include "GlobalCompletionEvent.hpp"
